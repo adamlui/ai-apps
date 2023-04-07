e55e2d6feaa87b5f5d9274feecfb132a2b66bb4d
adamlui <adamlui@users.noreply.github.com>
2023-04-07 00:43:06 +0000

    Changed default `vpYoffset` to 23 in `chatgpt.notify()` ↞ [auto-sync from `adamlui/chatgpt-widescreen`]

--- chatgpt-widescreen-mode/chatgpt-widescreen-mode.user.js
+++ chatgpt-widescreen-mode/chatgpt-widescreen-mode.user.js
@@ -11,7 +11,7 @@
 // @name:es             Modo de Pantalla Ancha de ChatGPT 🖥️
 // @name:fr             Mode Écran Large ChatGPT 🖥️
 // @name:it             ChatGPT Modalità Widescreen 🖥️
-// @version             2023.04.05.1
+// @version             2023.04.06
 // @description         Adds Widescreen + Full-Window modes to ChatGPT for reduced scrolling
 // @author              Adam Lui (刘展鹏), Xiao-Ying Yo (小影哟) & mefengl (冯不游)
 // @namespace           https://github.com/adamlui
@@ -67,7 +67,7 @@
         notify: function(msg, position, notifDuration, shadow) {
             notifDuration = notifDuration ? +notifDuration : 1.75; // sec duration to maintain notification visibility
             var fadeDuration = 0.6; // sec duration of fade-out
-            var vpYoffset = 13, vpXoffset = 27; // px offset from viewport border
+            var vpYoffset = 23, vpXoffset = 27; // px offset from viewport border
 
             // Make/stylize/insert div
             var notificationDiv = document.createElement('div'); // make div
