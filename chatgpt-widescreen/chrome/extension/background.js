bd096e6b8e174e4e347e1953d526ad0f008bbe5d
adamlui <adamlui@users.noreply.github.com>
2023-04-10 11:55:37 +0000

    Simplified listener logic ↞ [auto-sync from `adamlui/chatgpt-widescreen`]

--- chatgpt-widescreen/chrome/extension/background.js
+++ chatgpt-widescreen/chrome/extension/background.js
@@ -1,18 +1,13 @@
-/* Monitor active tab URL to toggle extension on matches */
+/* Monitor active tab URL to toggle extension on chat.openai.com */
 
 // Auto-enable on install/update
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.local.set({ 'chatGPTws_extensionDisabled': false })
 })
 
-// Extract content script matches from manifest
-var matches = chrome.runtime.getManifest().content_scripts.map(
-    (contentScript) => contentScript.matches.map(
-        (match) => match.replace(/\*$/, ''))).flat()
-
 // Add listener
 chrome.tabs.onActivated.addListener((activeInfo) => {
     chrome.tabs.get(activeInfo.tabId, (tab) => {
-        if (matches.some((url) => tab.url.includes(url))) {
+        if (tab.url.includes('https://chat.openai.com/chat')) {
             chrome.tabs.sendMessage(tab.id, { type: 'toggleExtension' })
 }})})
