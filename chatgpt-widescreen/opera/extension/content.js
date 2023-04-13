4533ae8cc657df9277031ff3e9bf778e5721e172
adamlui <adamlui@users.noreply.github.com>
2023-04-13 19:15:30 +0000

    Updated chatgpt.js URL ↞ [auto-sync from `adamlui/chatgpt-widescreen`]

--- chatgpt-widescreen/opera/extension/content.js
+++ chatgpt-widescreen/opera/extension/content.js
@@ -1,4 +1,4 @@
-/* NOTE: This script uses code from the powerful chatgpt.js library @ https://chatgptjs.org */
+/* NOTE: This script uses code from the powerful chatgpt.js library @ https://chatgpt.js.org */
 /* (c) 2023 Adam Lui, 冯不游 & chatgpt.js under the MIT license. */
 
 (async function() {
