d777f4b43371a3f1a086c32892e69fbe9ed98e5b
adamlui <adamlui@users.noreply.github.com>
2023-04-19 07:16:11 +0000

    Reshaped reply-box ↞ [auto-sync from `kudoai/bravegpt`]

--- bravegpt/greasemonkey/bravegpt.user.js
+++ bravegpt/greasemonkey/bravegpt.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name                BraveGPT 🤖
-// @version             2023.04.18
+// @version             2023.04.19
 // @author              Adam Lui
 // @namespace           https://github.com/adamlui
 // @description         Adds ChatGPT answers to Brave Search sidebar
@@ -492,7 +492,7 @@
             + '.chatgpt-js { font-family: var(--brand-font) ; font-size: .65rem ; position: relative ; right: .9rem } '
             + '.chatgpt-js > a { color: inherit ; top: .054rem } '
             + '.chatgpt-js > svg { top: 3px ; position: relative ; margin-right: 1px } '
-            + '.continue-chat > textarea { border: none ; background: #eeeeee70 ; height: 2.15rem ; width: 100% ; margin: -6px 0 5px 0 ; resize: none ; max-height: 200px ; padding: 9px 0 5px 10px } '
+            + '.continue-chat > textarea { border: none ; background: #eeeeee70 ; height: 2.15rem ; width: 100% ; margin: -6px 0 5px 0 ; resize: none ; max-height: 200px ; padding: 9px 0 5px 10px ; border-radius: 15px 16px 15px 0 } '
             + '.continue-chat > button { position: relative ; bottom: 54px; left: 299px; border: none ; background: none ; margin: 13px 4px 0 0 ; color: lightgrey ; cursor: pointer } '
             + '.kudo-ai { margin-left: 7px ; font-size: .65rem ; color: #aaa } '
             + '.kudo-ai a { color: #aaa ; text-decoration: none } '
