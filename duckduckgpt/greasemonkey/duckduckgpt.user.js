5ad48e32801edc72d66ca9813b1ad1a9b68e474e
adamlui <adamlui@users.noreply.github.com>
2023-04-20 00:06:24 +0000

    Changed chatgpt.js import src to CDN, re-ordered code ↞ [auto-sync from `kudoai/duckduckgpt`]

--- duckduckgpt/greasemonkey/duckduckgpt.user.js
+++ duckduckgpt/greasemonkey/duckduckgpt.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name                DuckDuckGPT 🤖
-// @version             2023.4.19.1
+// @version             2023.4.19.2
 // @author              Adam Lui
 // @namespace           https://github.com/adamlui
 // @description         Adds ChatGPT answers to DuckDuckGo sidebar
@@ -31,6 +31,7 @@
 // @connect             raw.githubusercontent.com
 // @connect             chat.openai.com
 // @connect             c1b9-67-188-52-169.ngrok.io
+// @require             https://cdn.jsdelivr.net/gh/chatgptjs/chatgpt.js@25d3b75b45a09687caa47c741b2718187927fee0/dist/chatgpt-1.2.3.min.js
 // @grant               GM_getValue
 // @grant               GM_setValue
 // @grant               GM_deleteValue
@@ -44,13 +45,12 @@
 // @supportURL          https://github.duckduckgpt.com/issues
 // ==/UserScript==
 
-// NOTE: This script uses code from the powerful chatgpt.js library @ https://chatgpt.js.org (c) 2023 Adam Lui & 冯不游 under the MIT license.
+// NOTE: This script uses code from the powerful chatgpt.js library @ https://chatgpt.js.org (c) 2023 Adam Lui, chatgpt.js & contributors under the MIT license.
 
 (function() {
 
     // API endpoints
     var openAIauthDomain = 'https://auth0.openai.com'
-    var chatGPTsessURL = 'https://chat.openai.com/api/auth/session'
     var openAIchatEndpoint = 'https://chat.openai.com/backend-api/conversation'
     var proxyEndpointMap = [[ 'https://c1b9-67-188-52-169.ngrok.io', 'pk-pJNAtlAqCHbUDTrDudubjSKeUVgbOMvkRQWMLtscqsdiKmhI', 'gpt-3.5-turbo' ]]
 
@@ -64,73 +64,73 @@
         suggestOpenAI: 'Proxy API is not working. (Try switching off Proxy Mode in toolbar)'
     }
 
-    // Import chatgpt.js functions
+    // Initialize settings/messages/menu
+    var config = {}, configKeyPrefix = 'ddgpt_', messages = []
+    loadSetting('proxyAPIenabled', 'prefixEnabled', 'suffixEnabled')
+    registerMenu() // create browser toolbar menu
+    
+    // Exit if prefix/suffix required but not present
+    if (( config.prefixEnabled && !/.*q=%2F/.test(document.location) ) || // if prefix required but not present
+        ( config.suffixEnabled && !/.*q=.*%3F(&|$)/.test(document.location) )) { // or suffix required but not present
+            return }
 
-    window.chatgptNotifyProps = { quadrants: { topRight: [], bottomRight: [], bottomLeft: [], topLeft: [] }};
-    var chatgpt = {
-        notify: function(msg, position, notifDuration, shadow) {
-            notifDuration = notifDuration ? +notifDuration : 1.75; // sec duration to maintain notification visibility
-            var fadeDuration = 0.6; // sec duration of fade-out
-            var vpYoffset = 23, vpXoffset = 27; // px offset from viewport border
+    // Stylize elements
+    var ddgptStyle = document.createElement('style')
+    ddgptStyle.innerText = (
+        '.ddgpt-container { border-radius: 8px ; border: 1px solid #dadce0 ; padding: 16px 26px ; flex-basis: 0 ;'
+            + 'flex-grow: 1 ; word-wrap: break-word ; white-space: pre-wrap ; box-shadow: 0 2px 3px rgba(0, 0, 0, 0.06) }'
+        + '.ddgpt-container p { margin: 0 } '
+        + '.ddgpt-container .prefix { font-size: 1.5rem ; font-weight: 700 }'
+        + '.ddgpt-container .prefix > a { color: inherit ; text-decoration: none }'
+        + '.ddgpt-container .loading { color: #b6b8ba ; animation: pulse 2s cubic-bezier(.4,0,.6,1) infinite }'
+        + '.ddgpt-container.sidebar-free { margin-left: 60px ; height: fit-content }'
+        + '.ddgpt-container pre { font-size: 1.14rem ; white-space: pre-wrap ; min-width: 0 ; margin: 12px 0 0 0 ; line-height: 21px ; padding: 1.25em ; border-radius: 10px }'
+        + '@keyframes pulse { 0%, to { opacity: 1 } 50% { opacity: .5 }}'
+        + '.ddgpt-container section.loading { padding-left: 5px } ' /* left-pad loading status when sending replies */
+        + '.chatgpt-feedback { margin: 2px 0 25px }'
+        + '.balloon-tip { content: "" ; position: relative ; top: 5px ; right: 16.5em ; border: 7px solid transparent ;'
+            + 'border-bottom-style: solid ; border-bottom-width: 1.19rem ; border-bottom-color: #eaeaea ; border-top: 0 }'
+        + '.continue-chat > textarea { border: none ; background: #eeeeee70 ; height: 1.55rem ; width: 97.6% ; margin: 3px 0 15px 0 ; resize: none ; max-height: 200px ; padding: 9px 0 5px 10px ; border-radius: 12px 13px 12px 0 } '
+        + '.kudo-ai { position: relative ; left: 6px ; color: #aaa } '
+        + '.kudo-ai a { color: #aaa ; text-decoration: none } '
+        + '.kudo-ai a:hover { color: black ; text-decoration: none } '
+    )
+    document.head.appendChild(ddgptStyle) // append style to <head>
 
-            // Make/stylize/insert div
-            var notificationDiv = document.createElement('div'); // make div
-            notificationDiv.style.cssText = ( // stylize it
-                  '/* Box style */   background-color: black ; padding: 10px ; border-radius: 8px ; '
-                + '/* Visibility */  opacity: 0 ; position: fixed ; z-index: 9999 ; font-size: 1.8rem ; color: white ; '
-                + ( shadow ? ( 'box-shadow: -8px 13px 25px 0 ' + ( /\b(shadow|on)\b/gi.test(shadow) ? 'gray' : shadow )) : '' ));
-            document.body.appendChild(notificationDiv); // insert into DOM
+    // Create DDGPT container & add class
+    var ddgptDiv = document.createElement('div') // create container div
+    ddgptDiv.className = 'ddgpt-container'
 
-            // Determine div position/quadrant
-            notificationDiv.isTop = !position || !/low|bottom/i.test(position) ? true : false;
-            notificationDiv.isRight = !position || !/left/i.test(position) ? true : false;
-            notificationDiv.quadrant = (notificationDiv.isTop ? 'top' : 'bottom')
-                                     + (notificationDiv.isRight ? 'Right' : 'Left');
+    // Create feedback footer & add classes/HTML
+    var ddgptFooter = document.createElement('div')
+    ddgptFooter.className = 'feedback-prompt chatgpt-feedback'
+    ddgptFooter.innerHTML = '<a href="https://github.ddgpt.com/discussions/new/choose" class="feedback-prompt__link" target="_blank">Share Feedback</a>'
 
-            // Store div in global memory
-            window.chatgptNotifyProps.quadrants[notificationDiv.quadrant].push(notificationDiv); // store div in global object
+    // Activate promo campaign if active
+    GM.xmlHttpRequest({
+        method: 'GET', url: 'https://raw.githubusercontent.com/kudoai/duckduckgpt/main/ads/live/creative.html',
+        onload: function(response) {
+            if (response.status == 200) {
 
-            // Position notification (defaults to top-right)
-            notificationDiv.style.top = notificationDiv.isTop ? vpYoffset.toString() + 'px' : '';
-            notificationDiv.style.bottom = !notificationDiv.isTop ? vpYoffset.toString() + 'px' : '';
-            notificationDiv.style.right = notificationDiv.isRight ? vpXoffset.toString() + 'px' : '';
-            notificationDiv.style.left = !notificationDiv.isRight ? vpXoffset.toString() + 'px' : '';
+                // Create campaign div & add class/style/HTML
+                var pcDiv = document.createElement('div')
+                pcDiv.className = 'ddgpt-container'
+                pcDiv.style.display = 'flex'
+                pcDiv.innerHTML = response.responseText
 
-            // Reposition old notifications
-            var thisQuadrantDivs = window.chatgptNotifyProps.quadrants[notificationDiv.quadrant];
-            if (thisQuadrantDivs.length > 1) {
-                var divsToMove = thisQuadrantDivs.slice(0, -1); // exclude new div
-                for (var j = 0; j < divsToMove.length; j++) {
-                    var oldDiv = divsToMove[j];
-                    var offsetProp = oldDiv.style.top ? 'top' : 'bottom'; // pick property to change
-                    var vOffset = +oldDiv.style[offsetProp].match(/\d+/)[0] + 5 + oldDiv.getBoundingClientRect().height;
-                    oldDiv.style[offsetProp] = `${vOffset}px`; // change prop
-            }}
+                // Create feedback footer & add classes/HTML
+                var pcFooter = document.createElement('div')
+                pcFooter.className = 'feedback-prompt chatgpt-feedback'
+                pcFooter.innerHTML = '<a href="https://github.ddgpt.com/discussions/new/choose" class="feedback-prompt__link" target="_blank">Share Feedback</a>'
 
-            // Show notification
-            notificationDiv.innerHTML = msg; // insert msg
-            notificationDiv.style.transition = 'none'; // remove fade effect
-            notificationDiv.style.opacity = 1; // show msg
+                // Inject in sidebar
+                ddgptFooter.insertAdjacentElement('afterend', pcDiv)
+                pcDiv.insertAdjacentElement('afterend', pcFooter)
+    }}})
 
-            // Hide notification
-            var hideDelay = ( // set delay before fading
-                fadeDuration > notifDuration ? 0 // don't delay if fade exceeds notification duration
-                : notifDuration - fadeDuration); // otherwise delay for difference
-            notificationDiv.hideTimer = setTimeout(function hideNotif() { // maintain notification visibility, then fade out
-                notificationDiv.style.transition = 'opacity ' + fadeDuration.toString() + 's'; // add fade effect
-                notificationDiv.style.opacity = 0; // hide notification
-                notificationDiv.hideTimer = null; // prevent memory leaks
-            }, hideDelay * 1000); // ...after pre-set duration
+    loadDDGPT()
 
-            // Destroy notification
-            notificationDiv.destroyTimer = setTimeout(function destroyNotif() {
-                notificationDiv.remove(); thisQuadrantDivs.shift(); // remove from DOM + memory
-                notificationDiv.destroyTimer = null; // prevent memory leaks
-            }, Math.max(fadeDuration, notifDuration) * 1000); // ...after notification hid
-        }
-    };
-
-    // Define userscript functions
+    // Define SCRIPT functions
 
     function registerMenu() {
         var menuID = [] // to store registered commands for removal while preserving order
@@ -189,7 +189,7 @@
         config[key] = value // and memory
     }
 
-    // Define console/alert functions
+    // Define CONSOLE/ALERT functions
 
     var ddgptConsole = {
         info: function(msg) {console.info('🐤 DuckDuckGPT >> ' + msg)},
@@ -277,17 +277,8 @@
 
         // Get answer from ChatGPT
         var data = {}
-        if (!config.proxyAPIenabled) {
-            data = JSON.stringify({
-                action: 'next', messages: messages,
-                model: model, parent_message_id: uuidv4(), max_tokens: 4000
-            })
-        } else {
-            data = JSON.stringify({
-                messages: messages,
-                model: model, max_tokens: 4000
-            })
-        }
+        if (!config.proxyAPIenabled) data = JSON.stringify({ action: 'next', messages: messages, model: model, parent_message_id: uuidv4(), max_tokens: 4000 })
+        else data = JSON.stringify({ messages: messages, model: model, max_tokens: 4000 })
 
         GM.xmlHttpRequest({
             method: 'POST', url: endpoint,
@@ -302,8 +293,7 @@
                 else { // if proxy mode
                     if (getShowReply.attemptCnt < 1 && proxyEndpointMap.length > 1) retryDiffHost()
                     else ddgptAlert('suggestOpenAI')
-                }
-            }
+            }}
         })
 
         function responseType() {
@@ -444,75 +434,5 @@
         } else { messages.push({ role: 'user', content: query }) }
         getShowReply(messages)
     }
-
-    // Run MAIN routine
-
-    // Initialize script
-    var config = {}, configKeyPrefix = 'ddgpt_'
-    loadSetting('proxyAPIenabled', 'prefixEnabled', 'suffixEnabled')
-    registerMenu() // create browser toolbar menu
-    var messages = []
-
-    // Load DuckDuckGPT if necessary
-    if (( !config.prefixEnabled && !config.suffixEnabled) || // prefix/suffix not required
-            ( config.prefixEnabled && /.*q=%2F/.test(document.location)) || // or prefix required & included
-            ( config.suffixEnabled && /.*q=.*%3F(&|$)/.test(document.location) )) { // or suffix required & included
-
-        // Stylize response container + reply box + footer
-        var ddgptStyle = document.createElement('style')
-        ddgptStyle.innerText = (
-            '.ddgpt-container { border-radius: 8px ; border: 1px solid #dadce0 ; padding: 16px 26px ; flex-basis: 0 ;'
-                + 'flex-grow: 1 ; word-wrap: break-word ; white-space: pre-wrap ; box-shadow: 0 2px 3px rgba(0, 0, 0, 0.06) }'
-            + '.ddgpt-container p { margin: 0 } '
-            + '.ddgpt-container .prefix { font-size: 1.5rem ; font-weight: 700 }'
-            + '.ddgpt-container .prefix > a { color: inherit ; text-decoration: none }'
-            + '.ddgpt-container .loading { color: #b6b8ba ; animation: pulse 2s cubic-bezier(.4,0,.6,1) infinite }'
-            + '.ddgpt-container.sidebar-free { margin-left: 60px ; height: fit-content }'
-            + '.ddgpt-container pre { font-size: 1.14rem ; white-space: pre-wrap ; min-width: 0 ; margin: 12px 0 0 0 ; line-height: 21px ; padding: 1.25em ; border-radius: 10px }'
-            + '@keyframes pulse { 0%, to { opacity: 1 } 50% { opacity: .5 }}'
-            + '.ddgpt-container section.loading { padding-left: 5px } ' /* left-pad loading status when sending replies */
-            + '.chatgpt-feedback { margin: 2px 0 25px }'
-            + '.balloon-tip { content: "" ; position: relative ; top: 5px ; right: 16.5em ; border: 7px solid transparent ;'
-                + 'border-bottom-style: solid ; border-bottom-width: 1.19rem ; border-bottom-color: #eaeaea ; border-top: 0 }'
-            + '.continue-chat > textarea { border: none ; background: #eeeeee70 ; height: 1.55rem ; width: 97.6% ; margin: 3px 0 15px 0 ; resize: none ; max-height: 200px ; padding: 9px 0 5px 10px ; border-radius: 12px 13px 12px 0 } '
-            + '.kudo-ai { position: relative ; left: 6px ; color: #aaa } '
-            + '.kudo-ai a { color: #aaa ; text-decoration: none } '
-            + '.kudo-ai a:hover { color: black ; text-decoration: none } '
-        )
-        document.head.appendChild(ddgptStyle) // append style to <head>
-
-        // Create DDGPT container & add class
-        var ddgptDiv = document.createElement('div') // create container div
-        ddgptDiv.className = 'ddgpt-container'
-
-        // Create feedback footer & add classes/HTML
-        var ddgptFooter = document.createElement('div')
-        ddgptFooter.className = 'feedback-prompt chatgpt-feedback'
-        ddgptFooter.innerHTML = '<a href="https://github.ddgpt.com/discussions/new/choose" class="feedback-prompt__link" target="_blank">Share Feedback</a>'
-
-        // Activate promo campaign if active
-        GM.xmlHttpRequest({
-            method: 'GET', url: 'https://raw.githubusercontent.com/kudoai/duckduckgpt/main/ads/live/creative.html',
-            onload: function(response) {
-                if (response.status == 200) {
-
-                    // Create campaign div & add class/style/HTML
-                    var pcDiv = document.createElement('div')
-                    pcDiv.className = 'ddgpt-container'
-                    pcDiv.style.display = 'flex'
-                    pcDiv.innerHTML = response.responseText
-
-                    // Create feedback footer & add classes/HTML
-                    var pcFooter = document.createElement('div')
-                    pcFooter.className = 'feedback-prompt chatgpt-feedback'
-                    pcFooter.innerHTML = '<a href="https://github.ddgpt.com/discussions/new/choose" class="feedback-prompt__link" target="_blank">Share Feedback</a>'
-
-                    // Inject in sidebar
-                    ddgptFooter.insertAdjacentElement('afterend', pcDiv)
-                    pcDiv.insertAdjacentElement('afterend', pcFooter)
-        }}})
-
-        loadDDGPT()
-    }
-
+ 
 })()
