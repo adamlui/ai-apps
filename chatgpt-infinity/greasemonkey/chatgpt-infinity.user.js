551dcc567ba0b63cc325102a2f8eb781e6fec994
adamlui <adamlui@users.noreply.github.com>
2023-04-28 18:06:54 +0000

    Added non-English meta ↞ [auto-sync from `adamlui/chatgpt-infinity`]

--- chatgpt-infinity/greasemonkey/chatgpt-infinity.user.js
+++ chatgpt-infinity/greasemonkey/chatgpt-infinity.user.js
@@ -1,11 +1,100 @@
 // ==UserScript==
 // @name                ChatGPT Infinity ∞
-// @name:zh-CN          ChatGPT无限 ∞
-// @name:zh-HK          ChatGPT無限 ∞
-// @name:zh-SG          ChatGPT无限 ∞
-// @name:zh-TW          ChatGPT無限 ∞
-// @version             2023.4.28.6
+// @name:ar             ChatGPT إنفينيتي ∞
+// @name:bg             ChatGPT Безкрайност ∞
+// @name:bn             চ্যাটজিপিটি ইনফিনিটি ∞
+// @name:cs             ChatGPT Nekonečno ∞
+// @name:da             ChatGPT Uendelighed ∞
+// @name:de             ChatGPT Unendlichkeit ∞
+// @name:el             ChatGPT Άπειρο ∞
+// @name:eo             ChatGPT Senfina ∞
+// @name:es             ChatGPT Infinito ∞
+// @name:fi             ChatGPT Äärettömyys ∞
+// @name:fr             ChatGPT Infini ∞
+// @name:fr-CA          ChatGPT Infini ∞
+// @name:gu             ચેટજીપીટી અનંત ∞
+// @name:hi             चैटजीपीटी अनंत ∞
+// @name:hu             ChatGPT Végtelen ∞
+// @name:in             ChatGPT Tak Terhingga ∞
+// @name:it             ChatGPT Infinito ∞
+// @name:iw             צ'אטגפט אינסוף ∞
+// @name:ja             ChatGPT 無限大 ∞
+// @name:ka             ChatGPT უსასი ∞
+// @name:kn             ಚಾಟ್ಜಿಪಿಟಿ ಅನಂತ ∞
+// @name:ko             ChatGPT 무한대 ∞
+// @name:ku             ChatGPT Bêdawî ∞
+// @name:ml             ചാറ്റ്‌ജിപിടി അനന്തതയും ∞
+// @name:mr             चॅटजीपीटी अनंत ∞
+// @name:ms             ChatGPT Tak Terhingga ∞
+// @name:my             ChatGPT အများကြီး ∞
+// @name:nl             ChatGPT Oneindigheid ∞
+// @name:no             ChatGPT Uendelighet ∞
+// @name:or             ଚାଟଜିପିଟି ଅନନ୍ତ ∞
+// @name:pa             ਚੈਟਜੀਪੀਟੀ ਅਨੰਤ ∞
+// @name:pl             ChatGPT Nieskończoność ∞
+// @name:pt             ChatGPT Infinito ∞
+// @name:pt-BR          ChatGPT Infinito ∞
+// @name:ro             ChatGPT Infinit ∞
+// @name:ru             ChatGPT Бесконечность ∞
+// @name:sk             ChatGPT Nekonečno ∞
+// @name:sr             ChatGPT Бесконачност ∞
+// @name:ta             சாட் ஜிபிடி முழுமையான ∞
+// @name:th             ChatGPT ความไม่สิ้นสุด ∞
+// @name:tr             ChatGPT Sonsuzluk ∞
+// @name:uk             ChatGPT Нескінченність ∞
+// @name:ur             چیٹ جی پی ٹی بے انتہا ∞
+// @name:vi             ChatGPT Vô Hạn ∞
+// @name:zh-CN          ChatGPT 无限 ∞
+// @name:zh-HK          ChatGPT 無限 ∞
+// @name:zh-SG          ChatGPT 无限 ∞
+// @name:zh-TW          ChatGPT 無限 ∞
+// @version             2023.4.28.7
 // @description         Generate endless answers from all-knowing ChatGPT (in any language!)
+// @description:ar      احصل على إجابات لا حصر لها من ChatGPT الذي يعرف الجميع (بأي لغة!)
+// @description:bg      Генерирайте безкрайни отговори от всезнаещия ChatGPT (на всеки език!)
+// @description:bn      সর্বজনবিদিত ChatGPT থেকে অবিরাম উত্তর তৈরি করুন (যেকোন ভাষায়!)
+// @description:cs      Generujte nekonečné odpovědi ze vševědoucího ChatGPT (v jakémkoli jazyce!)
+// @description:da      Generer endeløse svar fra alvidende ChatGPT (på ethvert sprog!)
+// @description:de      Generieren Sie endlose Antworten aus dem allwissenden ChatGPT (in jeder Sprache!)
+// @description:el      Δημιουργήστε ατελείωτες απαντήσεις από το παντογνώστη ChatGPT (σε οποιαδήποτε γλώσσα!)
+// @description:eo      Generu senfinajn respondojn de ĉioscia ChatGPT (en iu ajn lingvo!)
+// @description:es      Genera infinitas respuestas desde ChatGPT que todo lo sabe (¡en cualquier idioma!)
+// @description:fi      Luo loputtomasti vastauksia kaikkitietävästä ChatGPT:stä (millä tahansa kielellä!)
+// @description:fr      Générez des réponses sans fin à partir de ChatGPT qui sait tout (dans n'importe quelle langue!)
+// @description:fr-CA   Générez des réponses sans fin à partir de ChatGPT qui sait tout (dans n'importe quelle langue!)
+// @description:gu      સર્વજ્ઞાની ChatGPT (કોઈપણ ભાષામાં!) તરફથી અનંત જવાબો બનાવો
+// @description:hi      सभी जानकार ChatGPT से अंतहीन उत्तर उत्पन्न करें (किसी भी भाषा में!)
+// @description:hu      Generáljon végtelen válaszokat a mindent tudó ChatGPT segítségével (bármilyen nyelven!)
+// @description:in      Hasilkan jawaban tanpa akhir dari ChatGPT yang serba tahu (dalam bahasa apa pun!)
+// @description:it      Genera risposte infinite da ChatGPT onnisciente (in qualsiasi lingua!)
+// @description:iw      הפק תשובות אינסופיות מ-ChatGPT היודע הכל (בכל שפה!)
+// @description:ja      すべてを知っている ChatGPT から無限の回答を生成します (任意の言語で!)
+// @description:ka      შექმენით გაუთავებელი პასუხები ყოვლისმომცველი ChatGPT-დან (ნებისმიერ ენაზე!)
+// @description:kn      すべてを知っている ChatGPT から無限の回答を生成します (任意の言語で!)
+// @description:ko      모든 것을 아는 ChatGPT에서 끝없는 답변 생성(모든 언어로!)
+// @description:ku      Bersivên bêdawî ji ChatGPT-a-zana (bi her zimanî!)
+// @description:ml      എല്ലാം അറിയുന്ന ChatGPT-ൽ നിന്ന് അനന്തമായ ഉത്തരങ്ങൾ സൃഷ്ടിക്കുക (ഏത് ഭാഷയിലും!)
+// @description:mr      सर्वज्ञात ChatGPT (कोणत्याही भाषेत!) कडून अंतहीन उत्तरे व्युत्पन्न करा.
+// @description:ms      Hasilkan jawapan yang tidak berkesudahan daripada ChatGPT yang serba tahu (dalam mana-mana bahasa!)
+// @description:my      အားလုံးသိသော ChatGPT (မည်သည့်ဘာသာစကားဖြင့်မဆို) မှ အဆုံးမဲ့အဖြေများကို ဖန်တီးပါ။
+// @description:nl      Genereer eindeloze antwoorden van alwetende ChatGPT (in elke taal!)
+// @description:no      Generer endeløse svar fra allvitende ChatGPT (på hvilket som helst språk!)
+// @description:or      ସମସ୍ତ ଜାଣିବା ChatGPT ରୁ ଅସୀମ ଉତ୍ତର ସୃଷ୍ଟି କରନ୍ତୁ (ଯେକ language ଣସି ଭାଷାରେ!)
+// @description:pa      ਸਭ-ਜਾਣਨ ਵਾਲੇ ਚੈਟਜੀਪੀਟੀ (ਕਿਸੇ ਵੀ ਭਾਸ਼ਾ ਵਿੱਚ!) ਤੋਂ ਬੇਅੰਤ ਜਵਾਬ ਤਿਆਰ ਕਰੋ
+// @description:pl      Generuj niekończące się odpowiedzi z wszechwiedzącego ChatGPT (w dowolnym języku!)
+// @description:pt      Gere respostas infinitas do onisciente ChatGPT (em qualquer idioma!)
+// @description:pt-BR   Gere respostas infinitas do onisciente ChatGPT (em qualquer idioma!)
+// @description:ro      Generați răspunsuri nesfârșite de la atotștiutorul ChatGPT (în orice limbă!)
+// @description:ru      Создавайте бесконечные ответы от всезнающего ChatGPT (на любом языке!)
+// @description:sk      Generujte nekonečné odpovede od vševediaceho ChatGPT (v akomkoľvek jazyku!)
+// @description:sr      Генеришите бескрајне одговоре од свезнајућег ЦхатГПТ-а (на било ком језику!)
+// @description:sv      Generera oändliga svar från allvetande ChatGPT (på vilket språk som helst!)
+// @description:ta      அனைத்தையும் அறிந்த ChatGPT இலிருந்து முடிவற்ற பதில்களை உருவாக்கவும் (எந்த மொழியிலும்!)
+// @description:th      สร้างคำตอบไม่รู้จบจาก ChatGPT ที่รอบรู้ (ในภาษาใดก็ได้!)
+// @description:tr      Her şeyi bilen ChatGPT'den sonsuz cevaplar oluşturun (herhangi bir dilde!)
+// @description:uk      Генеруйте нескінченні відповіді від всезнаючого ChatGPT (будь-якою мовою!)
+// @description:ur      تمام جاننے والے ChatGPT سے لامتناہی جوابات تیار کریں (کسی بھی زبان میں!)
+// @description:vi      Tạo vô số câu trả lời từ ChatGPT toàn năng (bằng bất kỳ ngôn ngữ nào!)
 // @description:zh-CN   从无所不知的 ChatGPT 生成无穷无尽的答案 (用任何语言!)
 // @description:zh-HK   從無所不知的 ChatGPT 生成無窮無盡的答案 (用任何語言!)
 // @description:zh-SG   从无所不知的 ChatGPT 生成无穷无尽的答案 (用任何语言!)
