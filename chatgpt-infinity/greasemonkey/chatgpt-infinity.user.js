f8b96b37b662f951a8b3f49527108c8b8a5058eb
adamlui <adamlui@users.noreply.github.com>
2023-04-29 03:18:09 +0000

    Added Vivaldi compatibility meta ↞ [auto-sync from `adamlui/chatgpt-infinity`]

--- chatgpt-infinity/greasemonkey/chatgpt-infinity.user.js
+++ chatgpt-infinity/greasemonkey/chatgpt-infinity.user.js
@@ -48,7 +48,7 @@
 // @name:zh-HK          ChatGPT 無限 ∞
 // @name:zh-SG          ChatGPT 无限 ∞
 // @name:zh-TW          ChatGPT 無限 ∞
-// @version             2023.4.28.16
+// @version             2023.4.28.17
 // @description         Generate endless answers from all-knowing ChatGPT (in any language!)
 // @description:ar      احصل على إجابات لا حصر لها من ChatGPT الذي يعرف الجميع (بأي لغة!)
 // @description:bg      Генерирайте безкрайни отговори от всезнаещия ChatGPT (на всеки език!)
@@ -110,6 +110,7 @@
 // @compatible          edge
 // @compatible          opera
 // @compatible          brave
+// @compatible          vivaldi
 // @require             https://cdn.jsdelivr.net/gh/chatgptjs/chatgpt.js@aa37089ffb0462f72d5020d0c7d1c78937d42a07/dist/chatgpt-1.6.1.min.js
 // @grant               GM_setValue
 // @grant               GM_getValue
