20d8b63824a5929d94bfeaa6ab7f0f79f83af19c
adamlui <adamlui@users.noreply.github.com>
2023-05-03 04:29:36 +0000

    Converted double-quotes to single-quotes ↞ [auto-sync from `adamlui/userscripts`]

--- autopass-cloudflare-captcha/autopass-cloudflare-captcha.user.js
+++ autopass-cloudflare-captcha/autopass-cloudflare-captcha.user.js
@@ -4,7 +4,7 @@
 // @match        https://challenges.cloudflare.com/*
 // @match        https://chat.openai.com/*
 // @match        https://freegpt.one/*
-// @version      2023.4.27
+// @version      2023.5.2
 // @grant        none
 // @run-at       document-start
 // @author       github.com @XiaoYingYo
@@ -16,34 +16,34 @@
 // @description 2023-3-6 13:25:06
 // ==/UserScript==
 
-global_module = window["global_module"];
-var $ = window["$$$"];
+global_module = window['global_module'];
+var $ = window['$$$'];
 
 async function VerifyYouAreHuman_01() {
-    let dom = await global_module.waitForElement("input[value='Verify you are human'][type='button']", null, null, 200, -1);
+    let dom = await global_module.waitForElement('input[value="Verify you are human"][type="button"]', null, null, 200, -1);
     global_module.clickElement($(dom).eq(0)[0]);
 }
 
 async function VerifyYouAreHuman_02() {
-    let dom = await global_module.waitForElement("input[type='checkbox']", null, null, 200, -1);
+    let dom = await global_module.waitForElement('input[type="checkbox"]', null, null, 200, -1);
     global_module.clickElement($(dom).eq(0)[0]);
-    dom = await global_module.waitForElement("span[class='mark']", null, null, 200, -1);
+    dom = await global_module.waitForElement('span[class="mark"]', null, null, 200, -1);
     global_module.clickElement($(dom).eq(0)[0]);
 }
 
 async function VerifyYouAreHuman_03() {
-    let dom = await global_module.waitForElement("input[value='Verify you are human'][type='button']", null, null, 200, -1);
+    let dom = await global_module.waitForElement('input[value="Verify you are human"][type="button"]', null, null, 200, -1);
     global_module.clickElement($(dom).eq(0)[0]);
 }
 
 async function main() {
-    let ray_id = $("div[class='ray-id']");
-    let hrefdom = $("a[href*='cloudflare.com'][target='_blank']");
+    let ray_id = $('div[class="ray-id"]');
+    let hrefdom = $('a[href*="cloudflare.com"][target="_blank"]');
     if (ray_id.length > 0 && hrefdom.length > 0) {
         VerifyYouAreHuman_01();
         return;
     }
-    if (window.location.host == "challenges.cloudflare.com" && $("div[id='success']").length > 0 && $("div[id='fail']").length > 0 && $("div[id='expired']").length > 0) {
+    if (window.location.host == 'challenges.cloudflare.com' && $('div[id="success"]').length > 0 && $('div[id="fail"]').length > 0 && $('div[id="expired"]').length > 0) {
         VerifyYouAreHuman_02();
         return;
     }
