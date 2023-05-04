8a35826196d015602deddbb5fc013d465eb3d380
adamlui <adamlui@users.noreply.github.com>
2023-05-04 05:29:05 +0000

    Auto-sync test A ↞ [auto-sync from `adamlui/userscripts`]

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,16 +15,10 @@ module.exports  = {
         'no-async-promise-executor': 'off', // allow promise executor functions to be async (to accomodate await lines)
         'no-constant-condition': 'off', // allow constant conditions
         'no-empty': 'off', // allow empty blocks
-        'no-useless-escape': 'off' // allow all escape chars because ESLint sucks at detecting useless ones
+        'no-useless-escape': 'off' // allow all escape chars cause ESLint sucks at detecting truly useless ones
     },
     'globals': { '$': true, 'chatgpt': true, 'chrome': true, 'global_module': true, 'GM_cookie': true },
-    'parserOptions': {
-        'ecmaVersion': 2022,
-        'sourceType': 'script'
-    },
-    'overrides': [{
-        'files': ['**/lib/**.js'],
-        'parserOptions': { 'sourceType': 'module' }
-    }],
+    'parserOptions': { 'ecmaVersion': 2022, 'sourceType': 'script' },
+    'overrides': [{ 'files': ['**/lib*/**.js'], 'parserOptions': { 'sourceType': 'module' }}],
     'env': { 'browser': true, 'node': true, 'es6': true, 'greasemonkey': true }
 };
