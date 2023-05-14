e127cbef732b1195f7709c8ca2375b2f533192f6
adamlui <adamlui@users.noreply.github.com>
2023-05-14 02:54:47 +0000

    Changed message host dir ↞ [auto-sync from `adamlui/chatgpt-auto-refresh`]

--- chatgpt-auto-refresh/greasemonkey/chatgpt-auto-refresh.user.js
+++ chatgpt-auto-refresh/greasemonkey/chatgpt-auto-refresh.user.js
@@ -48,7 +48,7 @@
 // @name:zh-HK          ChatGPT 自動刷新 ↻
 // @name:zh-SG          ChatGPT 自动刷新 ↻
 // @name:zh-TW          ChatGPT 自動刷新 ↻
-// @version             2023.5.13.2
+// @version             2023.5.13.3
 // @description         *SAFELY* keeps ChatGPT sessions fresh, eliminating constant network errors + Cloudflare checks (all from the background!)
 // @author              Adam Lui
 // @namespace           https://github.com/adamlui
@@ -141,7 +141,7 @@
 
     // Define messages
     var msgsLoaded = new Promise(resolve => {
-        var msgHostDir = 'https://raw.githubusercontent.com/adamlui/chatgpt-auto-refresh/main/chrome/extension/_locales/'
+        var msgHostDir = 'https://raw.githubusercontent.com/adamlui/chatgpt-auto-refresh/main/greasemonkey/_locales/'
         var msgLocaleDir = ( config.userLanguage ? config.userLanguage.replace('-', '_') : 'en' ) + '/'
         var msgHref = msgHostDir + msgLocaleDir + 'messages.json' // build src link
         var msgXHRtries = 0
