e002a7944b9c609b53c078f5c5ad6c14185e811c
adamlui <adamlui@users.noreply.github.com>
2023-05-21 18:14:54 +0000

    Removed horizontal sep ↞ [auto-sync from `adamlui/chatgpt-auto-continue`]

---
+++ chatgpt-auto-continue/greasemonkey/chatgpt-auto-continue.user.js
@@ -0,0 +1,143 @@
+// ==UserScript==
+// @name                ChatGPT Auto-Continue
+// @version             2023.5.21.2
+// @description         Auto-continue multiple ChatGPT responses
+// @author              Adam Lui
+// @namespace           https://github.com/adamlui
+// @license             MIT
+// @match               https://chat.openai.com/*
+// @icon                https://www.google.com/s2/favicons?sz=64&domain=openai.com
+// @require             https://cdn.jsdelivr.net/gh/chatgptjs/chatgpt.js@744003f7589935a1027840a8be65239696efd05a/dist/chatgpt-1.7.1.min.js
+// @connect             raw.githubusercontent.com
+// @grant               none
+// @noframes
+// @homepageURL         https://github.com/adamlui/chatgpt-auto-continue
+// @supportURL          https://github.com/adamlui/chatgpt-auto-continue/issues
+// ==/UserScript==
+
+(async () => {
+
+    // Initialize settings
+    var appSymbol = '≫', configKeyPrefix = 'chatGPTactn_'
+    var config = { userLanguage: navigator.languages[0] || navigator.language || '' }
+    loadSetting('lastCheckTime', 'notifHidden', 'skipNextUpdate', 'skippedVer')
+
+    // Define messages
+    var msgsLoaded = new Promise(resolve => {
+        var msgHostDir = 'https://raw.githubusercontent.com/adamlui/chatgpt-auto-continue/main/greasemonkey/_locales/'
+        var msgLocaleDir = ( config.userLanguage ? config.userLanguage.replace('-', '_') : 'en' ) + '/'
+        var msgHref = msgHostDir + msgLocaleDir + 'messages.json' // build src link
+        var msgXHRtries = 0
+        GM.xmlHttpRequest({ method: 'GET', url: msgHref, onload: onLoad })
+        function onLoad(response) {
+            try { // to return localized messages.json
+                var messages = new Proxy(JSON.parse(response.responseText), {
+                    get(target, prop) { // remove need to ref nested keys
+                        if (typeof target[prop] === 'object' && target[prop] !== null && 'message' in target[prop]) {
+                            return target[prop].message
+                }}}) ; resolve(messages)
+            } catch (error) { // if 404
+                msgXHRtries++ ; if (msgXHRtries == 3) return // try up to 3X (original/region-stripped/EN) only
+                msgHref = config.userLanguage.includes('-') && msgXHRtries == 1 ? // if regional lang on 1st try...
+                    msgHref.replace(/(.*)_.*(\/.*)/, '$1$2') // ...strip region before retrying
+                        : ( msgHostDir + 'en/messages.json' ) // else use default English messages
+                GM.xmlHttpRequest({ method: 'GET', url: msgHref, onload: onLoad })
+            }
+        }
+    }) ; var messages = await msgsLoaded
+
+    await chatgpt.isLoaded()
+
+    // Check for update (1x/24h)
+    if (!config.lastCheckTime || Date.now() - config.lastCheckTime > 86400000) { // if 24h since last check
+
+        // Fetch .js to examine meta
+        var updateURL = GM_info.scriptUpdateURL || GM_info.script.updateURL || GM_info.script.downloadURL
+        fetch(updateURL + '?t=' + Date.now(), { cache: 'no-cache' })
+            .then((response) => { response.text().then((data) => {
+                saveSetting('lastCheckTime', Date.now())
+
+                // Compare versions
+                var currentVer = GM_info.script.version
+                var latestVer = data.match(/@version +(.*)/)[1]
+                if (config.skipNextUpdate && latestVer === config.skippedVer) return // exit comparison if past alert hidden
+                for (var i = 0 ; i < 4 ; i++) { // loop thru subver's
+                    if (parseInt(latestVer.split('.')[i] || 0) > parseInt(currentVer.split('.')[i] || 0)) { // if outdated
+
+                        // Alert to update
+                        var updateAlertID = chatgpt.alert(`${ appSymbol } ${ messages.alert_updateAvail }! 🚀`,
+                            `${ messages.alert_newerVer } ${ messages.appName } (v${ currentVer }) ${ messages.alert_isAvail }!`
+                                + `<a target="_blank" href="https://github.com/adamlui/chatgpt-auto-continue/commits/main/greasemonkey/chatgpt-auto-continue.user.js" style="font-size: 0.7rem ; position: relative ; left: 8px">${ messages.link_viewChanges }</a>`,
+                            function update() { // button
+                                saveSetting('skipNextUpdate', false) // reset hidden alert setting
+                                window.open(( updateURL.includes('.meta.') ? GM_info.script.downloadURL : updateURL )
+                                    + '?t=' + Date.now(), '_blank') },
+                            function dontShowAgainUntilNextUpdate() { // checkbox
+                                saveSetting('skipNextUpdate', !config.skipNextUpdate)
+                                saveSetting('skippedVer', config.skipNextUpdate ? latestVer : false) }
+                        )
+
+                        // Internationalize button/checkbox labels if needed
+                        if (!config.userLanguage.startsWith('en')) {
+                            var updateAlert = document.querySelector(`[id="${ updateAlertID }"]`)
+                            updateAlert.querySelector('label').textContent = ( // checkbox label
+                                `${ messages.alert_dontShowAgain } ${ messages.alert_untilNextVer }`)
+                            updateAlert.querySelectorAll('button')[1].textContent = messages.buttonLabel_update
+                            updateAlert.querySelectorAll('button')[0].textContent = messages.buttonLabel_dismiss
+                        }
+
+                        return
+    }}})})}
+
+    // Init/register menu
+    var menuIDs = [], state = { symbol: ['✔️', '❌'], word: ['ON', 'OFF'] } // initialize menu vars
+    registerMenu() // create browser toolbar menu
+
+    // Observe DOM for need to continue
+    const continueObserver = new MutationObserver((mutationsList) => {
+        for (const mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                for (const addedNode of mutation.addedNodes) {
+                    if (addedNode.nodeName === 'BUTTON' && addedNode.innerHTML.includes('Continue generating')) {
+                        addedNode.click()
+    }}}}})
+    continueObserver.observe(document, { childList: true, subtree: true })
+
+    // Notify of status on load
+    if (!config.notifHidden && document.title === 'New chat') {
+        chatgpt.notify(appSymbol + ' ' + messages.mode_autoContinue + ': ON',
+            '', '', chatgpt.isDarkMode() ? '' : 'shadow' )}
+
+    // Define SCRIPT functions
+
+    function registerMenu() {
+        menuIDs = [] // empty to store newly registered cmds for removal while preserving order
+        var stateSeparator = getUserscriptManager() === 'Tampermonkey' ? ' — ' : ': '
+
+        // Add command to hide/show notifications on load
+        var mnLabel = state.symbol[+config.notifHidden] + ' ' + messages.menuLabel_modeNotifs
+                    + stateSeparator + state.word[+config.notifHidden]
+        menuIDs.push(GM_registerMenuCommand(mnLabel, function() {
+            saveSetting('notifHidden', !config.notifHidden)
+            chatgpt.notify(appSymbol + ' ' + messages.menuLabel_modeNotifs + ': ' + state.word[+config.notifHidden],
+                '', '', chatgpt.isDarkMode() ? '' : 'shadow')
+            for (var i = 0 ; i < menuIDs.length ; i++) GM_unregisterMenuCommand(menuIDs[i]) // remove all cmd's
+            registerMenu() // serve fresh one
+        }))
+    }
+
+    function getUserscriptManager() {
+        try { return GM_info.scriptHandler } catch (error) { return 'other' }}
+
+    function loadSetting() {
+        var keys = [].slice.call(arguments)
+        keys.forEach(function(key) {
+            config[key] = GM_getValue(configKeyPrefix + key, false)
+    })}
+
+    function saveSetting(key, value) {
+        GM_setValue(configKeyPrefix + key, value) // save to browser
+        config[key] = value // and memory
+    }
+
+})()
---
+++ chatgpt-auto-continue/greasemonkey/docs/README.md
@@ -0,0 +1,85 @@
+# <picture><source media="(prefers-color-scheme: dark)" srcset="https://i.imgur.com/RduASbD.png"><img width=23 src="https://raw.githubusercontent.com/adamlui/userscripts/master/chatgpt/media/icons/openai-favicon64.png"></picture> ChatGPT Auto-Continue ⏩ &nbsp;[![Tweet](https://img.shields.io/twitter/url/http/shields.io.svg?style=social)](https://twitter.com/intent/tweet?text=Automatically%20continue%20the%20conversation%20from%20multiple%20ChatGPT%20responses%20w%2F%20this%20plug-in%21T&url=https://chatgptevo.com/autocontinue&hashtags=javascript,openai,chatgpt)
+
+[![license](https://img.shields.io/badge/License-MIT-green.svg)](../LICENSE.md)
+[![code-quality](https://img.shields.io/codefactor/grade/github/adamlui/chatgpt-auto-continue?label=Code%20Quality)](https://www.codefactor.io/repository/github/adamlui/chatgpt-auto-continue)
+<a alt="chatgpt.js" href="https://chatgpt.js.org"><img height=20 src="https://i.imgur.com/AwQqCmB.png"></a>
+
+### Automatically continue generating ChatGPT responses when conversation cuts-off.
+
+<a href="#"><img alt="Install this script" src="https://github.com/adamlui/userscripts/raw/master/media/images/buttons/install-button.svg"></a><a href="#installation"><img alt="How to install" title="How to install" src="https://github.com/adamlui/userscripts/raw/master/media/images/buttons/help-button.svg"></a>
+<br>
+[Discuss](https://chatgptevo.com/autocontinue/discussions) /
+[Review](#) /
+[Contact author](https://github.com/adamlui)
+
+## Installation
+
+1. Install Tampermonkey ([Chrome](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo), [Firefox](https://addons.mozilla.org/firefox/addon/tampermonkey/), [Edge](https://microsoftedge.microsoft.com/addons/detail/tampermonkey/iikmkjmpaadaobahmlepeloendndfphd), [Opera](https://addons.opera.com/extensions/details/tampermonkey-beta/)) or Violentmonkey ([Chrome](https://chrome.google.com/webstore/detail/violent-monkey/jinjaccalgkegednnccohejagnlnfdag), [Firefox](https://addons.mozilla.org/firefox/addon/violentmonkey/), [Edge](https://microsoftedge.microsoft.com/addons/detail/violentmonkey/eeagobfjdenkkddmbclomhiblgggliao))
+
+2. Install [ChatGPT Auto-Continue](#) (will load in userscript manager installed above)
+
+3. Visit [chat.openai.com](https://chat.openai.com) as normal, and multi-reply conversations will automatically continue generating when cut-off!
+
+## Dependencies
+
+<h6>
+<div align="center">
+<br />
+
+<a href="https://chatgpt.js.org">
+<picture>
+    <source media="(prefers-color-scheme: dark)" srcset="https://raw.githubusercontent.com/chatgptjs/chatgpt.js/main/media/images/chatgpt.js-logo-dark-mode-5995x619.png">
+    <img width=546 alt="chatgpt.js" src="https://raw.githubusercontent.com/chatgptjs/chatgpt.js/main/media/images/chatgpt.js-logo-light-mode-5995x619.png">
+</picture></a>
+<br /><br />
+
+ChatGPT Auto-Continue relies on code from the powerful [chatgpt.js](https://github.com/chatgptjs/chatgpt.js) library (c) 2023 KudoAI & contributors under the MIT license.
+
+</div>
+</h6>
+
+<a href="#"><img height-40 alt="Install this script" src="https://github.com/adamlui/userscripts/raw/master/media/images/buttons/install-button.svg"></a><a href="#installation"><img height-40 alt="How to install" title="How to install" src="https://github.com/adamlui/userscripts/raw/master/media/images/buttons/help-button.svg"></a>
+<br>
+[Discuss](https://chatgptevo.com/autocontinue/discussions) /
+[Leave a review](#) /
+[More ChatGPT userscripts](https://github.com/adamlui/userscripts/tree/master/chatgpt)
+
+## Author links
+
+[![Follow @adamlui on GitHub](https://img.shields.io/github/followers/adamlui?label=Follow%20%40adamlui&style=social "GitHub")](https://github.com/adamlui)
+[![Follow @adamluitube on YouTube](https://img.shields.io/youtube/channel/subscribers/UCgBMqK7SRL5R__3qM-YAcSg?label=Follow%20%40adamluitube&style=social)](https://www.youtube.com/AdamLuiTube?sub_confirmation=1)
+<a href="https://elonsucks.org/@adam" target="_blank"><img align="bottom" src="https://img.shields.io/mastodon/follow/109387703022229926?domain=https%3A%2F%2Felonsucks.org&style=social" alt="Follow @adam on Mastodon" title="Mastodon">
+[![Follow @adamllui on Twitter](https://img.shields.io/twitter/follow/adamllui?style=social)](https://twitter.com/adamllui)
+
+## More ChatGPT apps
+
+For even more epic ChatGPT apps, go here: https://github.com/adamlui/chatgpt-apps
+<br><br>
+
+## Related scripts
+
+### <picture><source media="(prefers-color-scheme: dark)" srcset="https://i.imgur.com/RduASbD.png"><img width=16 src="https://raw.githubusercontent.com/adamlui/chatgpt-userscripts/main/media/icons/openai-favicon64.png"></picture> [ChatGPT Auto Refresh ↻](https://chatgptevo.com/autorefresh/github) <a href="https://github.com/awesome-scripts/awesome-userscripts#chatgpt"><img src="https://awesome.re/mentioned-badge.svg" alt="Mentioned in Awesome Userscripts" style="margin:0 0 -2px 5px"></a>
+
+Keeps ChatGPT sessions fresh to eliminate network errors + Cloudflare checks.
+<br>[Install](https://greasyfork.org/scripts/462422-chatgpt-auto-refresh) / 
+[Readme](https://github.com/adamlui/chatgpt-auto-refresh#readme) / 
+[Discuss](https://chatgptevo.com/autorefresh/discuss)
+
+### <picture><source media="(prefers-color-scheme: dark)" srcset="https://i.imgur.com/RduASbD.png"><img width=16 src="https://raw.githubusercontent.com/adamlui/chatgpt-userscripts/main/media/icons/openai-favicon64.png"></picture> [ChatGPT Infinity ∞](https://chatgptevo.com/infinity/github) <a href="https://github.com/awesome-scripts/awesome-userscripts#chatgpt"><img src="https://awesome.re/mentioned-badge.svg" alt="Mentioned in Awesome Userscripts" style="margin:0 0 -2px 4px"></a>
+
+Generate endless answers from all-knowing ChatGPT (in any language!)
+<br>[Install](https://greasyfork.org/scripts/465051-chatgpt-infinity) / 
+[Readme](https://github.com/adamlui/chatgpt-infinity#readme) / 
+[Discuss](https://chatgptevo.com/infinity/discussions)
+
+### <img width=17 style="margin-bottom:-1px" src="https://raw.githubusercontent.com/adamlui/chatgpt-widescreen/main/media/images/icons/widescreen-robot-emoji/icon32.png"> [ChatGPT Widescreen Mode](https://chatgptevo.com/widescreen/github) <img src="https://raw.githubusercontent.com/adamlui/chatgpt-widescreen/main/media/images/badges/product-hunt/product-of-the-week-2-larger-centered-rounded-light.svg" alt="ChatGPT&#0032;Widescreen&#0032;Mode - Add&#0032;widescreen&#0032;&#0043;&#0032;full&#0032;window&#0032;modes&#0032;to&#0032;ChatGPT | Product Hunt" style="width: auto; height: 24px; margin:0 0 -4px 5px;" width="auto" height="24" />
+
+Adds Widescreen + Full-Window modes to ChatGPT for reduced scrolling.
+<br>[Install](https://github.com/adamlui/chatgpt-widescreen#installation) / 
+[Readme](https://github.com/adamlui/chatgpt-widescreen#readme) / 
+[Discuss](https://chatgptevo.com/widescreen/discuss)
+
+#
+  
+<a href="https://github.com/adamlui/userscripts">**More userscripts**</a> / 
+<a href="#-chatgpt-auto-continue--">Back to top ↑</a>
