a94ace5e00f84cc22ae37111108514eba473e450
adamlui <adamlui@users.noreply.github.com>
2023-05-31 04:31:36 +0000

    Changed `configKeyPrefix` to `configPrefix` ↞ [auto-sync from `adamlui/chatgpt-widescreen`]

--- chatgpt-widescreen/chrome/extension/lib/settings-utils.js
+++ chatgpt-widescreen/chrome/extension/lib/settings-utils.js
@@ -1,4 +1,4 @@
-var configKeyPrefix = 'chatGPTws_'
+var configPrefix = 'chatGPTws_'
 var config = {}
 var settings = {
 
@@ -7,13 +7,13 @@ var settings = {
             Array.isArray(arguments[0]) ? arguments[0] : Array.prototype.slice.call(arguments))
         return Promise.all(keys.map(function(key) { // resolve promise when all keys load
             return new Promise(function(resolve) { // resolve promise when single key value loads
-                chrome.storage.local.get(configKeyPrefix + key, function(result) { // load from Chrome
-                    config[key] = result[configKeyPrefix + key] === true // save to memory
+                chrome.storage.local.get(configPrefix + key, function(result) { // load from Chrome
+                    config[key] = result[configPrefix + key] === true // save to memory
                     resolve()
     })})}))},
 
     save: function(key, value) {
-        var obj = {} ; obj[configKeyPrefix + key] = value
+        var obj = {} ; obj[configPrefix + key] = value
         chrome.storage.local.set(obj) // save to Chrome
         config[key] = value // save to memory
     }
