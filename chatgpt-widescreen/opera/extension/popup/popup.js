e8edbb93c1a542f3c55931479d290977cfc7b019
adamlui <adamlui@users.noreply.github.com>
2023-06-13 12:38:53 +0000

    Added Taller Chatbox + update-check click-listeners...
    ± Updated toggle init's `settings.load()` args + states restored
    ± Renamed functions
    ± Shortened syntax
    - Removed no-longer-needed position arg from `notify()`
    ± Replaced `type` w/ `action` in Chrome msg sending ↞ [auto-sync from `adamlui/chatgpt-widescreen`]

--- chatgpt-widescreen/opera/extension/popup/popup.js
+++ chatgpt-widescreen/opera/extension/popup/popup.js
@@ -1,13 +1,14 @@
-(async function() {
+(async () => {
 
     // Import settings-utils.js
     var { config, settings } = await import(chrome.runtime.getURL('lib/settings-utils.js'))
 
     // Initialize popup toggles
-    settings.load('wideScreen', 'fullWindow', 'notifHidden', 'fullerWindow', 'extensionDisabled')
+    settings.load('fullerWindow', 'tcbDisabled', 'notifHidden', 'extensionDisabled')
         .then(function() { // restore extension/toggle states
             mainToggle.checked = !config.extensionDisabled
             fullerWinToggle.checked = config.fullerWindow
+            tallerChatboxToggle.checked = !config.tcbDisabled
             notificationsToggle.checked = !config.notifHidden
             updateGreyness()
         })
@@ -21,18 +22,17 @@
     var mainToggle = toggles[0]
     mainToggle.addEventListener('change', function() {    
         settings.save('extensionDisabled', !this.checked)
-        toggleExtension() ; updateGreyness()
+        syncExtension() ; updateGreyness()
     })
 
     // Add Fuller Windows toggle label click-listeners
     var fullerWinToggle = toggles[1]
     var fullerWinLabel = fullerWinToggle.parentNode.parentNode
-    fullerWinToggle.addEventListener('change', function toggleFullerWin() {
+    fullerWinToggle.addEventListener('change', () => {
         settings.save('fullerWindow', !config.fullerWindow)
-        settings.load('notifHidden').then(function() {
+        settings.load('notifHidden').then(() => {
             if (!config.notifHidden) { // show mode notification if enabled
-                notify(chrome.i18n.getMessage('menuLabel_fullerWins') + ' '
-                         + ( config.fullerWindow ? 'ON' : 'OFF' ), 'bottom-right')
+                notify(chrome.i18n.getMessage('menuLabel_fullerWins') + ' ' + ( config.fullerWindow ? 'ON' : 'OFF' ))
         }})
     })
     fullerWinLabel.addEventListener('click', (event) => {
@@ -40,18 +40,54 @@
             fullerWinToggle.click() 
     })
 
+    // Add Taller Chatbox toggle label click-listeners
+    var tallerChatboxToggle = toggles[2]
+    var tallerChatboxLabel = tallerChatboxToggle.parentNode.parentNode
+    tallerChatboxToggle.addEventListener('change', () => {
+        settings.save('tcbDisabled', !config.tcbDisabled)
+        syncExtension()
+        settings.load('notifHidden').then(() => {
+            if (!config.notifHidden) { // show mode notification if enabled
+                notify(chrome.i18n.getMessage('menuLabel_tallerChatbox') + ' ' + (config.tcbDisabled ? 'OFF' : 'ON'))
+        }})
+    })
+    tallerChatboxLabel.addEventListener('click', (event) => {
+        if ([tallerChatboxLabel, document.querySelector('[data-locale*="tallerChatbox"]')].includes(event.target))
+            tallerChatboxToggle.click() 
+    })
+
     // Add notifications toggle label click-listeners
-    var notificationsToggle = toggles[2]
+    var notificationsToggle = toggles[3]
     var notificationsLabel = notificationsToggle.parentNode.parentNode
     notificationsToggle.addEventListener('change', function toggleNotifications() {
         settings.save('notifHidden', !config.notifHidden)
-        notify(chrome.i18n.getMessage('menuLabel_modeNotifs') + ' '
-                 + ( config.notifHidden ? 'OFF' : 'ON' ), 'bottom-right' )
+        notify(chrome.i18n.getMessage('menuLabel_modeNotifs') + ' ' + ( config.notifHidden ? 'OFF' : 'ON' ))
     })
     notificationsLabel.addEventListener('click', (event) => {
         if ([notificationsLabel, document.querySelector('[data-locale*="modeNotifs"]')].includes(event.target))
             notificationsToggle.click()
     })
+
+    // Add update-check span click-listener
+    var updateSpan = document.querySelector('span[title*="update" i]')
+    updateSpan.addEventListener('click', (event) => {
+        window.close() // popup
+        chrome.runtime.requestUpdateCheck((status, version) => {
+            if (status === 'update_available') {
+                alert(`${ chrome.i18n.getMessage('alert_updateAvail') }!`,
+                    chrome.i18n.getMessage('alert_newerVer') + ' ' + chrome.i18n.getMessage('appName')
+                        + ' v' + version.toString() + ' ' + chrome.i18n.getMessage('alert_isAvail') + '!&nbsp;&nbsp;&nbsp;'
+                        + '<a target="_blank" href="https://github.com/adamlui/chatgpt-widescreen/commits/main/chrome/extension" '
+                        + 'style="font-size: 0.7rem">' + chrome.i18n.getMessage('link_viewChanges') + '</a>',
+                    function update() { chrome.runtime.reload() } // update button
+                )
+            } else { // alert to no update found
+                alert(chrome.i18n.getMessage('alert_upToDate') + '!',
+                    chrome.i18n.getMessage('appName') + ' v' + chrome.runtime.getManifest().version
+                        + ' ' + chrome.i18n.getMessage('alert_isUpToDate') + '!'
+                )
+            }
+    })})
 
     // Add Support span click-listener
     var supportLink = document.querySelector('a[title*="support" i]')
@@ -78,14 +114,20 @@
     // Define script functions
 
     function notify(msg, position) {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
             chrome.tabs.sendMessage(tabs[0].id, { 
-                type: 'notify', msg: msg, position: position ? position : 'bottom-right' })
+                action: 'notify', msg: msg, position: position ? position : 'bottom-right' })
     })}
 
-    function toggleExtension() {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { type: 'toggleExtension' })
+    function alert(title = '', msg = '', btns = '') {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            chrome.tabs.sendMessage(tabs[0].id, { 
+                action: 'alert', title: title, msg: msg, btns: btns })
+    })}
+
+    function syncExtension() {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            chrome.tabs.sendMessage(tabs[0].id, { action: 'syncExtension' })
     })}
 
     function updateGreyness() {
