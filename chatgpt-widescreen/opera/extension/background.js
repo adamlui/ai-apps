1629836890e604c15c927ba4298a567c4e8af3e8
adamlui <adamlui@users.noreply.github.com>
2023-06-15 08:26:35 +0000

    Re-formatted Update Available alert ↞ [auto-sync from `adamlui/chatgpt-widescreen`]

--- chatgpt-widescreen/opera/extension/background.js
+++ chatgpt-widescreen/opera/extension/background.js
@@ -8,7 +8,7 @@ chrome.runtime.onInstalled.addListener((details) => {
 chrome.runtime.onUpdateAvailable.addListener((details) => {
     alert(`${ chrome.i18n.getMessage('alert_updateAvail') }!`,
         chrome.i18n.getMessage('alert_newerVer') + ' ' + chrome.i18n.getMessage('appName')
-            + ' v' + details.version + ' ' + chrome.i18n.getMessage('alert_isAvail') + '!&nbsp;&nbsp;&nbsp;'
+            + ' v' + details.version + ' ' + chrome.i18n.getMessage('alert_isAvail') + '!   '
             + '<a target="_blank" href="https://github.com/adamlui/chatgpt-widescreen/commits/main/chrome/extension" '
             + 'style="font-size: 0.7rem">' + chrome.i18n.getMessage('link_viewChanges') + '</a>',
         function update() { chrome.runtime.reload() } // update button
