76ee4b3eb044ead887dc50e508f1badc7dd00908
adamlui <adamlui@users.noreply.github.com>
2023-06-16 16:41:07 +0000

    Added `settings-utils.js` ↞ [auto-sync from `adamlui/chatgpt-infinity`]

---
+++ chatgpt-infinity/chrome/extension/lib/settings-utils.js
@@ -0,0 +1,23 @@
+var configPrefix = 'chatGPTim_'
+var config = {}
+var settings = {
+
+    load: function() {
+        var keys = ( // original array if array, else new array from multiple args
+            Array.isArray(arguments[0]) ? arguments[0] : Array.prototype.slice.call(arguments))
+        return Promise.all(keys.map(function(key) { // resolve promise when all keys load
+            return new Promise(function(resolve) { // resolve promise when single key value loads
+                chrome.storage.local.get(configPrefix + key, function(result) { // load from Chrome
+                    config[key] = result[configPrefix + key] === true // save to memory
+                    resolve()
+    })})}))},
+
+    save: function(key, value) {
+        var obj = {} ; obj[configPrefix + key] = value
+        chrome.storage.local.set(obj) // save to Chrome
+        config[key] = value // save to memory
+    }
+
+}
+
+export { config, settings }
