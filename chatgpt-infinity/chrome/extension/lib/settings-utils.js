b5ba06752f387c11b7ca66c0f792a2fc19065d12
adamlui <adamlui@users.noreply.github.com>
2023-06-18 12:44:41 +0000

    Added string loading ↞ [auto-sync from `adamlui/chatgpt-infinity`]

--- chatgpt-infinity/chrome/extension/lib/settings-utils.js
+++ chatgpt-infinity/chrome/extension/lib/settings-utils.js
@@ -5,11 +5,10 @@ const settings = {
     load: function() {
         const keys = ( // original array if array, else new array from multiple args
             Array.isArray(arguments[0]) ? arguments[0] : Array.prototype.slice.call(arguments))
-        return Promise.all(keys.map(function(key) { // resolve promise when all keys load
-            return new Promise(function(resolve) { // resolve promise when single key value loads
-                chrome.storage.local.get(configPrefix + key, function(result) { // load from Chrome
-                    config[key] = result[configPrefix + key] === true // save to memory
-                    resolve()
+        return Promise.all(keys.map((key) => { // resolve promise when all keys load
+            return new Promise((resolve) => { // resolve promise when single key value loads
+                chrome.storage.local.get(configPrefix + key, (result) => { // load from Chrome
+                    config[key] = result[configPrefix + key] || false ; resolve()
     })})}))},
 
     save: function(key, value) {
