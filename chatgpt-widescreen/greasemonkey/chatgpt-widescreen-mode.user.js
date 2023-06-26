7c47bcfce3f5577704cb78ea872acf4e0a01dde8
adamlui <adamlui@users.noreply.github.com>
2023-06-26 08:47:24 +0000

    Bumped chatgpt.js to 1.10.3 to fix FWM detection in FF/VM ↞ [auto-sync from `adamlui/chatgpt-widescreen`]

--- chatgpt-widescreen/greasemonkey/chatgpt-widescreen-mode.user.js
+++ chatgpt-widescreen/greasemonkey/chatgpt-widescreen-mode.user.js
@@ -222,7 +222,7 @@
 // @description:zu      Engeza izinhlobo zezimodi ze-Widescreen + Fullscreen ku-ChatGPT ukuze kube nokubonakala + ukuncitsha ukusukela
 // @author              Adam Lui
 // @namespace           https://github.com/adamlui
-// @version             2023.6.24.1
+// @version             2023.6.26
 // @license             MIT
 // @compatible          chrome
 // @compatible          firefox
@@ -236,7 +236,7 @@
 // @match               https://chat.openai.com/*
 // @icon                https://raw.githubusercontent.com/adamlui/chatgpt-widescreen/main/media/images/icons/widescreen-robot-emoji/icon48.png
 // @icon64              https://raw.githubusercontent.com/adamlui/chatgpt-widescreen/main/media/images/icons/widescreen-robot-emoji/icon64.png
-// @require             https://cdn.jsdelivr.net/gh/chatgptjs/chatgpt.js@76b0c6869466e1c2200137afc96f9687f7b9616b/dist/chatgpt-1.10.2.min.js
+// @require             https://cdn.jsdelivr.net/gh/chatgptjs/chatgpt.js@ffd846eb2c6211a7f1ee733a18e88433f05b5c8c/dist/chatgpt-1.10.3.min.js
 // @connect             raw.githubusercontent.com
 // @connect             greasyfork.org
 // @grant               GM_setValue
