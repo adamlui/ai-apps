deb346aa2151dc78dd6b861eb15bf0f79bf768f9
adamlui <adamlui@users.noreply.github.com>
2023-06-27 23:54:54 +0000

    Updated `version` to `2023.6.27` ↞ [auto-sync from `adamlui/chatgpt-widescreen`]

--- chatgpt-widescreen/chrome/extension/background.js
+++ chatgpt-widescreen/chrome/extension/background.js
@@ -4,18 +4,9 @@ chrome.runtime.onInstalled.addListener((details) => {
     if (details.reason == 'install') chrome.tabs.create({ url: 'https://chat.openai.com/' }) // open ChatGPT
 })
 
-// Add auto-alert on update available
-chrome.runtime.onUpdateAvailable.addListener((details) => { alertToUpdate(details.version) })
-
 // Sync extension state/settings when ChatGPT tab active
 chrome.tabs.onActivated.addListener((activeInfo) => {
     chrome.tabs.get(activeInfo.tabId, (tab) => {
         if (tab.url.includes('https://chat.openai.com')) {
             chrome.tabs.sendMessage(tab.id, { action: 'syncExtension' })
 }})})
-
-function alertToUpdate(version) {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, { 
-            action: 'alertToUpdate', args: version
-})})}
