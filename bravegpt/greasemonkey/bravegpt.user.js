cb4b274368154400c0eaeaa3ab8df1e4eff29339
adamlui <adamlui@users.noreply.github.com>
2023-06-28 02:20:10 +0000

    Removed leftover `else` ↞ [auto-sync from `adamlui/userscripts`]

--- bravegpt/greasemonkey/bravegpt.user.js
+++ bravegpt/greasemonkey/bravegpt.user.js
@@ -114,7 +114,7 @@
 // @description:zu      Engeza amaswazi aseChatGPT emugqa wokuqala weBrave Search (ibhulohwe nguGPT-4!)
 // @author              Adam Lui
 // @namespace           https://github.com/adamlui
-// @version             2023.6.27.1
+// @version             2023.6.27.2
 // @license             MIT
 // @icon                https://media.bravegpt.com/images/bravegpt-icon48.png
 // @icon64              https://media.bravegpt.com/images/bravegpt-icon64.png
@@ -433,7 +433,7 @@
                         } catch (error) {
                             braveGPTconsole.error(braveGPTalerts.parseFailed + ': ' + error)
                             braveGPTconsole.info('Response: ' + event.response)
-                            else braveGPTalert('suggestProxy')
+                            braveGPTalert('suggestProxy')
                         }
                     }
                 } else if (config.proxyAPIenabled) {
