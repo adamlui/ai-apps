8bfd22012ed76aa004281083ce6acaf043c87d8b
adamlui <adamlui@users.noreply.github.com>
2023-07-14 14:26:07 +0000

    Bumped chatgpt.js to 2.0.0, added `chatgpt.getUserLanguage()` ↞ [auto-sync from `adamlui/chatgpt-widescreen`]

--- chatgpt-widescreen/greasemonkey/chatgpt-widescreen-mode.user.js
+++ chatgpt-widescreen/greasemonkey/chatgpt-widescreen-mode.user.js
@@ -222,7 +222,7 @@
 // @description:zu      Engeza izinhlobo zezimodi ze-Widescreen + Fullscreen ku-ChatGPT ukuze kube nokubonakala + ukuncitsha ukusukela
 // @author              Adam Lui
 // @namespace           https://github.com/adamlui
-// @version             2023.7.11
+// @version             2023.7.14
 // @license             MIT
 // @compatible          chrome
 // @compatible          firefox
@@ -238,7 +238,7 @@
 // @match               https://you.com/*youchat*
 // @icon                https://raw.githubusercontent.com/adamlui/chatgpt-widescreen/main/media/images/icons/widescreen-robot-emoji/icon48.png
 // @icon64              https://raw.githubusercontent.com/adamlui/chatgpt-widescreen/main/media/images/icons/widescreen-robot-emoji/icon64.png
-// @require             https://cdn.jsdelivr.net/gh/kudoai/chatgpt.js@24a755998291094d0cd3b2bd395dff7c6756bbf9/dist/chatgpt-1.12.0.min.js
+// @require             https://cdn.jsdelivr.net/gh/kudoai/chatgpt.js@09e429363da222312c22ff7b67e3cc0fa08c4665/dist/chatgpt-2.0.0.min.js
 // @connect             raw.githubusercontent.com
 // @connect             greasyfork.org
 // @grant               GM_setValue
