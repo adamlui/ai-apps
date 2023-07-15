201ac240cc8f0450172fd2d6dba4f9b681d4735c
adamlui <adamlui@users.noreply.github.com>
2023-07-15 16:51:43 +0000

    Renamed `dimensions` to `iconDimensions` ↞ [auto-sync from `adamlui/chatgpt-widescreen`]

--- chatgpt-widescreen/edge/extension/popup/popup.js
+++ chatgpt-widescreen/edge/extension/popup/popup.js
@@ -123,8 +123,8 @@
     function updateGreyness() {
 
         // Updated toolbar icon
-        const dimensions = [16, 32, 48, 64, 128, 223], iconPaths = {}
-        dimensions.forEach((dimension) => {
+        const iconDimensions = [16, 32, 48, 64, 128, 223], iconPaths = {}
+        iconDimensions.forEach((dimension) => {
             iconPaths[dimension] = '../icons/'
                 + (config.extensionDisabled ? 'faded/' : '')
                 + 'icon' + dimension + '.png'
