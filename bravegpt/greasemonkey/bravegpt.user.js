5f39971e236564ba1d4fa63641ab87701c1767ed
adamlui@protonmail.com <adamlui@protonmail.com>
2023-07-15 12:04:54 -0700

    Replaced string matching of regex w/ faster method

--- bravegpt/greasemonkey/bravegpt.user.js
+++ bravegpt/greasemonkey/bravegpt.user.js
@@ -114,7 +114,7 @@
 // @description:zu      Engeza amaswazi aseChatGPT emugqa wokuqala weBrave Search (ibhulohwe nguGPT-4!)
 // @author              Adam Lui
 // @namespace           https://github.com/adamlui
-// @version             2023.7.15
+// @version             2023.7.15.1
 // @license             MIT
 // @icon                https://media.bravegpt.com/images/bravegpt-icon48.png
 // @icon64              https://media.bravegpt.com/images/bravegpt-icon64.png
@@ -224,7 +224,7 @@
             onload: (response) => { saveSetting('lastCheckTime', Date.now())
 
                 // Compare versions
-                const latestVer = response.responseText.match(/@version +(.*)/)[1]
+                const latestVer = /@version +(.*)/.exec(response.responseText)[1]
                 if (!checkForUpdates.fromMenu && config.skipNextUpdate && latestVer === config.skippedVer)
                     return // exit comparison if past auto-alert hidden
                 for (let i = 0 ; i < 4 ; i++) { // loop thru subver's
