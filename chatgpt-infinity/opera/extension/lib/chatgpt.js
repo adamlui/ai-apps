6fbba32aaec590fbed26f5d279a700b03509072f
adamlui <adamlui@users.noreply.github.com>
2023-07-15 17:59:39 +0000

    Bumped to 2.0.1, added `randomFloat()` ↞ [auto-sync from `adamlui/chatgpt-infinity`]

--- chatgpt-infinity/opera/extension/lib/chatgpt.js
+++ chatgpt-infinity/opera/extension/lib/chatgpt.js
@@ -1,12 +1,12 @@
-// This library is a condensed version of chatgpt.js v1.10.3
+// This library is a condensed version of chatgpt.js v2.0.1
 // (c) 2023 KudoAI & contributors under the MIT license
 // Source: https://github.com/kudoai/chatgpt.js
 // Latest minified release: https://code.chatgptjs.org/chatgpt-latest.min.js
 
-var alertProps = { queue: [] };
-var notifyProps = { quadrants: { topRight: [], bottomRight: [], bottomLeft: [], topLeft: [] }};
-localStorage.alertProps = JSON.stringify(alertProps);
-localStorage.notifyProps = JSON.stringify(notifyProps);
+// Init queues for feedback methods
+var alertQueue = []; localStorage.alertQueue = JSON.stringify(alertQueue);
+var notifyQueue = { quadrants: { topRight: [], bottomRight: [], bottomLeft: [], topLeft: [] }};
+localStorage.notifyQueue = JSON.stringify(notifyQueue);
 
 var chatgpt = {
 
@@ -15,17 +15,17 @@ var chatgpt = {
     // * Spaces are inserted into button labels by parsing function names in camel/kebab/snake case
 
         // Create modal parent/children elements
-        var modalContainer = document.createElement('div');
-        modalContainer.id = Math.floor(Math.random() * 1000000) + Date.now();
+        const modalContainer = document.createElement('div');
+        modalContainer.id = Math.floor(chatgpt.randomFloat() * 1000000) + Date.now();
         modalContainer.classList.add('chatgpt-modal'); // add class to main div
-        var modal = document.createElement('div');
-        var modalTitle = document.createElement('h2');
-        var modalMessage = document.createElement('p');
+        const modal = document.createElement('div');
+        const modalTitle = document.createElement('h2');
+        const modalMessage = document.createElement('p');
 
         // Create/append style if necessary
         if (!document.querySelector('#chatgpt-alert-style')) {
-            var scheme = chatgpt.isDarkMode() ? 'dark' : 'light';
-            var modalStyle = document.createElement('style');
+            const scheme = chatgpt.isDarkMode() ? 'dark' : 'light';
+            const modalStyle = document.createElement('style');
             modalStyle.id = 'chatgpt-alert-style';
             modalStyle.innerText = (
 
@@ -74,23 +74,23 @@ var chatgpt = {
         modalMessage.innerText = msg ? msg : ''; this.renderHTML(modalMessage);
 
         // Create/append buttons (if provided) to buttons div
-        var modalButtons = document.createElement('div');
+        const modalButtons = document.createElement('div');
         modalButtons.classList.add('modal-buttons');
         if (btns) { // are supplied
             if (!Array.isArray(btns)) btns = [btns]; // convert single button to array if necessary
             btns.forEach((buttonFn) => { // create title-cased labels + attach listeners
-                var button = document.createElement('button');
+                const button = document.createElement('button');
                 button.textContent = buttonFn.name
                     .replace(/[_-]\w/g, match => match.slice(1).toUpperCase()) // convert snake/kebab to camel case
                     .replace(/([A-Z])/g, ' $1') // insert spaces
                     .replace(/^\w/, firstChar => firstChar.toUpperCase()); // capitalize first letter
-                button.addEventListener('click', function() { destroyAlert(); buttonFn(); });
+                button.addEventListener('click', () => { destroyAlert(); buttonFn(); });
                 modalButtons.insertBefore(button, modalButtons.firstChild); // insert button to left
             });
         }
 
         // Create/append OK/dismiss button to buttons div
-        var dismissBtn = document.createElement('button');
+        const dismissBtn = document.createElement('button');
         dismissBtn.textContent = btns ? 'Dismiss' : 'OK';
         dismissBtn.addEventListener('click', destroyAlert);
         modalButtons.insertBefore(dismissBtn, modalButtons.firstChild);
@@ -99,16 +99,16 @@ var chatgpt = {
         modalButtons.lastChild.classList.add('primary-modal-btn');
 
         // Create/append checkbox (if provided) to checkbox group div
-        var checkboxDiv = document.createElement('div');
+        const checkboxDiv = document.createElement('div');
         if (checkbox) { // is supplied
             checkboxDiv.classList.add('checkbox-group');
-            var checkboxFn = checkbox; // assign the named function to checkboxFn
-            var checkboxInput = document.createElement('input');
+            const checkboxFn = checkbox; // assign the named function to checkboxFn
+            const checkboxInput = document.createElement('input');
             checkboxInput.type = 'checkbox';
             checkboxInput.addEventListener('change', checkboxFn);
 
             // Create/show label
-            var checkboxLabel = document.createElement('label');
+            const checkboxLabel = document.createElement('label');
             checkboxLabel.addEventListener('click', function() {
                 checkboxInput.checked = !checkboxInput.checked; checkboxFn(); });
             checkboxLabel.textContent = checkboxFn.name.charAt(0).toUpperCase() // capitalize first char
@@ -121,28 +121,28 @@ var chatgpt = {
         }
 
         // Assemble/append div
-        var elements = [modalTitle, modalMessage, modalButtons, checkboxDiv];
-        elements.forEach(function(element) { modal.appendChild(element); });
+        const elements = [modalTitle, modalMessage, modalButtons, checkboxDiv];
+        elements.forEach((element) => { modal.appendChild(element); });
         modalContainer.appendChild(modal); document.body.appendChild(modalContainer); 
 
         // Enqueue alert
-        alertProps = JSON.parse(localStorage.alertProps);
-        alertProps.queue.push(modalContainer.id);
-        localStorage.alertProps = JSON.stringify(alertProps);
+        alertQueue = JSON.parse(localStorage.alertQueue);
+        alertQueue.push(modalContainer.id);
+        localStorage.alertQueue = JSON.stringify(alertQueue);
 
         // Add listeners
         document.addEventListener('keydown', keyHandler);
-        modalContainer.addEventListener('click', function(event) {
+        modalContainer.addEventListener('click', (event) => {
             if (event.target === modalContainer) destroyAlert(); });
 
         // Show alert if none active
-        modalContainer.style.display = (alertProps.queue.length === 1) ? '' : 'none';
+        modalContainer.style.display = (alertQueue.length === 1) ? '' : 'none';
 
         function destroyAlert() {
             modalContainer.remove(); // remove from DOM
-            var alertProps = JSON.parse(localStorage.alertProps);
-            alertProps.queue.shift(); // + memory
-            localStorage.alertProps = JSON.stringify(alertProps); // + storage
+            alertQueue = JSON.parse(localStorage.alertQueue);
+            alertQueue.shift(); // + memory
+            localStorage.alertQueue = JSON.stringify(alertQueue); // + storage
 
             // Prevent memory leaks
             modalContainer.removeEventListener('click', destroyAlert);
@@ -150,21 +150,21 @@ var chatgpt = {
             dismissBtn.removeEventListener('click', destroyAlert);
 
             // Check for pending alerts in queue
-            if (alertProps.queue.length > 0) {
-                var nextAlert = document.getElementById(alertProps.queue[0]);
+            if (alertQueue.length > 0) {
+                const nextAlert = document.getElementById(alertQueue[0]);
                 setTimeout(() => { nextAlert.style.display = 'flex'; }, 500 );
             }
         }
 
         function keyHandler(event) {
-            var dismissKeys = [13, 27, 32]; // enter/esc/space
+            const dismissKeys = [13, 27]; // enter/esc
             if (dismissKeys.includes(event.keyCode)) {
-                for (var i = 0; i < alertProps.queue.length; i++) { // look to handle only if triggering alert is active
-                    var alert = document.getElementById(alertProps.queue[i]);
+                for (let i = 0; i < alertQueue.length; i++) { // look to handle only if triggering alert is active
+                    const alert = document.getElementById(alertQueue[i]);
                     if (alert && alert.style.display != 'none') { // active alert found
                         if (event.keyCode === 27) destroyAlert(); // if esc pressed, dismiss alert & do nothing
-                        else if (event.keyCode === 32 || event.keyCode === 13) { // else if space/enter pressed
-                            var mainButton = alert.querySelector('.modal-buttons').lastChild; // look for main button
+                        else if (event.keyCode === 13) { // else if enter pressed
+                            const mainButton = alert.querySelector('.modal-buttons').lastChild; // look for main button
                             if (mainButton) { mainButton.click(); event.preventDefault(); } // click if found
                         } return;
         }}}}
@@ -180,8 +180,8 @@ var chatgpt = {
 
     history: {
         isOn: function() {
-            for (var navLink of document.querySelectorAll('nav[aria-label="Chat history"] a')) {
-                if (navLink.text.match(/clear chat/i)) return false;
+            for (const navLink of document.querySelectorAll('nav[aria-label="Chat history"] a')) {
+                if (/clear chat/i.test(navLink.text)) return false;
             } return true;
         },
         isOff: function() { return !this.isOn(); }
@@ -198,19 +198,19 @@ var chatgpt = {
 
     isLoaded: function() {
         return new Promise(resolve => {
-            var intervalId = setInterval(() => {
+            const intervalId = setInterval(() => {
                 if (document.querySelector('nav button[id*="menu"]')) {
                     clearInterval(intervalId); resolve();
     }}, 100);});},
 
     notify: function(msg, position, notifDuration, shadow) {
         notifDuration = notifDuration ? +notifDuration : 1.75; // sec duration to maintain notification visibility
-        var fadeDuration = 0.6; // sec duration of fade-out
-        var vpYoffset = 23, vpXoffset = 27; // px offset from viewport border
+        const fadeDuration = 0.6; // sec duration of fade-out
+        const vpYoffset = 23, vpXoffset = 27; // px offset from viewport border
 
         // Make/stylize/insert div
-        var notificationDiv = document.createElement('div'); // make div
-        notificationDiv.id = Math.floor(Math.random() * 1000000) + Date.now();
+        const notificationDiv = document.createElement('div'); // make div
+        notificationDiv.id = Math.floor(chatgpt.randomFloat() * 1000000) + Date.now();
         notificationDiv.style.cssText = ( // stylize it
               ' background-color: black ; padding: 10px ; border-radius: 8px ; ' // box style
             + ' opacity: 0 ; position: fixed ; z-index: 9999 ; font-size: 1.8rem ; color: white ; ' // visibility
@@ -225,9 +225,9 @@ var chatgpt = {
             + (notificationDiv.isRight ? 'Right' : 'Left');
 
         // Store div
-        notifyProps = JSON.parse(localStorage.notifyProps);
-        notifyProps.quadrants[notificationDiv.quadrant].push(notificationDiv.id);
-        localStorage.notifyProps = JSON.stringify(notifyProps);
+        notifyQueue = JSON.parse(localStorage.notifyQueue);
+        notifyQueue.quadrants[notificationDiv.quadrant].push(notificationDiv.id);
+        localStorage.notifyQueue = JSON.stringify(notifyQueue);
 
         // Position notification (defaults to top-right)
         notificationDiv.style.top = notificationDiv.isTop ? vpYoffset.toString() + 'px' : '';
@@ -236,14 +236,15 @@ var chatgpt = {
         notificationDiv.style.left = !notificationDiv.isRight ? vpXoffset.toString() + 'px' : '';
 
         // Reposition old notifications
-        var thisQuadrantDivIDs = notifyProps.quadrants[notificationDiv.quadrant];
+        const thisQuadrantDivIDs = notifyQueue.quadrants[notificationDiv.quadrant];
         if (thisQuadrantDivIDs.length > 1) {
             try { // to move old notifications
-                var divsToMove = thisQuadrantDivIDs.slice(0, -1); // exclude new div
-                for (var j = 0; j < divsToMove.length; j++) {
-                    var oldDiv = document.getElementById(divsToMove[j]);
-                    var offsetProp = oldDiv.style.top ? 'top' : 'bottom'; // pick property to change
-                    var vOffset = +oldDiv.style[offsetProp].match(/\d+/)[0] + 5 + oldDiv.getBoundingClientRect().height;
+                const divsToMove = thisQuadrantDivIDs.slice(0, -1); // exclude new div
+                for (let j = 0; j < divsToMove.length; j++) {
+                    const oldDiv = document.getElementById(divsToMove[j]);
+                    const offsetProp = oldDiv.style.top ? 'top' : 'bottom'; // pick property to change
+                    // const vOffset = +oldDiv.style[offsetProp].match(/\d+/)[0] + 5 + oldDiv.getBoundingClientRect().height;
+                    const vOffset = +/\d+/.exec(oldDiv.style[offsetProp])[0] + 5 + oldDiv.getBoundingClientRect().height;
                     oldDiv.style[offsetProp] = `${vOffset}px`; // change prop
                 }
             } catch (error) {}
@@ -255,23 +256,29 @@ var chatgpt = {
         notificationDiv.style.opacity = 1; // show msg
 
         // Hide notification
-        var hideDelay = ( // set delay before fading
+        const hideDelay = ( // set delay before fading
             fadeDuration > notifDuration ? 0 // don't delay if fade exceeds notification duration
             : notifDuration - fadeDuration); // otherwise delay for difference
-        notificationDiv.hideTimer = setTimeout(function hideNotif() { // maintain notification visibility, then fade out
+        notificationDiv.hideTimer = setTimeout(() => { // maintain notification visibility, then fade out
             notificationDiv.style.transition = 'opacity ' + fadeDuration.toString() + 's'; // add fade effect
             notificationDiv.style.opacity = 0; // hide notification
             notificationDiv.hideTimer = null; // prevent memory leaks
         }, hideDelay * 1000); // ...after pre-set duration
 
         // Destroy notification
-        notificationDiv.destroyTimer = setTimeout(function destroyNotif() {
+        notificationDiv.destroyTimer = setTimeout(() => {
             notificationDiv.remove(); // remove from DOM
-            notifyProps = JSON.parse(localStorage.notifyProps);
-            notifyProps.quadrants[notificationDiv.quadrant].shift(); // + memory
-            localStorage.notifyProps = JSON.stringify(notifyProps); // + storage
+            notifyQueue = JSON.parse(localStorage.notifyQueue);
+            notifyQueue.quadrants[notificationDiv.quadrant].shift(); // + memory
+            localStorage.notifyQueue = JSON.stringify(notifyQueue); // + storage
             notificationDiv.destroyTimer = null; // prevent memory leaks
         }, Math.max(fadeDuration, notifDuration) * 1000); // ...after notification hid
+    },
+
+    randomFloat: function() {
+    // * Generates a random, cryptographically secure value between 0 (inclusive) & 1 (exclusive)
+        const crypto = window.crypto || window.msCrypto;
+        return crypto.getRandomValues(new Uint32Array(1))[0] / 0xFFFFFFFF;
     },
 
     renderHTML: function(node) {
@@ -327,8 +334,7 @@ var chatgpt = {
     },
 
     scrollToBottom: function() {
-        try { document.querySelector('button[class*="cursor"]').click(); } catch (error) { console.error('🤖 chatgpt.js >> ', error); }
-    },
+        try { document.querySelector('button[class*="cursor"]').click(); } catch (error) { console.error('🤖 chatgpt.js >> ', error); }},
 
     send: function(msg, method='') {
         var textArea = document.querySelector('form textarea');
@@ -345,13 +351,13 @@ var chatgpt = {
     },
 
     startNewChat: function() {
-        for (var navLink of document.querySelectorAll('nav[aria-label="Chat history"] a')) {
-            if (navLink.text.match(/(new|clear) chat/i)) {
+        for (const navLink of document.querySelectorAll('nav[aria-label="Chat history"] a')) {
+            if (/(new|clear) chat/i.test(navLink.text)) {
                 navLink.click(); return;
     }}},
 
     stop: function() {
-        for (var formButton of document.querySelectorAll('form button')) {
+        for (const formButton of document.querySelectorAll('form button')) {
             if (formButton.textContent.toLowerCase().includes('stop')) {
                 formButton.click(); return;
     }}}
