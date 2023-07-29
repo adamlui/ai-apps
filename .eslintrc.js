dfce0ec84f81e893d70ca7a9b287f09428ccfb4c
Adam Lui 刘展鹏 <adamlui@pm.me>
2023-07-28 18:57:02 -0700

    Reverted to pre-sync ver except indent rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,9 @@ module.exports  = {
         'no-empty': 'off', // allow empty blocks
         'no-useless-escape': 'off' // allow all escape chars cause ESLint sucks at detecting truly useless ones
     },
-    'globals': { '$': true, 'chatgpt': true, 'chrome': true, 'global_module': true, 'GM_cookie': true },
+    'globals': { '$': 'readonly', 'chatgpt': 'readonly', 'chrome': 'readonly', 'syncExtension': 'writable',
+                 'global_module': 'readonly', 'GM_cookie': 'readonly', 'fullScreenButton': 'readonly',
+                 'fullWindowButton': 'readonly', 'newChatButton': 'readonly', 'wideScreenButton': 'readonly' },
     'parserOptions': { 'ecmaVersion': 2022, 'sourceType': 'script' },
     'overrides': [{ 'files': ['**/lib*/**.js'], 'parserOptions': { 'sourceType': 'module' }}],
     'env': { 'browser': true, 'node': true, 'es6': true, 'greasemonkey': true }
