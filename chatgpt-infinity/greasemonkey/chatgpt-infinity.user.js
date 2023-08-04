3618a555ab47a721e2b64706d6143e7be67b3661
adamlui <adamlui@users.noreply.github.com>
2023-08-04 07:13:22 +0000

    Bumped chatgpt.js to 2.0.6 ↞ [auto-sync from `adamlui/chatgpt-infinity`]

--- chatgpt-infinity/greasemonkey/chatgpt-infinity.user.js
+++ chatgpt-infinity/greasemonkey/chatgpt-infinity.user.js
@@ -199,7 +199,7 @@
 // @description:zh-TW   從無所不知的 ChatGPT 生成無窮無盡的答案 (用任何語言!)
 // @author              Adam Lui
 // @namespace           https://github.com/adamlui
-// @version             2023.7.31
+// @version             2023.8.4
 // @license             MIT
 // @match               https://chat.openai.com/*
 // @icon                https://raw.githubusercontent.com/adamlui/chatgpt-infinity/main/media/images/icons/infinity-symbol/black/icon48.png
@@ -214,7 +214,7 @@
 // @compatible          librewolf
 // @compatible          ghost
 // @compatible          qq
-// @require             https://cdn.jsdelivr.net/gh/kudoai/chatgpt.js@794614321da08c1a577090bfe3f3c88291357ca9/dist/chatgpt-2.0.5.min.js
+// @require             https://cdn.jsdelivr.net/gh/kudoai/chatgpt.js@0ccfc9e6acfcc24f864b233199f811316cfcd136/dist/chatgpt-2.0.6.min.js
 // @connect             raw.githubusercontent.com
 // @connect             greasyfork.org
 // @grant               GM_setValue
