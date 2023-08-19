dbf497d54e0e48bb5b8c7343a4eee7e02543d01d
adamlui <adamlui@users.noreply.github.com>
2023-08-19 08:20:35 +0000

    Removed protocol from `// @match` ↞ [auto-sync from `kudoai/bravegpt`]

--- bravegpt/greasemonkey/bravegpt.user.js
+++ bravegpt/greasemonkey/bravegpt.user.js
@@ -114,7 +114,7 @@
 // @description:zu      Engeza amaswazi aseChatGPT emugqa wokuqala weBrave Search (ibhulohwe nguGPT-4!)
 // @author              KudoAI
 // @namespace           https://kudoai.com
-// @version             2023.8.14.1
+// @version             2023.8.19
 // @license             MIT
 // @icon                https://media.bravegpt.com/images/bravegpt-icon48.png
 // @icon64              https://media.bravegpt.com/images/bravegpt-icon64.png
@@ -127,7 +127,7 @@
 // @compatible          librewolf
 // @compatible          ghost
 // @compatible          qq
-// @match               https://search.brave.com/search*
+// @match               *://search.brave.com/search*
 // @include             https://auth0.openai.com
 // @connect             raw.githubusercontent.com
 // @connect             greasyfork.org
