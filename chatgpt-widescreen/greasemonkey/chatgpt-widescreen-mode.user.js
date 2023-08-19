e3c5ae58c96c27cc9a5075d20482ea2720eb372b
adamlui <adamlui@users.noreply.github.com>
2023-08-19 08:19:43 +0000

    Removed protocol from `// @match` metas ↞ [auto-sync from `adamlui/chatgpt-widescreen`]

--- chatgpt-widescreen/greasemonkey/chatgpt-widescreen-mode.user.js
+++ chatgpt-widescreen/greasemonkey/chatgpt-widescreen-mode.user.js
@@ -222,7 +222,7 @@
 // @description:zu      Engeza izinhlobo zezimodi ze-Widescreen + Fullscreen ku-ChatGPT ukuze kube nokubonakala + ukuncitsha ukusukela
 // @author              Adam Lui
 // @namespace           https://github.com/adamlui
-// @version             2023.8.14.8
+// @version             2023.8.19
 // @license             MIT
 // @compatible          chrome
 // @compatible          firefox
@@ -233,8 +233,8 @@
 // @compatible          librewolf
 // @compatible          ghost
 // @compatible          qq
-// @match               https://chat.openai.com/*
-// @match               https://poe.com/*
+// @match               *://chat.openai.com/*
+// @match               *://poe.com/*
 // @icon                https://raw.githubusercontent.com/adamlui/chatgpt-widescreen/main/media/images/icons/widescreen-robot-emoji/icon48.png
 // @icon64              https://raw.githubusercontent.com/adamlui/chatgpt-widescreen/main/media/images/icons/widescreen-robot-emoji/icon64.png
 // @require             https://cdn.jsdelivr.net/gh/kudoai/chatgpt.js@315fc8e62d4d3e82276fbb641128774a0d1c5219/dist/chatgpt-2.1.0.min.js
