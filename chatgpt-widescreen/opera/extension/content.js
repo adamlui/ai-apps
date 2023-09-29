a615650492edbe3e0ac26406c7081d0b30a00650
adamlui <adamlui@users.noreply.github.com>
2023-09-29 21:00:19 +0000

    Added `menuLabel_hiddenHeader` ↞ [auto-sync from `adamlui/chatgpt-widescreen`]

--- chatgpt-widescreen/opera/extension/content.js
+++ chatgpt-widescreen/opera/extension/content.js
@@ -179,7 +179,7 @@
                 chrome.i18n.getMessage('alert_newerVer') + ' ' + chrome.i18n.getMessage('appName')
                     + ' v' + version + ' ' + chrome.i18n.getMessage('alert_isAvail') + '!   '
                     + '<a target="_blank" rel="noopener" style="font-size: 0.7rem" '
-                        + 'href="' + config.ghRepoURL + '/commits/main/chrome/extension" '
+                        + 'href="' + config.githubURL + '/commits/main/chrome/extension" '
                         + '>' + chrome.i18n.getMessage('link_viewChanges') + '</a>',
                 function reloadChrome() { chrome.runtime.reload() } // update button
             )
