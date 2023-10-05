f1082977caf7e7a274c31c31dc4a5668d638f71c
adamlui <adamlui@users.noreply.github.com>
2023-10-05 11:02:55 +0000

    Renamed `alert_intUpdated` to `alert_willRefresh` ↞ [auto-sync from `adamlui/chatgpt-auto-refresh`]

---
+++ chatgpt-auto-refresh/greasemonkey/json-key-rename.py
@@ -0,0 +1,22 @@
+import os
+
+def process_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+
+    # Perform string replacements
+    content = content.replace('alert_intUpdated', 'alert_willRefresh')
+
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(content)
+
+def process_directory(directory):
+    for foldername, subfolders, filenames in os.walk(directory):
+        for filename in filenames:
+            if filename == 'messages.json':
+                file_path = os.path.join(foldername, filename)
+                process_file(file_path)
+
+if __name__ == "__main__":
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+    process_directory(current_directory)
