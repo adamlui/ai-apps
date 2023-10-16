b2ea89139a1e432bc0b157202b173f970a23bdb8
adamlui <adamlui@users.noreply.github.com>
2023-10-16 22:07:07 +0000

    Re-colored toggle-on bg purple ↞ [auto-sync from `adamlui/autoclear-chatgpt-history`]

--- autoclear-chatgpt-history/greasemonkey/autoclear-chatgpt-history.user.js
+++ autoclear-chatgpt-history/greasemonkey/autoclear-chatgpt-history.user.js
@@ -225,7 +225,7 @@
 // @description:zu      Ziba itshala lokucabanga okuzoshintshwa ngokuzenzakalelayo uma ukubuka chat.openai.com
 // @author              Adam Lui
 // @namespace           https://github.com/adamlui
-// @version             2023.10.15
+// @version             2023.10.16
 // @license             MIT
 // @icon                https://raw.githubusercontent.com/adamlui/userscripts/master/chatgpt/media/icons/openai-favicon48.png
 // @icon64              https://raw.githubusercontent.com/adamlui/userscripts/master/chatgpt/media/icons/openai-favicon64.png
@@ -333,7 +333,7 @@
 
             // Position/color ON-state
             + 'input:checked { position: absolute ; right: 3px } '
-            + 'input:checked + .slider { background-color: #42B4BF } '
+            + 'input:checked + .slider { background-color: #A057F8 } '
             + 'input:checked + .slider:before { '
                 + '-webkit-transform: translateX(14px) translateY(1px) ; '
                 + '-ms-transform: translateX(14px) translateY(1px) ; '
