c05af5ca03f86b7fecf2559e9158030eaeae304a
adamlui <adamlui@users.noreply.github.com>
2023-10-20 12:47:09 +0000

    Bumped chatgpt.js to 2.3.11, removed alert button hover styles ↞ [auto-sync from `adamlui/chatgpt-auto-continue`]

--- chatgpt-auto-continue/greasemonkey/chatgpt-auto-continue.user.js
+++ chatgpt-auto-continue/greasemonkey/chatgpt-auto-continue.user.js
@@ -219,12 +219,12 @@
 // @description:zu      ⚡ Terus menghasilkan imibuzo eminingi ye-ChatGPT ngokwesizulu
 // @author              Adam Lui
 // @namespace           https://github.com/adamlui
-// @version             2023.10.20
+// @version             2023.10.20.1
 // @license             MIT
 // @match               *://chat.openai.com/*
 // @icon                https://raw.githubusercontent.com/adamlui/userscripts/master/chatgpt/media/icons/openai-favicon48.png
 // @icon64              https://raw.githubusercontent.com/adamlui/userscripts/master/chatgpt/media/icons/openai-favicon64.png
-// @require             https://cdn.jsdelivr.net/gh/kudoai/chatgpt.js@8e2cdd86eb71f3b19b55928c01b33ec235d26809/dist/chatgpt-2.3.10.min.js
+// @require             https://cdn.jsdelivr.net/gh/kudoai/chatgpt.js@5d59b49dfec37e7f9c49e52441d125402a5ca10c/dist/chatgpt-2.3.11.min.js
 // @connect             raw.githubusercontent.com
 // @connect             greasyfork.org
 // @grant               GM_setValue
@@ -289,9 +289,7 @@
         chatgptAlertStyle.innerText = '.chatgpt-modal button {'
                 + 'font-size: 0.77rem ; text-transform: uppercase ;'
                 + 'border-radius: 0 !important ; padding: 5px !important ; min-width: 102px }'
-            + '.chatgpt-modal button:hover { color: white !important ;'
-                + ( 'background-color: ' + ( chatgpt.isDarkMode() ? '#00cfff' : '#1e9ebb' ) + '!important }'
-            + '.modal-buttons { margin-left: -13px !important }')
+            + '.modal-buttons { margin-left: -13px !important }'
         document.head.appendChild(chatgptAlertStyle)
     }
 
