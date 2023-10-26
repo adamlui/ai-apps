8d5d2c8c8d4fe5d17a9972e4d323487ddf03babe
adamlui <adamlui@users.noreply.github.com>
2023-10-26 04:26:17 +0000

    Refactored code
    - Removed `()` around unmodded params of arrow functions
    ± Converted string concatenations of config keys to template literals ↞ [auto-sync from `adamlui/chatgpt-widescreen`]

--- chatgpt-widescreen/chrome/extension/lib/settings-utils.js
+++ chatgpt-widescreen/chrome/extension/lib/settings-utils.js
@@ -9,17 +9,16 @@ const settings = {
         const keys = ( // original array if array, else new array from multiple args
             Array.isArray(arguments[0]) ? arguments[0] : Array.from(arguments))
         return Promise.all(keys.map((key) => { // resolve promise when all keys load
-            return new Promise((resolve) => { // resolve promise when single key value loads
-                chrome.storage.local.get(config.prefix + '_' + key, (result) => { // load from Chrome
-                    config[key] = result[config.prefix + '_' + key] || false ; resolve()
+            return new Promise(resolve => { // resolve promise when single key value loads
+                chrome.storage.local.get(`${ config.prefix }_${ key }`, result => { // load from Chrome
+                    config[key] = result[`${ config.prefix }_${ key }`] || false ; resolve()
     })})}))},
 
     save: function(key, value) {
-        const obj = {} ; obj[config.prefix + '_' + key] = value
+        const obj = {} ; obj[`${ config.prefix }_${ key }`] = value
         chrome.storage.local.set(obj) // save to Chrome
         config[key] = value // save to memory
     }
-
 }
 
 export { config, settings }
