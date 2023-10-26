5f39fbfb627f7c662e242cecb390af46425bea3a
adamlui <adamlui@users.noreply.github.com>
2023-10-26 04:48:48 +0000

    Refactored code
    - Removed `()` around unmodded params of arrow functions
    + Converted loose equality comparison to strict one ↞ [auto-sync from `adamlui/chatgpt-infinity`]

--- chatgpt-infinity/opera/extension/background.js
+++ chatgpt-infinity/opera/extension/background.js
@@ -1,14 +1,14 @@
 const allowedHosts = ['chat.openai.com']
 
 // Add install/update actions
-chrome.runtime.onInstalled.addListener((details) => {
+chrome.runtime.onInstalled.addListener(details => {
     chrome.storage.local.set({ 'chatgptInfinity_extensionDisabled': false }) // auto-enable
-    if (details.reason == 'install') chrome.tabs.create({ url: 'https://chat.openai.com/' }) // open ChatGPT
+    if (details.reason === 'install') chrome.tabs.create({ url: 'https://chat.openai.com/' }) // open ChatGPT
 })
 
 // Sync extension state/settings when ChatGPT tab active
-chrome.tabs.onActivated.addListener((activeInfo) => {
-    chrome.tabs.get(activeInfo.tabId, (tab) => {
+chrome.tabs.onActivated.addListener(activeInfo => {
+    chrome.tabs.get(activeInfo.tabId, tab => {
         if (allowedHosts.includes(new URL(tab.url).hostname)) {
             chrome.tabs.sendMessage(tab.id, { action: 'syncExtension' })
 }})})
