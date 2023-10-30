f66728177e16b0f2036687d3cfcf14940d648402
adamlui <adamlui@users.noreply.github.com>
2023-10-30 06:26:33 +0000

    Removed `()` from unmodded params of arrow functions ↞ [auto-sync from `kudoai/bravegpt`]

--- bravegpt/greasemonkey/bravegpt.user.js
+++ bravegpt/greasemonkey/bravegpt.user.js
@@ -114,7 +114,7 @@
 // @description:zu      Engeza amaswazi aseChatGPT emugqa wokuqala weBrave Search (ibhulohwe nguGPT-4!)
 // @author              KudoAI
 // @namespace           https://kudoai.com
-// @version             2023.10.26.1
+// @version             2023.10.29
 // @license             MIT
 // @icon                https://media.bravegpt.com/images/bravegpt-icon48.png
 // @icon64              https://media.bravegpt.com/images/bravegpt-icon64.png
@@ -168,7 +168,7 @@
         GM.xmlHttpRequest({
             method: 'GET', url: config.updateURL + '?t=' + Date.now(),
             headers: { 'Cache-Control': 'no-cache' },
-            onload: (response) => {
+            onload: response => {
 
                 // Compare versions
                 const latestVer = /@version +(.*)/.exec(response.responseText)[1]
@@ -371,11 +371,11 @@
     }}})}
 
     function getOpenAItoken() {
-        return new Promise((resolve) => {
+        return new Promise(resolve => {
             const accessToken = GM_getValue(config.prefix + '_openAItoken')
             braveGPTconsole.info('OpenAI access token: ' + accessToken)
             if (!accessToken) {
-                GM.xmlHttpRequest({ url: openAIendpoints.session, onload: (response) => {
+                GM.xmlHttpRequest({ url: openAIendpoints.session, onload: response => {
                     if (isBlockedbyCloudflare(response.responseText)) {
                         braveGPTalert('checkCloudflare') ; return }
                     try {
@@ -388,7 +388,7 @@
     })}
 
     function getAIGCFkey() {
-        return new Promise((resolve) => {
+        return new Promise(resolve => {
             const publicKey = GM_getValue(config.prefix + '_aigcfKey')
             if (!publicKey) {
                 GM.xmlHttpRequest({ method: 'GET', url: 'https://api.aigcfun.com/fc/key',
@@ -396,7 +396,7 @@
                         'Content-Type': 'application/json',
                         'Referer': 'https://aigcfun.com/',
                         'X-Forwarded-For': chatgpt.generateRandomIP() },
-                    onload: (response) => {
+                    onload: response => {
                         const newPublicKey = JSON.parse(response.responseText).data
                         if (!newPublicKey) { braveGPTconsole.error('Failed to get AIGCFun public key') ; return }
                         GM_setValue(config.prefix + '_aigcfKey', newPublicKey)
@@ -425,7 +425,7 @@
     let endpoint, accessKey, model
     async function pickAPI() {
         if (config.proxyAPIenabled) { // randomize proxy API
-            const untriedEndpoints = proxyEndpoints.filter((entry) => {
+            const untriedEndpoints = proxyEndpoints.filter(entry => {
                 return !getShowReply.triedEndpoints?.includes(entry[0]) })
             const entry = untriedEndpoints[Math.floor(chatgpt.randomFloat() * untriedEndpoints.length)]
             endpoint = entry[0] ; accessKey = entry[1] ; model = entry[2]
@@ -458,7 +458,7 @@
             method: 'POST', url: endpoint,
             headers: { 'Content-Type': 'application/json', Authorization: 'Bearer ' + accessKey },
             responseType: responseType(), data: createPayload(convo), onloadstart: onLoadStart(), onload: onLoad(),
-            onerror: (err) => {
+            onerror: err => {
                 braveGPTconsole.error(err)
                 if (!config.proxyAPIenabled) braveGPTalert(!accessKey ? 'login' : 'suggestProxy')
                 else { // if proxy mode
@@ -480,7 +480,7 @@
         function onLoadStart() { // process streams for unproxied TM users
             braveGPTconsole.info('Endpoint used: ' + endpoint)
             if (!config.proxyAPIenabled && getUserscriptManager() === 'Tampermonkey') {
-                return (stream) => {
+                return stream => {
                     const reader = stream.response.getReader()
                     reader.read().then(function processText({ done, value }) {
                         if (done) return
@@ -499,7 +499,7 @@
         })}}}
 
         function onLoad() {
-            return async (event) => {
+            return async event => {
                 if (event.status !== 200) {
                     braveGPTconsole.error('Event status: ' + event.status)
                     braveGPTconsole.error('Event response: ' + event.responseText)
@@ -796,7 +796,7 @@
     // Activate promo campaign if active
     GM.xmlHttpRequest({
         method: 'GET', url: config.assetHostURL + 'ads/live/creative.html',
-        onload: (response) => { if (response.status == 200) {
+        onload: response => { if (response.status == 200) {
 
                 // Create campaign div & add class/style/HTML
                 const pcDiv = document.createElement('div')
