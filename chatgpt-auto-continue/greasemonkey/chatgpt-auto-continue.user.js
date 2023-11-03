019e237c80ef72741e759c261e5a8c86a0f12525
adamlui <adamlui@users.noreply.github.com>
2023-11-03 10:54:24 +0000

    Bumped chatgpt.js to 2.3.14 ↞ [auto-sync from `adamlui/chatgpt-auto-continue`]

--- chatgpt-auto-continue/greasemonkey/chatgpt-auto-continue.user.js
+++ chatgpt-auto-continue/greasemonkey/chatgpt-auto-continue.user.js
@@ -219,12 +219,12 @@
 // @description:zu      ⚡ Terus menghasilkan imibuzo eminingi ye-ChatGPT ngokwesizulu
 // @author              Adam Lui
 // @namespace           https://github.com/adamlui
-// @version             2023.11.3
+// @version             2023.11.3.1
 // @license             MIT
 // @match               *://chat.openai.com/*
 // @icon                https://raw.githubusercontent.com/adamlui/userscripts/master/chatgpt/media/icons/openai-favicon48.png
 // @icon64              https://raw.githubusercontent.com/adamlui/userscripts/master/chatgpt/media/icons/openai-favicon64.png
-// @require             https://cdn.jsdelivr.net/gh/kudoai/chatgpt.js@516ad148b02335b98db82c89dec02e5fa28c7d56/dist/chatgpt-2.3.13.min.js
+// @require             https://cdn.jsdelivr.net/gh/kudoai/chatgpt.js@54bbbf01c1ea38d49fe4de504b868373fd29907e/dist/chatgpt-2.3.14.min.js
 // @connect             raw.githubusercontent.com
 // @connect             greasyfork.org
 // @grant               GM_setValue
