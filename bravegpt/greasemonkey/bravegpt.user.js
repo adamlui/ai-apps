a861b1e23b3c58afeac5bfca8a5db8c5f114d065
adamlui <adamlui@users.noreply.github.com>
2023-11-05 08:29:53 +0000

    Added optional chaining & ensured boolean return value in `isDarkMode()` ↞ [auto-sync from `kudoai/bravegpt`]

--- bravegpt/greasemonkey/bravegpt.user.js
+++ bravegpt/greasemonkey/bravegpt.user.js
@@ -114,7 +114,7 @@
 // @description:zu      Engeza amaswazi aseChatGPT emugqa wokuqala weBrave Search (ibhulohwe nguGPT-4!)
 // @author              KudoAI
 // @namespace           https://kudoai.com
-// @version             2023.11.5
+// @version             2023.11.5.1
 // @license             MIT
 // @icon                https://media.bravegpt.com/images/bravegpt-icon48.png
 // @icon64              https://media.bravegpt.com/images/bravegpt-icon64.png
@@ -361,8 +361,8 @@
     // Define UI functions
 
     function isDarkMode() {
-        return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
-            || document.documentElement.classList.toString().includes('dark')
+        return window.matchMedia?.('(prefers-color-scheme: dark)')?.matches
+            || !!document.querySelector('[data-darkmode="true"]')
     }
 
     function isChromium() { return navigator.userAgent.includes('Chrome') }
