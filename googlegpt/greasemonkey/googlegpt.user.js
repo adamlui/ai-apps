3cb45b7aeb044c4aeba9ee779ee51838d1f09d79
adamlui <adamlui@users.noreply.github.com>
2023-11-08 15:47:41 +0000

    Updated `googleGPTshow(answer)`
    ± Added `const fillColor`
    + Added about button
    ± Re-positioned speak  button
    + Renamed `.speak` to `.speak-btn`
    - Merged lines ↞ [auto-sync from `kudoai/googlegpt`]

--- googlegpt/greasemonkey/googlegpt.user.js
+++ googlegpt/greasemonkey/googlegpt.user.js
@@ -152,7 +152,7 @@
 // @description:zu      Faka amaphawu ase-ChatGPT kuvaliwe i-Google Search (okwesikhashana ngu-GPT-4!)
 // @author              KudoAI
 // @namespace           https://kudoai.com
-// @version             2023.11.8.3
+// @version             2023.11.8.4
 // @license             MIT
 // @icon                https://www.google.com/s2/favicons?sz=64&domain=google.com
 // @compatible          chrome
@@ -873,6 +873,7 @@
     function googleGPTshow(answer) {
         while (googleGPTdiv.firstChild) // clear all children
             googleGPTdiv.removeChild(googleGPTdiv.firstChild)
+        const fillColor = isDarkMode() ? 'white' : 'black'
 
         // Create/append '🤖 GoogleGPT'
         const appNameSpan = document.createElement('span')
@@ -892,20 +893,28 @@
         kudoAIlink.textContent = 'KudoAI'
         kudoAIspan.appendChild(kudoAIlink) ; googleGPTdiv.appendChild(kudoAIspan)
 
+        // Create/append about button
+        const aboutSpan = document.createElement('span'),
+              aboutSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg'),
+              aboutSVGpath = document.createElementNS('http://www.w3.org/2000/svg','path')
+        aboutSpan.className = 'about-btn' ; aboutSpan.title = messages.menuLabel_about
+        aboutSVG.setAttribute('width', '17') ; aboutSVG.setAttribute('viewBox', '0 0 56.693 56.693')
+        aboutSVGpath.setAttribute('d',
+            'M28.765,4.774c-13.562,0-24.594,11.031-24.594,24.594c0,13.561,11.031,24.594,24.594,24.594  c13.561,0,24.594-11.033,24.594-24.594C53.358,15.805,42.325,4.774,28.765,4.774z M31.765,42.913c0,0.699-0.302,1.334-0.896,1.885  c-0.587,0.545-1.373,0.82-2.337,0.82c-0.993,0-1.812-0.273-2.431-0.814c-0.634-0.551-0.954-1.188-0.954-1.891v-1.209  c0-0.703,0.322-1.34,0.954-1.891c0.619-0.539,1.438-0.812,2.431-0.812c0.964,0,1.75,0.277,2.337,0.82  c0.594,0.551,0.896,1.186,0.896,1.883V42.913z M38.427,24.799c-0.389,0.762-0.886,1.432-1.478,1.994  c-0.581,0.549-1.215,1.044-1.887,1.473c-0.643,0.408-1.248,0.852-1.798,1.315c-0.539,0.455-0.99,0.963-1.343,1.512  c-0.336,0.523-0.507,1.178-0.507,1.943v0.76c0,0.504-0.247,1.031-0.735,1.572c-0.494,0.545-1.155,0.838-1.961,0.871l-0.167,0.004  c-0.818,0-1.484-0.234-1.98-0.699c-0.532-0.496-0.801-1.055-0.801-1.658c0-1.41,0.196-2.611,0.584-3.572  c0.385-0.953,0.86-1.78,1.416-2.459c0.554-0.678,1.178-1.27,1.854-1.762c0.646-0.467,1.242-0.93,1.773-1.371  c0.513-0.428,0.954-0.885,1.312-1.354c0.328-0.435,0.489-0.962,0.489-1.608c0-1.066-0.289-1.83-0.887-2.334  c-0.604-0.512-1.442-0.771-2.487-0.771c-0.696,0-1.294,0.043-1.776,0.129c-0.471,0.083-0.905,0.223-1.294,0.417  c-0.384,0.19-0.745,0.456-1.075,0.786c-0.346,0.346-0.71,0.783-1.084,1.301c-0.336,0.473-0.835,0.83-1.48,1.062  c-0.662,0.239-1.397,0.175-2.164-0.192c-0.689-0.344-1.11-0.793-1.254-1.338c-0.135-0.5-0.135-1.025-0.002-1.557  c0.098-0.453,0.369-1.012,0.83-1.695c0.451-0.67,1.094-1.321,1.912-1.938c0.814-0.614,1.847-1.151,3.064-1.593  c1.227-0.443,2.695-0.668,4.367-0.668c1.648,0,3.078,0.249,4.248,0.742c1.176,0.496,2.137,1.157,2.854,1.967  c0.715,0.809,1.242,1.738,1.568,2.762c0.322,1.014,0.486,2.072,0.486,3.146C39.024,23.075,38.823,24.024,38.427,24.799z')
+        aboutSVGpath.setAttribute('fill', fillColor)
+        aboutSVG.appendChild(aboutSVGpath) ; aboutSpan.appendChild(aboutSVG) ; googleGPTdiv.appendChild(aboutSpan)
+
         // Create/append speak button
         const speakSpan = document.createElement('span'),
-              speakSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg'),
-              scheme = isDarkMode() ? 'dark' : 'light'
-        speakSpan.className = 'speak' ; speakSpan.title = messages.tooltip_playAnswer
-        speakSVG.setAttributeNS(null, 'viewBox', '0 0 32 32')
-        speakSVG.setAttributeNS(null, 'width', '22')
+              speakSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+        speakSpan.className = 'speak-btn' ; speakSpan.title = messages.tooltip_playAnswer
+        speakSpan.style.margin = '-0.099em 14px 0px' // fine-tune position
+        speakSVG.setAttributeNS(null, 'viewBox', '0 0 32 32') ; speakSVG.setAttributeNS(null, 'width', '22')
         const speakSVGpaths = [
-            { d: 'M24.5,26c2.881,-2.652 4.5,-6.249 4.5,-10c0,-3.751 -1.619,-7.348 -4.5,-10',
-              stroke: scheme == 'dark' ? 'white' : '#000', strokeWidth: '2px' },
-            { d: 'M22,20.847c1.281,-1.306 2,-3.077 2,-4.924c0,-1.846 -0.719,-3.617 -2,-4.923',
-              stroke: scheme == 'dark' ? 'white' : '#000', strokeWidth: '2px' },
+            { d: 'M24.5,26c2.881,-2.652 4.5,-6.249 4.5,-10c0,-3.751 -1.619,-7.348 -4.5,-10', stroke: fillColor, strokeWidth: '2px' },
+            { d: 'M22,20.847c1.281,-1.306 2,-3.077 2,-4.924c0,-1.846 -0.719,-3.617 -2,-4.923', stroke: fillColor, strokeWidth: '2px' },
             { d: 'M9.957,10.88c-0.605,0.625 -1.415,0.98 -2.262,0.991c-4.695,0.022 -4.695,0.322 -4.695,4.129c0,3.806 0,4.105 4.695,4.129c0.846,0.011 1.656,0.366 2.261,0.991c1.045,1.078 2.766,2.856 4.245,4.384c0.474,0.49 1.18,0.631 1.791,0.36c0.611,-0.272 1.008,-0.904 1.008,-1.604c0,-4.585 0,-11.936 0,-16.52c0,-0.7 -0.397,-1.332 -1.008,-1.604c-0.611,-0.271 -1.317,-0.13 -1.791,0.36c-1.479,1.528 -3.2,3.306 -4.244,4.384Z',
-              fill: scheme == 'dark' ? 'white' : '#000' }
+              fill: fillColor }
         ]
         speakSVGpaths.forEach(pathData => {
             const path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
@@ -941,8 +950,7 @@
               sendSVGline = document.createElementNS('http://www.w3.org/2000/svg', 'line'),
               sendSVGpolygon = document.createElementNS('http://www.w3.org/2000/svg', 'polygon')
         sendButton.className = 'send-button' ; sendButton.title = messages.tooltip_sendReply
-        sendSVG.setAttribute('viewBox', '0 0 24 24')
-        sendSVG.setAttribute('fill', 'none')
+        sendSVG.setAttribute('viewBox', '0 0 24 24') ; sendSVG.setAttribute('fill', 'none')
         sendSVG.setAttribute('stroke', 'currentColor') ; sendSVG.setAttribute('stroke-width', '2')
         sendSVG.setAttribute('stroke-linecap', 'round') ; sendSVG.setAttribute('stroke-linejoin', 'round')
         sendSVG.setAttribute('height', '1em') ; sendSVG.setAttribute('width', '1em')
@@ -972,6 +980,7 @@
         // Add listeners
         speakSVG.addEventListener('click', () => {
             chatgpt.speak(answer, { voice: 2, pitch: 1, speed: 1.5 })})
+        aboutSVG.addEventListener('click', launchAboutModal)
         form.addEventListener('keydown', handleEnter)
         form.addEventListener('submit', handleSubmit)
         chatTextarea.addEventListener('input', autosizeChatbar)
@@ -1136,7 +1145,7 @@
         + ( scheme == 'dark' ? '.googlegpt-container a { text-decoration: underline }' : '' ) // underline dark-mode links in alerts
         + '.app-name { font-size: 1.5rem ; font-weight: 700 }'
         + '.app-name a { color: ' + ( scheme == 'dark' ? 'white' : 'inherit' ) + ' ; text-decoration: none }'
-        + '.speak { float: right ; cursor: pointer ; position: relative ; top: 4px }'
+        + '.about-btn, .speak-btn { float: right ; cursor: pointer ; position: relative ; top: 4px }'
         + '.googlegpt-container .loading {'
             + 'padding-bottom: 17px ; color: #b6b8ba ; animation: pulse 2s cubic-bezier(.4,0,.6,1) infinite ;'
             + '-webkit-user-select: none ; -moz-user-select: none ; -ms-user-select: none ; user-select: none }'
