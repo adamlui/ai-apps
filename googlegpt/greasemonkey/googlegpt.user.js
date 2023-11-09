314d149a40332cabed6e4a2b8dc49d0ea65bcaf7
adamlui <adamlui@users.noreply.github.com>
2023-11-09 04:43:27 +0000

    Localized 'Fatter Sidebar' menu label ↞ [auto-sync from `kudoai/googlegpt`]

--- googlegpt/greasemonkey/googlegpt.user.js
+++ googlegpt/greasemonkey/googlegpt.user.js
@@ -152,7 +152,7 @@
 // @description:zu      Faka amaphawu ase-ChatGPT kuvaliwe i-Google Search (okwesikhashana ngu-GPT-4!)
 // @author              KudoAI
 // @namespace           https://kudoai.com
-// @version             2023.11.8.5
+// @version             2023.11.8.6
 // @license             MIT
 // @icon                https://www.google.com/s2/favicons?sz=64&domain=google.com
 // @compatible          chrome
@@ -451,7 +451,7 @@
 
         // Add command to toggle fatter sidebar
         const fsbLabel = ( config.fatterSidebar ? '🔛' : '↔️' ) + ' '
-                       + 'Fatter Sidebar'
+                       + messages.menuLabel_fatterSidebar
                        + state.separator + state.word[+!config.fatterSidebar]
         menuIDs.push(GM_registerMenuCommand(fsbLabel, () => {
             saveSetting('fatterSidebar', !config.fatterSidebar)
