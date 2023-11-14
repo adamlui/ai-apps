4ccf49910d7d6f475f86c081749e09920aa6e314
adamlui <adamlui@users.noreply.github.com>
2023-11-14 05:28:16 +0000

    Removed old `while` condition from reply topic prompt routine ↞ [auto-sync from `adamlui/chatgpt-infinity`]

--- chatgpt-infinity/edge/extension/popup/popup.js
+++ chatgpt-infinity/edge/extension/popup/popup.js
@@ -97,25 +97,22 @@
 
     // Add 'Reply Topic' click-listener
     replyTopicDiv.addEventListener('click', () => {
-        while (true) {
-            const replyTopic = prompt(chrome.i18n.getMessage('prompt_updateReplyTopic')
-                + ' (' + chrome.i18n.getMessage('prompt_orEnter') + ' \'ALL\'):', config.replyTopic)
-            if (replyTopic === null) break // user cancelled so do nothing
-            else {
-                const str_replyTopic = replyTopic.toString()
-                settings.save('replyTopic', !replyTopic || re_all.test(str_replyTopic) ? 'ALL' : str_replyTopic)
-                window.close() // popup
-                alert(chrome.i18n.getMessage('alert_replyTopicUpdated') + '!',
-                    chrome.i18n.getMessage('appName') + ' ' + chrome.i18n.getMessage('alert_willAnswer') + ' '
-                        + ( !replyTopic || re_all.test(str_replyTopic) ? chrome.i18n.getMessage('alert_onAllTopics')
-                                                                       : chrome.i18n.getMessage('alert_onTopicOf')
-                                                                           + ' ' + str_replyTopic ) + '!')
-                chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => { // check active tab
-                    if (new URL(tabs[0].url).hostname === 'chat.openai.com' && config.infinityMode) { // reboot active session
-                        chrome.tabs.sendMessage(tabs[0].id, { action: 'restartInNewChat' }) }
-                })
-                break
-    }}})
+        const replyTopic = prompt(chrome.i18n.getMessage('prompt_updateReplyTopic')
+            + ' (' + chrome.i18n.getMessage('prompt_orEnter') + ' \'ALL\'):', config.replyTopic)
+        if (replyTopic !== null) { // user didn't cancel
+            const str_replyTopic = replyTopic.toString()
+            settings.save('replyTopic', !replyTopic || re_all.test(str_replyTopic) ? 'ALL' : str_replyTopic)
+            window.close() // popup
+            alert(chrome.i18n.getMessage('alert_replyTopicUpdated') + '!',
+                chrome.i18n.getMessage('appName') + ' ' + chrome.i18n.getMessage('alert_willAnswer') + ' '
+                    + ( !replyTopic || re_all.test(str_replyTopic) ? chrome.i18n.getMessage('alert_onAllTopics')
+                                                                   : chrome.i18n.getMessage('alert_onTopicOf')
+                                                                       + ' ' + str_replyTopic ) + '!')
+            chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => { // check active tab
+                if (new URL(tabs[0].url).hostname === 'chat.openai.com' && config.infinityMode) { // reboot active session
+                    chrome.tabs.sendMessage(tabs[0].id, { action: 'restartInNewChat' }) }
+            })
+    }})
 
     // Add 'Reply Interval' click-listener
     replyIntervalDiv.addEventListener('click', () => {
