b5dd2a56f94d3463a97afa6415dad123712612da
adamlui <adamlui@users.noreply.github.com>
2023-11-16 01:45:18 +0000

    Bumped to 2.4.2 ↞ [auto-sync from `adamlui/chatgpt-infinity`]

--- chatgpt-infinity/chrome/extension/lib/chatgpt.js
+++ chatgpt-infinity/chrome/extension/lib/chatgpt.js
@@ -1,4 +1,4 @@
-// This library is a condensed version of chatgpt.js v2.4.1
+// This library is a condensed version of chatgpt.js v2.4.2
 // (c) 2023 KudoAI & contributors under the MIT license
 // Source: https://github.com/kudoai/chatgpt.js
 // Latest minified release: https://code.chatgptjs.org/chatgpt-latest.min.js
@@ -88,7 +88,7 @@ const chatgpt = {
                     + 'transition: opacity 0.1s cubic-bezier(.165,.84,.44,1), transform 0.2s cubic-bezier(.165,.84,.44,1) ;'
                     + `background-color: ${ scheme == 'dark' ? 'black' : 'white' } ;`
                     + ( scheme != 'dark' ? 'border: 1px solid rgba(0, 0, 0, 0.3) ;' : '' )
-                    + 'padding: 20px ; margin: 12px 23px ; border-radius: 15px ; box-shadow: 0 30px 60px rgba(0,0,0,.12) ;'
+                    + 'padding: 20px ; margin: 12px 23px ; border-radius: 15px ; box-shadow: 0 30px 60px rgba(0, 0, 0, .12) ;'
                     + ' -webkit-user-select: none ; -moz-user-select: none ; -ms-user-select: none ; user-select: none ; }'
                 + '.chatgpt-modal h2 { margin-bottom: 9px }'
                 + `.chatgpt-modal a { color: ${ scheme == 'dark' ? '#00cfff' : '#1e9ebb' }}`
@@ -209,7 +209,7 @@ const chatgpt = {
             modalContainer.style.display = '';
             setTimeout(() => { // delay non-0 opacity's for transition fx
                 modalContainer.style.backgroundColor = ( 
-                    `rgba(67, 70, 72, ${ scheme === 'dark' ? 0.62 : 0.18 })`);
+                    `rgba(67, 70, 72, ${ scheme === 'dark' ? 0.62 : 0 })`);
                 modalContainer.classList.add('animated'); }, 100);
         }
 
