1a150273739eddf70820c128290b1b8ab68780f5
adamlui <adamlui@users.noreply.github.com>
2023-11-18 06:42:04 +0000

    Added missing comma ↞ [auto-sync from `kudoai/bravegpt`]

--- bravegpt/greasemonkey/bravegpt.user.js
+++ bravegpt/greasemonkey/bravegpt.user.js
@@ -114,7 +114,7 @@
 // @description:zu      Engeza amaswazi aseChatGPT emugqa wokuqala weBrave Search (ibhulohwe nguGPT-4!)
 // @author              KudoAI
 // @namespace           https://kudoai.com
-// @version             2023.11.17.8
+// @version             2023.11.17.9
 // @license             MIT
 // @icon                https://media.bravegpt.com/images/bravegpt-icon48.png
 // @icon64              https://media.bravegpt.com/images/bravegpt-icon64.png
@@ -978,7 +978,7 @@
 
     // Stylize elements
     const braveGPTstyle = document.createElement('style'),
-          scheme = isDarkMode() ? 'dark' : 'light'
+          scheme = isDarkMode() ? 'dark' : 'light',
           isChromium = chatgpt.browser.isChromium()
     braveGPTstyle.innerText = (
           '.no-user-select { -webkit-user-select: none ; -moz-user-select: none ; -ms-user-select: none ; user-select: none }'
