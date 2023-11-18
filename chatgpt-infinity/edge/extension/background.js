636ac51621b8e90fe2597e619d913fad8309c5e7
adamlui <adamlui@users.noreply.github.com>
2023-11-18 05:08:06 +0000

    Loosened equality comparison ↞ [auto-sync from `adamlui/chatgpt-infinity`]

--- chatgpt-infinity/edge/extension/background.js
+++ chatgpt-infinity/edge/extension/background.js
@@ -3,7 +3,7 @@ const allowedHosts = ['chat.openai.com']
 // Add install/update actions
 chrome.runtime.onInstalled.addListener(details => {
     chrome.storage.local.set({ 'chatgptInfinity_extensionDisabled': false }) // auto-enable
-    if (details.reason === 'install') chrome.tabs.create({ url: 'https://chat.openai.com/' }) // open ChatGPT
+    if (details.reason == 'install') chrome.tabs.create({ url: 'https://chat.openai.com/' }) // open ChatGPT
 })
 
 // Sync extension state/settings when ChatGPT tab active
