44b2da442cf6ecef7f87c3335979b33cc05d124c
adamlui <adamlui@users.noreply.github.com>
2023-11-18 07:10:35 +0000

    Updated hash in chatgpt.js import URL ↞ [auto-sync from `adamlui/autoclear-chatgpt-history`]

--- autoclear-chatgpt-history/greasemonkey/autoclear-chatgpt-history.user.js
+++ autoclear-chatgpt-history/greasemonkey/autoclear-chatgpt-history.user.js
@@ -240,7 +240,7 @@
 // @compatible          qq
 // @match               *://chat.openai.com/*
 // @run-at              document-end
-// @require             https://cdn.jsdelivr.net/gh/kudoai/chatgpt.js@70905dcc82868c9043ae9f83fd5882dc543eebb8/dist/chatgpt-2.5.0.min.js
+// @require             https://cdn.jsdelivr.net/gh/kudoai/chatgpt.js@20e555cd51b70f4d9429f73e7ddddaa37ca0bda4/dist/chatgpt-2.5.0.min.js
 // @connect             raw.githubusercontent.com
 // @connect             greasyfork.org
 // @grant               GM_setValue
