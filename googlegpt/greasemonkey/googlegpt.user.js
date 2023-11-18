ce13175e97d6af5813a56ffc404a99de688e05c7
adamlui <adamlui@users.noreply.github.com>
2023-11-18 04:54:40 +0000

    Removed localization from title-check in `isBlockedbyCloudflare()` ↞ [auto-sync from `kudoai/googlegpt`]

--- googlegpt/greasemonkey/googlegpt.user.js
+++ googlegpt/greasemonkey/googlegpt.user.js
@@ -154,7 +154,7 @@
 // @description:zu      Faka amaphawu ase-ChatGPT kuvaliwe i-Google Search (okwesikhashana ngu-GPT-4!)
 // @author              KudoAI
 // @namespace           https://kudoai.com
-// @version             2023.11.17.3
+// @version             2023.11.17.4
 // @license             MIT
 // @icon                https://www.google.com/s2/favicons?sz=64&domain=google.com
 // @compatible          chrome
@@ -637,7 +637,7 @@
         try {
             const html = new DOMParser().parseFromString(resp, 'text/html'),
                   title = html.querySelector('title')
-            return title.innerText == ( messages.alert_justAmoment || 'Just a moment' ) + '...'
+            return title.innerText == 'Just a moment...'
         } catch (error) { return false }
     }
 
