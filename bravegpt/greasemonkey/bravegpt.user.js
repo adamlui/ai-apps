802589c84cb5e662c3bdd0240ef4e199ed823d0b
adamlui <adamlui@users.noreply.github.com>
2023-11-22 01:58:22 +0000

    Added `// @compatible kiwi` ↞ [auto-sync from `kudoai/bravegpt`]

--- bravegpt/greasemonkey/bravegpt.user.js
+++ bravegpt/greasemonkey/bravegpt.user.js
@@ -114,7 +114,7 @@
 // @description:zu      Engeza amaswazi aseChatGPT emugqa wokuqala weBrave Search (ibhulohwe nguGPT-4!)
 // @author              KudoAI
 // @namespace           https://kudoai.com
-// @version             2023.11.21.9
+// @version             2023.11.21.10
 // @license             MIT
 // @icon                https://media.bravegpt.com/images/bravegpt-icon48.png
 // @icon64              https://media.bravegpt.com/images/bravegpt-icon64.png
@@ -127,6 +127,7 @@
 // @compatible          librewolf
 // @compatible          ghost
 // @compatible          qq
+// @compatible          kiwi
 // @match               *://search.brave.com/search*
 // @include             https://auth0.openai.com
 // @connect             raw.githubusercontent.com
