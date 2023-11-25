0c515048465d8ba0f654a9880d146cb80f350c61
adamlui <adamlui@users.noreply.github.com>
2023-11-25 14:01:53 +0000

    Added `config.appName`, renamed `config.prefix`
    ... to `config.keyPrefix` ↞ [auto-sync from `adamlui/chatgpt-infinity`]

--- chatgpt-infinity/opera/extension/lib/settings-utils.js
+++ chatgpt-infinity/opera/extension/lib/settings-utils.js
@@ -1,5 +1,5 @@
 const config = {
-    prefix: 'chatgptInfinity', appSymbol: '∞',
+    appName: 'ChatGPT Infinity', appSymbol: '∞', keyPrefix = 'chatGPTinfinity'
     gitHubURL: 'https://github.com/adamlui/chatgpt-infinity' }
 config.assetHostURL = config.gitHubURL.replace('github.com', 'raw.githubusercontent.com') + '/main/'
 
@@ -10,12 +10,12 @@ const settings = {
             Array.isArray(arguments[0]) ? arguments[0] : Array.from(arguments))
         return Promise.all(keys.map(key => { // resolve promise when all keys load
             return new Promise(resolve => { // resolve promise when single key value loads
-                chrome.storage.local.get(`${ config.prefix }_${ key }`, result => { // load from Chrome
-                    config[key] = result[`${ config.prefix }_${ key }`] || false ; resolve()
+                chrome.storage.local.get(`${ config.keyPrefix }_${ key }`, result => { // load from Chrome
+                    config[key] = result[`${ config.keyPrefix }_${ key }`] || false ; resolve()
     })})}))},
 
     save: function(key, value) {
-        const obj = {} ; obj[`${ config.prefix }_${ key }`] = value
+        const obj = {} ; obj[`${ config.keyPrefix }_${ key }`] = value
         chrome.storage.local.set(obj) // save to Chrome
         config[key] = value // save to memory
     }
