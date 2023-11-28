4d1e40ba8623e95b297b90d1ab8ebfb40e576034
Adam Lui 刘展鹏 <adamlui@pm.me>
2023-11-28 01:08:25 -0800

    Disabled `no-unexpected-multiline`

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
     'extends': ['eslint:recommended'],
     'ignorePatterns': ['**/*.md'],
     'rules': {
-        'indent': 'off',
+        'indent': 'off', 'no-unexpected-multiline': 'off', // allow whitespace anywhere
         'quotes': ['error', 'single', { 'avoidEscape': true, 'allowTemplateLiterals': true }], // enforce single quotes for string literals
         'key-spacing': ['error', { 'beforeColon': false, 'afterColon': true }], // enforce spacing in object properties
         'comma-dangle': ['error', 'never'], // enforce no trailing commas in arrays or objects
