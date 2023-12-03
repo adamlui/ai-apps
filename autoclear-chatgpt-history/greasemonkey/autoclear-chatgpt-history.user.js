b08a867b48f1f651d13c0023a2bae21a863601aa
adamlui <adamlui@users.noreply.github.com>
2023-12-03 12:23:52 +0000

    Made FF scrollbar skinny to not crop toggle
    ± Renamed `chatgptAlertStyle` to `tweaksStyle`
    ± Renamed `alertStyleUpdated` to `tweaksStyleUpdated`
    ± Renamed `#chatgpt-alert-override-style` to `#tweaks-style`
    - Removed unneeded `await chatgpt.isLoaded()` ↞ [auto-sync from `adamlui/autoclear-chatgpt-history`]

--- autoclear-chatgpt-history/greasemonkey/autoclear-chatgpt-history.user.js
+++ autoclear-chatgpt-history/greasemonkey/autoclear-chatgpt-history.user.js
@@ -225,7 +225,7 @@
 // @description:zu      Ziba itshala lokucabanga okuzoshintshwa ngokuzenzakalelayo uma ukubuka chat.openai.com
 // @author              Adam Lui
 // @namespace           https://github.com/adamlui
-// @version             2023.11.28
+// @version             2023.12.3
 // @license             MIT
 // @icon                https://raw.githubusercontent.com/adamlui/userscripts/master/chatgpt/media/icons/openai-favicon48.png
 // @icon64              https://raw.githubusercontent.com/adamlui/userscripts/master/chatgpt/media/icons/openai-favicon64.png
@@ -301,23 +301,21 @@
         separator: getUserscriptManager() == 'Tampermonkey' ? ' — ' : ': ' }
     let menuIDs = [] ; registerMenu() // create browser toolbar menu
 
-    // Wait for site load + determine UI for toggle routines
-    await chatgpt.isLoaded()
-
-    // Stylize alerts (if style missing or outdated)
-    const alertStyleUpdated = 20231110; // datestamp of last edit for this file's `chatgptAlertStyle` 
-    let chatgptAlertStyle = document.getElementById('chatgpt-alert-override-style'); // try to select existing style
-    if (!chatgptAlertStyle || parseInt(chatgptAlertStyle.getAttribute('last-updated'), 10) < alertStyleUpdated) { // if missing or outdated
-        if (!chatgptAlertStyle) { // outright missing, create/id/attr/append it first
-            chatgptAlertStyle = document.createElement('style'); chatgptAlertStyle.id = 'chatgpt-alert-override-style';
-            chatgptAlertStyle.setAttribute('last-updated', alertStyleUpdated.toString());
-            document.head.appendChild(chatgptAlertStyle);
+    // Add/update tweaks style
+    const tweaksStyleUpdated = 2023123 // datestamp of last edit for this file's `tweaksStyle` 
+    let tweaksStyle = document.getElementById('tweaks-style') // try to select existing style
+    if (!tweaksStyle || parseInt(tweaksStyle.getAttribute('last-updated'), 10) < tweaksStyleUpdated) { // if missing or outdated
+        if (!tweaksStyle) { // outright missing, create/id/attr/append it first
+            tweaksStyle = document.createElement('style') ; tweaksStyle.id = 'tweaks-style'
+            tweaksStyle.setAttribute('last-updated', tweaksStyleUpdated.toString())
+            document.head.appendChild(tweaksStyle)
         }
-        chatgptAlertStyle.innerText = (
+        tweaksStyle.innerText = (
             '.chatgpt-modal button {'
               + 'font-size: 0.77rem ; text-transform: uppercase ;'
               + 'border-radius: 0 !important ; padding: 5px !important ; min-width: 102px }'
           + '.modal-buttons { margin-left: -13px !important }'
+          + '* { scrollbar-width: thin }' // make FF scrollbar skinny to not crop toggle
         )
     }
 
