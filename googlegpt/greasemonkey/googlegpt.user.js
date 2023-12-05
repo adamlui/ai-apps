44728f2eb35c61c42b2f961efa3edc90565a7094
adamlui <adamlui@users.noreply.github.com>
2023-12-05 01:10:35 +0000

    Replaced icon, added `// @icon64` ↞ [auto-sync from `kudoai/googlegpt`]

--- googlegpt/greasemonkey/googlegpt.user.js
+++ googlegpt/greasemonkey/googlegpt.user.js
@@ -154,9 +154,10 @@
 // @description:zu      Faka amaphawu ase-ChatGPT kuvaliwe i-Google Search
 // @author              KudoAI
 // @namespace           https://kudoai.com
-// @version             2023.12.4.4
+// @version             2023.12.4.5
 // @license             MIT
-// @icon                https://www.google.com/s2/favicons?sz=64&domain=google.com
+// @icon                https://raw.githubusercontent.com/KudoAI/googlegpt/main/media/images/icons/googlegpt/beta/black/icon48.png
+// @icon64              https://raw.githubusercontent.com/KudoAI/googlegpt/main/media/images/icons/googlegpt/beta/black/icon64.png
 // @compatible          chrome
 // @compatible          firefox
 // @compatible          edge
