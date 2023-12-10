4d52132f4bd51bf88fd0c6af73badf02e4ff6b5d
adamlui <adamlui@users.noreply.github.com>
2023-12-10 09:50:16 +0000

    Widened sidebar content selector's scope for Sticky Sidebar ↞ [auto-sync from `kudoai/bravegpt`]

--- bravegpt/greasemonkey/bravegpt.user.js
+++ bravegpt/greasemonkey/bravegpt.user.js
@@ -114,7 +114,7 @@
 // @description:zu      Engeza amaswazi aseChatGPT emugqa wokuqala weBrave Search (ibhulohwe nguGPT-4!)
 // @author              KudoAI
 // @namespace           https://kudoai.com
-// @version             2023.12.10
+// @version             2023.12.10.1
 // @license             MIT
 // @icon                https://media.bravegpt.com/images/bravegpt-icon48.png
 // @icon64              https://media.bravegpt.com/images/bravegpt-icon64.png
@@ -1286,7 +1286,7 @@
           wsbStyle = 'main.main-column, aside.sidebar { max-width: 521px !important }'
                    + '.bravegpt { width: 521px }',
           ssbStyle = '.bravegpt { position: sticky ; top: 14px }'
-                   + '.bravegpt ~ div { display: none }' // hide sidebar contents
+                   + '.bravegpt ~ * { display: none }' // hide sidebar contents
     updateTweaksStyle() ; document.head.append(tweaksStyle)
 
     // Create/classify BraveGPT container
