92d15b127a1fc63afc987a768258c964fd124a05
Adam Lui 刘展鹏 <adamlui@pm.me>
2024-02-04 06:32:05 -0800

    Removed `ignorePatterns`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,5 @@
 module.exports = {
     'extends': ['eslint:recommended'],
-    'ignorePatterns': ['**/*.md'],
     'rules': {
         'indent': 'off', 'no-unexpected-multiline': 'off', // allow whitespace anywhere
         'quotes': ['error', 'single', { 'avoidEscape': true, 'allowTemplateLiterals': true }], // enforce single quotes for string literals
