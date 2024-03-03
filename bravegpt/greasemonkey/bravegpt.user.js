d1af480d3e6155a5469909c1bd39ae450fd22b66
adamlui <adamlui@users.noreply.github.com>
2024-03-03 10:51:22 +0000

    Removed title attr's from Sticky/Wider Sidebar button spans
    ... to disable browser-native tooltips
    
    + Added line breaks to `updateSSBsvg()` def for readability ↞ [auto-sync from `KudoAI/bravegpt`]

--- bravegpt/greasemonkey/bravegpt.user.js
+++ bravegpt/greasemonkey/bravegpt.user.js
@@ -114,7 +114,7 @@
 // @description:zu      Engeza amaswazi aseChatGPT emugqa wokuqala weBrave Search (ibhulohwe nguGPT-4!)
 // @author              KudoAI
 // @namespace           https://kudoai.com
-// @version             2024.3.1.1
+// @version             2024.3.3
 // @license             MIT
 // @icon                https://media.bravegpt.com/images/icons/bravegpt/icon48.png
 // @icon64              https://media.bravegpt.com/images/icons/bravegpt/icon64.png
@@ -449,8 +449,6 @@
         // Set SVG attributes
         for (const [attr, value] of [['width', 18], ['height', 18], ['viewBox', '8 8 20 20']])
             wsbSVG.setAttribute(attr, value)
-        wsbSpan.title = ( config.widerSidebar ? `${ messages.prefix_exit || 'Exit' } ` :  '' )
-                      + messages.menuLabel_widerSidebar || 'Wider Sidebar'
 
         // Update SVG elements
         while (wsbSVG.firstChild) { wsbSVG.removeChild(wsbSVG.firstChild) }
@@ -460,6 +458,7 @@
     }
 
     function updateSSBsvg() {
+
         // Init span/SVG/paths
         const ssbSpan = appDiv.querySelector('#ssb-btn'),
               ssbSVG = ssbSpan.querySelector('svg')
@@ -469,11 +468,11 @@
         const ssbOFFpaths = [
             createSVGpath({
                 d: 'M9.828.722a.5.5 0 0 1 .354.146l4.95 4.95a.5.5 0 0 1 0 .707c-.48.48-1.072.588-1.503.588-.177 0-.335-.018-.46-.039l-3.134 3.134a5.927 5.927 0 0 1 .16 1.013c.046.702-.032 1.687-.72 2.375a.5.5 0 0 1-.707 0l-2.829-2.828-3.182 3.182c-.195.195-1.219.902-1.414.707-.195-.195.512-1.22.707-1.414l3.182-3.182-2.828-2.829a.5.5 0 0 1 0-.707c.688-.688 1.673-.767 2.375-.72a5.922 5.922 0 0 1 1.013.16l3.134-3.133a2.772 2.772 0 0 1-.04-.461c0-.43.108-1.022.589-1.503a.5.5 0 0 1 .353-.146zm.122 2.112v-.002.002zm0-.002v.002a.5.5 0 0 1-.122.51L6.293 6.878a.5.5 0 0 1-.511.12H5.78l-.014-.004a4.507 4.507 0 0 0-.288-.076 4.922 4.922 0 0 0-.765-.116c-.422-.028-.836.008-1.175.15l5.51 5.509c.141-.34.177-.753.149-1.175a4.924 4.924 0 0 0-.192-1.054l-.004-.013v-.001a.5.5 0 0 1 .12-.512l3.536-3.535a.5.5 0 0 1 .532-.115l.096.022c.087.017.208.034.344.034.114 0 .23-.011.343-.04L9.927 2.028c-.029.113-.04.23-.04.343a1.779 1.779 0 0 0 .062.46z' }) ]
+
         // Set SVG attributes
         for (const [attr, value] of [['width', 16], ['height', 16], ['viewBox', '0 0 16 16']])
             ssbSVG.setAttribute(attr, value)
-        ssbSpan.title = ( config.stickySidebar ? `${ messages.prefix_exit || 'Exit' } ` :  '' )
-                      + messages.menuLabel_stickySidebar || 'Sticky Sidebar'
+
         // Update SVG elements
         while (ssbSVG.firstChild) { ssbSVG.removeChild(ssbSVG.firstChild) }
         const ssbSVGpaths = config.stickySidebar ? ssbONpaths : ssbOFFpaths
