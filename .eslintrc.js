c41fe3fadd5958ab0029e619f69f9f92db359633
Adam Lui 刘展鹏 <adamlui@pm.me>
2024-03-29 00:12:52 -0700

    Added `'ipv4'` to `globals`

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
         'no-useless-escape': 'off' // allow all escape chars cause ESLint sucks at detecting truly useless ones
     },
     'globals': { 'chatgpt': 'readonly', 'chrome': 'readonly', 'CryptoJS': 'readonly',
-                 'syncExtension': 'writable', 'GM_cookie': 'readonly' },
+                 'syncExtension': 'writable', 'GM_cookie': 'readonly', 'ipv4': 'readonly' },
     'parserOptions': { 'ecmaVersion': 2022, 'sourceType': 'script' },
     'overrides': [
         { 'files': ['**/lib*/*.js'], 'parserOptions': { 'sourceType': 'module' }},
