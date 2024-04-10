34c4604e9d86d33cb73a2c2f7bb224486be9c3e4
adamlui <adamlui@users.noreply.github.com>
2024-04-10 07:45:39 +0000

    Bumped generate-ip to 2.2.3 ↞ [auto-sync from `KudoAI/duckduckgpt`]

--- duckduckgpt/greasemonkey/duckduckgpt.user.js
+++ duckduckgpt/greasemonkey/duckduckgpt.user.js
@@ -152,7 +152,7 @@
 // @description:zu      Faka amaphawu ase-ChatGPT kuvaliwe i-DuckDuckGo Search (okwesikhashana ngu-GPT-4!)
 // @author              KudoAI
 // @namespace           https://kudoai.com
-// @version             2024.4.7
+// @version             2024.4.10
 // @license             MIT
 // @icon                https://media.ddgpt.com/images/icons/duckduckgpt/icon48.png
 // @icon64              https://media.ddgpt.com/images/icons/duckduckgpt/icon64.png
@@ -180,7 +180,7 @@
 // @require             https://cdn.jsdelivr.net/npm/katex@0.16.10/dist/katex.min.js#sha256-n0UwfFeU7SR6DQlfOmLlLvIhWmeyMnIDp/2RmVmuedE=
 // @require             https://cdn.jsdelivr.net/npm/katex@0.16.7/dist/contrib/auto-render.min.js#sha256-nLjaz8CGwpZsnsS6VPSi3EO3y+KzPOwaJ0PYhsf7R6c=
 // @require             https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js#sha256-jjsBF/TfS+RSwLavW48KCs+dSt4j0I1V1+MSryIHd2I=
-// @require             https://cdn.jsdelivr.net/npm/generate-ip@2.2.1/dist/generate-ip.min.js#sha256-mEN+gb14oCbNFM33ZU0Jqxj9vXEkBln+5SeAa/44U+s=
+// @require             https://cdn.jsdelivr.net/npm/generate-ip@2.2.3/dist/generate-ip.min.js#sha256-9+FkbSAnp7CzyH9rXeQyYSK3E+rkOhE5NTq2xaahXJY=
 // @grant               GM_getValue
 // @grant               GM_setValue
 // @grant               GM_deleteValue
