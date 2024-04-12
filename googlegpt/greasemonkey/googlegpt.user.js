dfffeb9c0b18f497c25e1066826389343e14fc7c
adamlui <adamlui@users.noreply.github.com>
2024-04-12 10:24:20 +0000

    Bumped generate-ip to 2.2.4 ↞ [auto-sync from `KudoAI/googlegpt`]

--- googlegpt/greasemonkey/googlegpt.user.js
+++ googlegpt/greasemonkey/googlegpt.user.js
@@ -154,7 +154,7 @@
 // @description:zu      Faka amaphawu ase-ChatGPT kuvaliwe i-Google Search
 // @author              KudoAI
 // @namespace           https://kudoai.com
-// @version             2024.4.10
+// @version             2024.4.12
 // @license             MIT
 // @icon                https://media.googlegpt.io/images/icons/googlegpt/black/icon48.png
 // @icon64              https://media.googlegpt.io/images/icons/googlegpt/black/icon64.png
@@ -371,7 +371,7 @@
 // @require             https://cdn.jsdelivr.net/npm/katex@0.16.10/dist/katex.min.js#sha256-n0UwfFeU7SR6DQlfOmLlLvIhWmeyMnIDp/2RmVmuedE=
 // @require             https://cdn.jsdelivr.net/npm/katex@0.16.7/dist/contrib/auto-render.min.js#sha256-nLjaz8CGwpZsnsS6VPSi3EO3y+KzPOwaJ0PYhsf7R6c=
 // @require             https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js#sha256-jjsBF/TfS+RSwLavW48KCs+dSt4j0I1V1+MSryIHd2I=
-// @require             https://cdn.jsdelivr.net/npm/generate-ip@2.2.3/dist/generate-ip.min.js#sha256-9+FkbSAnp7CzyH9rXeQyYSK3E+rkOhE5NTq2xaahXJY=
+// @require             https://cdn.jsdelivr.net/npm/generate-ip@2.2.4/dist/generate-ip.min.js#sha256-FJO9oo6Fpy0nY5ak5NWIEg8BtX+As53nDJEYrODHRPM=
 // @grant               GM_getValue
 // @grant               GM_setValue
 // @grant               GM_deleteValue
