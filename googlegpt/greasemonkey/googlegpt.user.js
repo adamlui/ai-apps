b5247735aa4b99f08b4bd85fe664de906dfea181
adamlui <adamlui@users.noreply.github.com>
2024-04-28 11:46:03 +0000

    Loosened comparison operators for smaller filesize ↞ [auto-sync from `KudoAI/googlegpt`]

--- googlegpt/greasemonkey/googlegpt.user.js
+++ googlegpt/greasemonkey/googlegpt.user.js
@@ -154,7 +154,7 @@
 // @description:zu      Faka amaphawu ase-ChatGPT kuvaliwe i-Google Search
 // @author              KudoAI
 // @namespace           https://kudoai.com
-// @version             2024.4.28.2
+// @version             2024.4.28.4
 // @license             MIT
 // @icon                https://media.googlegpt.io/images/icons/googlegpt/black/icon48.png
 // @icon64              https://media.googlegpt.io/images/icons/googlegpt/black/icon64.png
@@ -487,7 +487,7 @@
             while (true) {
                 let replyLanguage = prompt(
                     ( msgs.prompt_updateReplyLang || 'Update reply language' ) + ':', config.replyLanguage)
-                if (replyLanguage === null) break // user cancelled so do nothing
+                if (replyLanguage == null) break // user cancelled so do nothing
                 else if (!/\d/.test(replyLanguage)) {
                     replyLanguage = ( // auto-case for menu/alert aesthetics
                         [2, 3].includes(replyLanguage.length) || replyLanguage.includes('-') ? replyLanguage.toUpperCase()
@@ -833,7 +833,7 @@
                                 // Skip disqualified groups
                                 if (/^self$/i.test(groupName) && !re_appName.test(campaignName) // self-group for other apps
                                     || re_appName.test(campaignName) && !/^self$/i.test(groupName) // non-self group for this app
-                                    || adGroup.active === false // group explicitly disabled
+                                    || adGroup.active == false // group explicitly disabled
                                     || adGroup.targetBrowsers && // target browser(s) exist...
                                         !adGroup.targetBrowsers.some( // ...but doesn't match user's
                                             browser => new RegExp(browser, 'i').test(navigator.userAgent))
@@ -843,8 +843,8 @@
                                 ) continue // to next group
 
                                 // Filter out inactive ads, pick random active one
-                                const activeAds = adGroup.ads.filter(ad => ad.active !== false)
-                                if (activeAds.length === 0) continue // to next group since no ads active
+                                const activeAds = adGroup.ads.filter(ad => ad.active != false)
+                                if (activeAds.length == 0) continue // to next group since no ads active
                                 const chosenAd = activeAds[Math.floor(Math.random() * activeAds.length)] // random active one
 
                                 // Build destination URL
@@ -879,7 +879,7 @@
 
         function shuffle(list) {
             let currentIdx = list.length, tempValue, randomIdx
-            while (currentIdx !== 0) { // elements remain to be shuffled
+            while (currentIdx != 0) { // elements remain to be shuffled
                 randomIdx = Math.floor(Math.random() * currentIdx) ; currentIdx -= 1
                 tempValue = list[currentIdx] ; list[currentIdx] = list[randomIdx] ; list[randomIdx] = tempValue
             }
@@ -1178,16 +1178,16 @@
 
         function onLoad() { // process text
             return async event => {
-                if (event.status !== 200) {
+                if (event.status != 200) {
                     appError('Event status: ' + event.status)
                     appError('Event response: ' + event.responseText)
                     if (config.proxyAPIenabled && getShowReply.attemptCnt < proxyEndpoints.length)
                         retryDiffHost()
-                    else if (event.status === 401 && !config.proxyAPIenabled) {
+                    else if (event.status == 401 && !config.proxyAPIenabled) {
                         GM_deleteValue(config.keyPrefix + '_openAItoken') ; appAlert('login') }
-                    else if (event.status === 403)
+                    else if (event.status == 403)
                         appAlert(config.proxyAPIenabled ? 'suggestOpenAI' : 'checkCloudflare')
-                    else if (event.status === 429) appAlert('tooManyRequests')
+                    else if (event.status == 429) appAlert('tooManyRequests')
                     else appAlert(config.proxyAPIenabled ? 'suggestOpenAI' : 'suggestProxy')
                 } else if (endpoint.includes('openai')) {
                     if (event.response) {
@@ -1234,7 +1234,7 @@
                 appLogoImg.width = isMobile ? 197 : isFirefox ? 127 : 125
                 appLogoImg.style.cssText = (
                     appLogoImg.loaded ? `position: relative ; top: ${ isMobile ? 4 : isFirefox ? 3 : 2 }px`
-                                          + ( isMobile ? '; margin-left: 1px' : '' ) : '' )
+                                      + ( isMobile ? '; margin-left: 1px' : '' ) : '' )
                 return appLogoImg
             } else { // create/fill/size/return app name span
                 const appNameSpan = document.createElement('span')
@@ -1340,7 +1340,7 @@
                     + encodeURIComponent(securePayload),
                 method: 'GET', responseType: 'arraybuffer',
                 onload: async response => {
-                    if (response.status !== 200) chatgpt.speak(answer, { voice: 2, pitch: 1, speed: 1.5 })
+                    if (response.status != 200) chatgpt.speak(answer, { voice: 2, pitch: 1, speed: 1.5 })
                     else {
                         const audioContext = new (window.AudioContext || window.webkitAudioContext)()
                         audioContext.decodeAudioData(response.response, buffer => {
