38e31c77af786d8097eee16cd37c7486cf56d789
Adam Lui 刘展鹏 <adamlui@pm.me>
2024-04-29 07:04:56 -0700

    Excepted single quote enforcement when using backticks to avoid escaping quotes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,7 @@ export default [
     {
         rules: {
             'indent': 'off', 'no-unexpected-multiline': 'off', // allow whitespace anywhere
-            'quotes': ['error', 'single'], // enforce single quotes for string literals
+            'quotes': ['error', 'single', { 'allowTemplateLiterals': true }], // enforce single quotes except backticks to avoid escaping quotes
             'key-spacing': ['error', { 'beforeColon': false, 'afterColon': true }], // enforce spacing in object properties
             'comma-dangle': ['error', 'never'], // enforce no trailing commas in arrays or objects
             'no-async-promise-executor': 'off', // allow promise executor functions to be async (to accomodate await lines)
