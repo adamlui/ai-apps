b7f6cf1b6323ea0d5d72bf28d0b1c950fa95d95b
adamlui <adamlui@users.noreply.github.com>
2024-05-01 18:09:10 +0000

    Bumped chatgpt.js to 2.6.9 + generate-ip to 2.2.12 ↞ [auto-sync from `KudoAI/bravegpt`]

--- bravegpt/greasemonkey/bravegpt.user.js
+++ bravegpt/greasemonkey/bravegpt.user.js
@@ -114,7 +114,7 @@
 // @description:zu      Engeza amaswazi aseChatGPT emugqa wokuqala weBrave Search (ibhulohwe nguGPT-4!)
 // @author              KudoAI
 // @namespace           https://kudoai.com
-// @version             2024.4.28.3
+// @version             2024.5.1
 // @license             MIT
 // @icon                https://media.bravegpt.com/images/icons/bravegpt/icon48.png
 // @icon64              https://media.bravegpt.com/images/icons/bravegpt/icon64.png
@@ -138,11 +138,11 @@
 // @connect             api.openai.com
 // @connect             fanyi.sogou.com
 // @connect             api.aigcfun.com
-// @require             https://cdn.jsdelivr.net/npm/@kudoai/chatgpt.js@2.6.8/dist/chatgpt.min.js#sha256-veeaKpmLF7CZ6kxNfn8SMjD36ijLffGCzel9G4ykpPI=
+// @require             https://cdn.jsdelivr.net/npm/@kudoai/chatgpt.js@2.6.9/dist/chatgpt.min.js#sha256-pl2y5K/m1OovCuCUxK8yh2aA+y+A9uesk3HMBRBQSiE=
 // @require             https://cdn.jsdelivr.net/npm/katex@0.16.10/dist/katex.min.js#sha256-n0UwfFeU7SR6DQlfOmLlLvIhWmeyMnIDp/2RmVmuedE=
 // @require             https://cdn.jsdelivr.net/npm/katex@0.16.7/dist/contrib/auto-render.min.js#sha256-nLjaz8CGwpZsnsS6VPSi3EO3y+KzPOwaJ0PYhsf7R6c=
 // @require             https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js#sha256-jjsBF/TfS+RSwLavW48KCs+dSt4j0I1V1+MSryIHd2I=
-// @require             https://cdn.jsdelivr.net/npm/generate-ip@2.2.10/dist/generate-ip.min.js#sha256-zbpxOSObRfvoZAdAXnLF9XrKFT6DT0zYhUN6qOzGw8c=
+// @require             https://cdn.jsdelivr.net/npm/generate-ip@2.2.12/dist/generate-ip.min.js#sha256-vCFZlcQIRxmB8f4O5qwMsjCr/YyaCfGoh1/heh+GF+w=
 // @grant               GM_getValue
 // @grant               GM_setValue
 // @grant               GM_deleteValue
