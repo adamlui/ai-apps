d42a6ec2f26ad67d5670f180a81a5c5184cc3805
adamlui <adamlui@users.noreply.github.com>
2024-05-15 23:46:08 +0000

    Removed no longer needed first button padding tweak from `insertToggle()` ↞ [auto-sync from `adamlui/chatgpt-auto-refresh`]

--- chatgpt-auto-refresh/greasemonkey/chatgpt-auto-refresh.user.js
+++ chatgpt-auto-refresh/greasemonkey/chatgpt-auto-refresh.user.js
@@ -220,7 +220,7 @@
 // @description:zu      *NGOKUPHEPHA* susa ukusetha kabusha ingxoxo yemizuzu eyi-10 + amaphutha enethiwekhi ahlala njalo + Ukuhlolwa kwe-Cloudflare ku-ChatGPT.
 // @author              Adam Lui
 // @namespace           https://github.com/adamlui
-// @version             2024.5.15.1
+// @version             2024.5.15.2
 // @license             MIT
 // @match               *://chatgpt.com/*
 // @match               *://chat.openai.com/*
@@ -567,9 +567,6 @@
     async function insertToggle() {
         await chatgpt.history.isLoaded()
 
-        // Select sidebar elems
-        const firstButton = document.querySelector('nav a[href="/"]')
-
         // Insert toggle
         const parentToInsertInto = document.querySelector('nav > div:not(.invisible)'),
               childToInsertBefore = parentToInsertInto.children[1]
@@ -577,7 +574,6 @@
             try { parentToInsertInto.insertBefore(navToggleDiv, childToInsertBefore) } catch (err) {}
 
         // Tweak styles
-        firstButton.parentNode.parentNode.style.paddingBottom = '0'
         parentToInsertInto.style.backgroundColor = ( // hide transparency revealing chat log
             chatgpt.isDarkMode() ? '#0d0d0d' : '#f9f9f9' )
         navToggleDiv.style.paddingLeft = '8px'
