3fe617917ebdcd4a569b7cf00b723a0c28b4a97d
adamlui <adamlui@users.noreply.github.com>
2024-05-16 13:13:58 +0000

    Updated latest asset commit hash in `config.asssetHostURL` ↞ [auto-sync from `adamlui/chatgpt-widescreen/greasemonkey`]

--- chatgpt-widescreen/greasemonkey/chatgpt-widescreen-mode.user.js
+++ chatgpt-widescreen/greasemonkey/chatgpt-widescreen-mode.user.js
@@ -222,7 +222,7 @@
 // @description:zu      Engeza izinhlobo zezimodi ze-Widescreen + Fullscreen ku-ChatGPT ukuze kube nokubonakala + ukuncitsha ukusukela
 // @author              Adam Lui
 // @namespace           https://github.com/adamlui
-// @version             2024.5.16.3
+// @version             2024.5.16.4
 // @license             MIT
 // @compatible          chrome
 // @compatible          firefox
@@ -271,7 +271,7 @@
     config.updateURL = config.greasyForkURL.replace('https://', 'https://update.')
         .replace(/(\d+)-?([a-zA-Z-]*)$/, (_, id, name) => `${ id }/${ !name ? 'script' : name }.meta.js`)
     config.supportURL = config.gitHubURL + '/issues/new'
-    config.assetHostURL = config.gitHubURL.replace('github.com', 'cdn.jsdelivr.net/gh') + '@49856ac/'
+    config.assetHostURL = config.gitHubURL.replace('github.com', 'cdn.jsdelivr.net/gh') + '@11a0711/'
     loadSetting('fullerWindows', 'fullWindow', 'hiddenFooter', 'hiddenHeader', 'notifDisabled',
                 'ncbDisabled', 'tcbDisabled', 'widerChatbox', 'wideScreen')
 
