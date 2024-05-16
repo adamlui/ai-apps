a0c0e89e8b3b83e6e7f4ebfb117f45e5eab4a00f
adamlui <adamlui@users.noreply.github.com>
2024-05-16 00:36:39 +0000

    Updated `version` to `2024.5.15` ↞ [auto-sync from `adamlui/chatgpt-infinity/greasemonkey`]

--- chatgpt-infinity/greasemonkey/chatgpt-infinity.user.js
+++ chatgpt-infinity/greasemonkey/chatgpt-infinity.user.js
@@ -199,7 +199,7 @@
 // @description:zh-TW   從無所不知的 ChatGPT 生成無窮無盡的答案 (用任何語言!)
 // @author              Adam Lui
 // @namespace           https://github.com/adamlui
-// @version             2024.5.15.4
+// @version             2024.5.15.5
 // @license             MIT
 // @match               *://chatgpt.com/*
 // @match               *://chat.openai.com/*
