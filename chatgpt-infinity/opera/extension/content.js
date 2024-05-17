196ae763f68382b484241b92c31a3bb80ea57041
adamlui <adamlui@users.noreply.github.com>
2024-05-17 11:28:38 +0000

    Replaced `.no-click-transform` assignment w/ direct selector to reduce complexity ↞ [auto-sync from `chatgpt-infinity/chrome`]

--- chatgpt-infinity/opera/extension/content.js
+++ chatgpt-infinity/opera/extension/content.js
@@ -51,6 +51,25 @@
             if (config.infinityMode) infinityMode.deactivate()
     })}
 
+    // Add/update tweaks style
+    const tweaksStyleUpdated = 202405171 // datestamp of last edit for this file's `tweaksStyle`
+    let tweaksStyle = document.getElementById('tweaks-style') // try to select existing style
+    if (!tweaksStyle || parseInt(tweaksStyle.getAttribute('last-updated'), 10) < tweaksStyleUpdated) { // if missing or outdated
+        if (!tweaksStyle) { // outright missing, create/id/attr/append it first
+            tweaksStyle = document.createElement('style') ; tweaksStyle.id = 'tweaks-style'
+            tweaksStyle.setAttribute('last-updated', tweaksStyleUpdated.toString())
+            document.head.append(tweaksStyle)
+        }
+        tweaksStyle.innerText = (
+            '.chatgpt-modal button {'
+              + 'font-size: 0.77rem ; text-transform: uppercase ;'
+              + 'border-radius: 0 !important ; padding: 5px !important ; min-width: 102px }'
+          + '.modal-buttons { margin-left: -13px !important }'
+          + '.sticky div:active, .sticky div:focus {' // post-GPT-4o UI sidebar
+              + 'transform: none !important }' // disable distracting click zoom effect
+        )
+    }
+
     // Create nav toggle div, add styles
     const navToggleDiv = document.createElement('div')
     navToggleDiv.style.maxHeight = '44px' // prevent flex overgrowth
