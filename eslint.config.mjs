10569ce35860ca44dba3e583be769ba04e0adb42
adamlui@protonmail.com <adamlui@protonmail.com>
2024-06-01 22:31:54 -0700

    Allowed whitespace anywhere in between obj key/vals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,9 +6,8 @@ export default [
     js.configs.recommended,
     {
         rules: {
-            'indent': 'off', 'no-unexpected-multiline': 'off', // allow whitespace anywhere
+            'indent': 'off', 'no-unexpected-multiline': 'off', 'key-spacing': 'off', // allow whitespace anywhere
             'quotes': ['error', 'single', { 'allowTemplateLiterals': true }], // enforce single quotes except backticks to avoid escaping quotes
-            'key-spacing': ['error', { 'beforeColon': false, 'afterColon': true }], // enforce spacing in object properties
             'comma-dangle': ['error', 'never'], // enforce no trailing commas in arrays or objects
             'no-async-promise-executor': 'off', // allow promise executor functions to be async (to accomodate await lines)
             'no-constant-condition': 'off', // allow constant conditions
