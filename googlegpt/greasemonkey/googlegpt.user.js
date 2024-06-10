b8b7e5ff892a9bfce366f03760465c56f7e269ae
adamlui@protonmail.com <adamlui@protonmail.com>
2024-06-09 18:20:23 -0700

    Added Standby Mode check before showing answer in `appShow()`

--- googlegpt/greasemonkey/googlegpt.user.js
+++ googlegpt/greasemonkey/googlegpt.user.js
@@ -156,7 +156,7 @@
 // @description:zu      Yengeza izimpendulo ze-AI ku-Google Search (inikwa amandla yi-Google Gemma + GPT-4o!)
 // @author              KudoAI
 // @namespace           https://kudoai.com
-// @version             2024.6.9.3
+// @version             2024.6.9.4
 // @license             MIT
 // @icon                https://media.googlegpt.io/images/icons/googlegpt/black/icon48.png?8652a6e
 // @icon64              https://media.googlegpt.io/images/icons/googlegpt/black/icon64.png?8652a6e
@@ -1863,7 +1863,8 @@
             sendButton.addEventListener('mouseout', toggleTooltip)
         }
 
-        appDiv.querySelector('pre').textContent = answer // show answer
+        if (answer != 'standby') // show answer
+            appDiv.querySelector('pre').textContent = answer
         updateTweaksStyle() // in case sticky mode on
 
         // Render markdown/math + highlight code
