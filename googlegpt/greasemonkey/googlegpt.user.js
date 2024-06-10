ddba2f649fe42dfa8a1b460b6f83fdb667e03e53
adamlui@protonmail.com <adamlui@protonmail.com>
2024-06-09 18:36:15 -0700

    Corrected `replyForm` init in `handleSubmit()`

--- googlegpt/greasemonkey/googlegpt.user.js
+++ googlegpt/greasemonkey/googlegpt.user.js
@@ -156,7 +156,7 @@
 // @description:zu      Yengeza izimpendulo ze-AI ku-Google Search (inikwa amandla yi-Google Gemma + GPT-4o!)
 // @author              KudoAI
 // @namespace           https://kudoai.com
-// @version             2024.6.9.4
+// @version             2024.6.9.5
 // @license             MIT
 // @icon                https://media.googlegpt.io/images/icons/googlegpt/black/icon48.png?8652a6e
 // @icon64              https://media.googlegpt.io/images/icons/googlegpt/black/icon64.png?8652a6e
@@ -1924,7 +1924,7 @@
             getShowReply(msgChain)
 
             // Remove re-added reply section listeners
-            const replyForm = appDiv
+            const replyForm = appDiv.querySelector('form')
             replyForm.removeEventListener('keydown', handleEnter)
             replyForm.removeEventListener('submit', handleSubmit)
             appDiv.querySelector('#app-chatbar').removeEventListener('input', autosizeChatbar)
