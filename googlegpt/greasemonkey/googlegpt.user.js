bc67e2dab8998913f277433178f6d534d09f55dc
adamlui@protonmail.com <adamlui@protonmail.com>
2024-06-13 11:12:49 -0700

    Updated `appLogoImg.src` w/ smaller one

--- googlegpt/greasemonkey/googlegpt.user.js
+++ googlegpt/greasemonkey/googlegpt.user.js
@@ -156,7 +156,7 @@
 // @description:zu      Yengeza izimpendulo ze-AI ku-Google Search (inikwa amandla yi-Google Gemma + GPT-4o!)
 // @author              KudoAI
 // @namespace           https://kudoai.com
-// @version             2024.6.13.9
+// @version             2024.6.13.10
 // @license             MIT
 // @icon                https://media.googlegpt.io/images/icons/googlegpt/black/icon48.png?8652a6e
 // @icon64              https://media.googlegpt.io/images/icons/googlegpt/black/icon64.png?8652a6e
@@ -806,7 +806,7 @@
     }
 
     function updateAppLogoSrc() {
-        appLogoImg.src = `https://media.googlegpt.io/images/logos/googlegpt/${ scheme == 'dark' ? 'white' : 'black' }.png`
+        appLogoImg.src = `https://media.googlegpt.io/images/logos/googlegpt/flat/${ scheme == 'dark' ? 'white' : 'black' }/logo480x64.png?80636a7`
         appLogoImg.onerror = () => appLogoImg.style.display = 'none'
     }
 
