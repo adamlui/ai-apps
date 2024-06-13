af8d73a8c82d93e8568bfcf087ced268a2d19c90
adamlui@protonmail.com <adamlui@protonmail.com>
2024-06-12 17:57:59 -0700

    Removed straggling `function` keyword

--- duckduckgpt/greasemonkey/duckduckgpt.user.js
+++ duckduckgpt/greasemonkey/duckduckgpt.user.js
@@ -152,7 +152,7 @@
 // @description:zu      Faka amaphawu ase-ChatGPT kuvaliwe i-DuckDuckGo Search (okwesikhashana ngu-GPT-4o!)
 // @author              KudoAI
 // @namespace           https://kudoai.com
-// @version             2024.6.12.1
+// @version             2024.6.12.2
 // @license             MIT
 // @icon                https://media.ddgpt.com/images/icons/duckduckgpt/icon48.png?af89302
 // @icon64              https://media.ddgpt.com/images/icons/duckduckgpt/icon64.png?af89302
@@ -1406,9 +1406,7 @@
                     onerror: err => { consoleErr(err) ; reject(err) }
             })})
         }
-    }
-
-    function 
+    } 
 
     // Define PROCESS functions
 
