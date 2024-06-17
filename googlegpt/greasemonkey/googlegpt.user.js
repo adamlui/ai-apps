84919d87aef62ffac5be6ec22879275f419db434
adamlui@protonmail.com <adamlui@protonmail.com>
2024-06-17 00:32:34 -0700

    Doubled recursion delay for Edge users in `dataProcess.stream()`

--- googlegpt/greasemonkey/googlegpt.user.js
+++ googlegpt/greasemonkey/googlegpt.user.js
@@ -156,7 +156,7 @@
 // @description:zu      Yengeza izimpendulo ze-AI ku-Google Search (inikwa amandla yi-Google Gemma + GPT-4o!)
 // @author              KudoAI
 // @namespace           https://kudoai.com
-// @version             2024.6.16.17
+// @version             2024.6.17
 // @license             MIT
 // @icon                https://media.googlegpt.io/images/icons/googlegpt/black/icon48.png?8652a6e
 // @icon64              https://media.googlegpt.io/images/icons/googlegpt/black/icon64.png?8652a6e
@@ -1661,7 +1661,7 @@
                 } catch (err) { consoleErr('Error showing stream', err.message) }
                 return reader.read().then(({ done, value }) => {
                     if (get.reply.sender == caller.api) // am designated sender, recurse
-                        setTimeout(() => { processStreamText({ done, value }) }, isEdge ? 100 : 1) // Edge delay vs. STATUS_ACCESS_VIOLATION bug
+                        setTimeout(() => { processStreamText({ done, value }) }, isEdge ? 200 : 1) // Edge delay vs. STATUS_ACCESS_VIOLATION bug
                 }).catch(err => consoleErr('Error reading stream', err.message))
             }
         }
