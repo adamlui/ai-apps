9ed87322e06e108b39e1f91c06a9f67ca3166c69
adamlui <adamlui@users.noreply.github.com>
2024-06-25 23:51:53 +0000

    Replaced invalid optional chaining w/ traditional `if` check ↞ [auto-sync from `adamlui/chatgpt-infinity/opera`]

--- chatgpt-infinity/greasemonkey/chatgpt-infinity.user.js
+++ chatgpt-infinity/greasemonkey/chatgpt-infinity.user.js
@@ -199,7 +199,7 @@
 // @description:zh-TW   從無所不知的 ChatGPT 生成無窮無盡的答案 (用任何語言!)
 // @author              Adam Lui
 // @namespace           https://github.com/adamlui
-// @version             2024.6.25.3
+// @version             2024.6.25.4
 // @license             MIT
 // @match               *://chatgpt.com/*
 // @match               *://chat.openai.com/*
@@ -552,7 +552,8 @@
         if (isGPT4oUI) navToggleDiv.style.flexGrow = 'unset' // overcome OpenAI .grow
         if (!firstLink) parentToInsertInto.children[0].style.marginBottom = '5px'
         navToggleDiv.style.paddingLeft = '8px'
-        document.getElementById('infToggleFavicon')?.src = `${ // update navicon color in case scheme changed
+        const navicon = document.getElementById('infToggleFavicon')
+        if (navicon) navicon.src = `${ // update navicon color in case scheme changed
             config.assetHostURL }media/images/icons/infinity-symbol/${
             chatgpt.isDarkMode() ? 'white' : 'black' }/icon32.png`
     }
