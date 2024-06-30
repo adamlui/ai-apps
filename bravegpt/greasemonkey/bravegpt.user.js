d1251e8e6d5252573e01dd8664f4afa4d92adddb
adamlui@protonmail.com <adamlui@protonmail.com>
2024-06-29 20:46:31 -0700

    Corrected selector in `modals.settings.get()`

--- bravegpt/greasemonkey/bravegpt.user.js
+++ bravegpt/greasemonkey/bravegpt.user.js
@@ -148,7 +148,7 @@
 // @description:zu      Yengeza izimpendulo ze-AI ku-Brave Search (inikwa amandla yi-GPT-4o!)
 // @author              KudoAI
 // @namespace           https://kudoai.com
-// @version             2024.6.29.13
+// @version             2024.6.29.14
 // @license             MIT
 // @icon                https://media.bravegpt.com/images/icons/bravegpt/icon48.png?0a9e287
 // @icon64              https://media.bravegpt.com/images/icons/bravegpt/icon64.png?0a9e287
@@ -796,7 +796,7 @@ setTimeout(async () => {
                 return settingsContainer
             },
 
-            get() { return document.getElementById('ddgpt-settings') },
+            get() { return document.getElementById('bravegpt-settings') },
 
             hide() {
                 const settingsContainer = modals.settings.get()?.parentNode
--- googlegpt/greasemonkey/googlegpt.user.js
+++ googlegpt/greasemonkey/googlegpt.user.js
@@ -149,7 +149,7 @@
 // @description:zu      Yengeza izimpendulo ze-AI ku-Google Search (inikwa amandla yi-Google Gemma + GPT-4o!)
 // @author              KudoAI
 // @namespace           https://kudoai.com
-// @version             2024.6.29.10
+// @version             2024.6.29.11
 // @license             MIT
 // @icon                https://media.googlegpt.io/images/icons/googlegpt/black/icon48.png?8652a6e
 // @icon64              https://media.googlegpt.io/images/icons/googlegpt/black/icon64.png?8652a6e
@@ -992,7 +992,7 @@
                 return settingsContainer
             },
 
-            get() { return document.getElementById('ddgpt-settings') },
+            get() { return document.getElementById('bravegpt-settings') },
 
             hide() {
                 const settingsContainer = modals.settings.get()?.parentNode
