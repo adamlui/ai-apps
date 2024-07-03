cb91152c3a97389a4cec7ede0793e07d840c4918
adamlui@protonmail.com <adamlui@protonmail.com>
2024-07-03 14:40:13 -0700

    Made `icons.googleGPT.update()` arg optional + accept arrays/multi-args

--- googlegpt/greasemonkey/googlegpt.user.js
+++ googlegpt/greasemonkey/googlegpt.user.js
@@ -149,7 +149,7 @@
 // @description:zu        Yengeza izimpendulo ze-AI ku-Google Search (inikwa amandla yi-Google Gemma + GPT-4o!)
 // @author                KudoAI
 // @namespace             https://kudoai.com
-// @version               2024.7.3.1
+// @version               2024.7.3.2
 // @license               MIT
 // @icon                  https://media.googlegpt.io/images/icons/googlegpt/black/icon48.png?8652a6e
 // @icon64                https://media.googlegpt.io/images/icons/googlegpt/black/icon64.png?8652a6e
@@ -1158,12 +1158,16 @@
                 return googleGPTicon
             },
 
-            update(googleGPTicon) {
-                googleGPTicon.src = `${config.mediaHostURL}images/icons/googlegpt/${ scheme == 'dark' ? 'white' : 'black' }`
-                                  + `/icon64.png?${config.latestAssetCommitHash}`
-                googleGPTicon.style.filter = googleGPTicon.style.webkitFilter = ( // add RGB shift effect
-                    'drop-shadow(2px 1px 0px #ff5b5b) drop-shadow(-1px -1px 0px #49d749)'
-                  + ( scheme == 'light' ? 'drop-shadow(white 1px 1px)' : '' ))
+            update(...targetIcons) {
+                targetIcons = targetIcons.flat() // flatten array args nested by spread operator
+                if (targetIcons.length == 0) targetIcons = document.querySelectorAll('#googlegpt-icon')
+                targetIcons.forEach(icon => {
+                    icon.src = `${config.mediaHostURL}images/icons/googlegpt/${ scheme == 'dark' ? 'white' : 'black' }`
+                             + `/icon64.png?${config.latestAssetCommitHash}`
+                    icon.style.filter = icon.style.webkitFilter = ( // add RGB shift effect
+                        'drop-shadow(2px 1px 0px #ff5b5b) drop-shadow(-1px -1px 0px #49d749)'
+                      + ( scheme == 'light' ? 'drop-shadow(white 1px 1px)' : '' ))
+                })
             }
         },
 
