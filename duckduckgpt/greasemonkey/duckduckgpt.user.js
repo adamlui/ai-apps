beeb51a365c4812749bbe6d1c8e49bd49c1a6c9a
adamlui@protonmail.com <adamlui@protonmail.com>
2024-07-08 03:16:24 -0700

    Abstracted Standby button click handler for re-use

--- duckduckgpt/greasemonkey/duckduckgpt.user.js
+++ duckduckgpt/greasemonkey/duckduckgpt.user.js
@@ -148,7 +148,7 @@
 // @description:zu         Yengeza izimpendulo ze-AI ku-DuckDuckGo (inikwa amandla yi-GPT-4o!)
 // @author                 KudoAI
 // @namespace              https://kudoai.com
-// @version                2024.7.8
+// @version                2024.7.8.1
 // @license                MIT
 // @icon                   https://media.ddgpt.com/images/icons/duckduckgpt/icon48.png?af89302
 // @icon64                 https://media.ddgpt.com/images/icons/duckduckgpt/icon64.png?af89302
@@ -2517,7 +2517,7 @@
                     standbyBtn.className = 'standby-btn'
                     standbyBtn.textContent = msgs.buttonLabel_sendQueryToGPT || 'Send search query to GPT'
                     appDiv.append(standbyBtn)
-                    standbyBtn.onclick = () => {
+                    this.standbyBtnClickHandler = function() {
                         appAlert('waitingResponse')
                         appFooter.style.right = 0 // reset counteract right-offset bug from chatbar padding
                         msgChain.push({ role: 'user', content: augmentQuery(new URL(location.href).searchParams.get('q')) })
@@ -2525,6 +2525,7 @@
                         menus.pin.topPos = menus.pin.rightPos = null
                         get.reply(msgChain)
                     }
+                    standbyBtn.onclick = this.standbyBtnClickHandler
 
                 // Otherwise create/append answer bubble
                 } else {
--- googlegpt/greasemonkey/googlegpt.user.js
+++ googlegpt/greasemonkey/googlegpt.user.js
@@ -149,7 +149,7 @@
 // @description:zu           Yengeza izimpendulo ze-AI ku-Google Search (inikwa amandla yi-Google Gemma + GPT-4o!)
 // @author                   KudoAI
 // @namespace                https://kudoai.com
-// @version                  2024.7.8
+// @version                  2024.7.8.1
 // @license                  MIT
 // @icon                     https://media.googlegpt.io/images/icons/googlegpt/black/icon48.png?8652a6e
 // @icon64                   https://media.googlegpt.io/images/icons/googlegpt/black/icon64.png?8652a6e
@@ -2837,13 +2837,14 @@
                     standbyBtn.className = 'standby-btn'
                     standbyBtn.textContent = msgs.buttonLabel_sendQueryToGPT || 'Send search query to GPT'
                     appDiv.append(standbyBtn)
-                    standbyBtn.onclick = () => {
+                    this.standbyBtnClickHandler = function() {
                         appAlert('waitingResponse')
                         msgChain.push({ role: 'user', content: augmentQuery(new URL(location.href).searchParams.get('q')) })
                         show.reply.submitSrc = 'click' ; show.reply.chatbarFocused = false
                         menus.pin.topPos = menus.pin.rightPos = null
                         get.reply(msgChain)
                     }
+                    standbyBtn.onclick = this.standbyBtnClickHandler
 
                 // Otherwise create/append answer bubble
                 } else {
