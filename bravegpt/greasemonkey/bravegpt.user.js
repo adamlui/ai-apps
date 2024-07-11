ba9f87eb73de9e17cea5c1d900cc73df510e997c
adamlui@protonmail.com <adamlui@protonmail.com>
2024-07-11 15:42:21 -0700

    Added `// @antifeature ads` description

--- bravegpt/greasemonkey/bravegpt.user.js
+++ bravegpt/greasemonkey/bravegpt.user.js
@@ -148,11 +148,11 @@
 // @description:zu        Yengeza izimpendulo ze-AI ku-Brave Search (inikwa amandla yi-GPT-4o!)
 // @author                KudoAI
 // @namespace             https://kudoai.com
-// @version               2024.7.11.1
+// @version               2024.7.11.2
 // @license               MIT
 // @icon                  https://media.bravegpt.com/images/icons/bravegpt/icon48.png?0a9e287
 // @icon64                https://media.bravegpt.com/images/icons/bravegpt/icon64.png?0a9e287
-// @antifeature           ads
+// @antifeature           ads A small, inobtrusive text ad displays below BraveGPT chatbar ~50% of the time to motivate author to spend time upgrading script w/ new enhancements.
 // @compatible            chrome except for Streaming Mode w/ Tampermonkey (use ScriptCat instead)
 // @compatible            firefox
 // @compatible            edge except for Streaming Mode w/ Tampermonkey (use ScriptCat instead)
