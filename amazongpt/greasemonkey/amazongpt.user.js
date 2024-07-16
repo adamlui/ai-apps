fd4ae0ecf47cd74bab616d215629653b23b2ad34
adamlui <adamlui@users.noreply.github.com>
2024-07-16 03:40:18 +0000

    Lowered app div in Minimized state ↞ [auto-sync from `KudoAI/amazongpt`]

--- amazongpt/greasemonkey/amazongpt.user.js
+++ amazongpt/greasemonkey/amazongpt.user.js
@@ -3,7 +3,7 @@
 // @description            Adds the magic of AI to Amazon shopping
 // @author                 KudoAI
 // @namespace              https://kudoai.com
-// @version                2024.7.15.4
+// @version                2024.7.15.5
 // @license                MIT
 // @icon                   https://amazongpt.kudoai.com/assets/images/icons/amazongpt/black-gold-teal/icon48.png?v=0fddfc7
 // @icon64                 https://amazongpt.kudoai.com/assets/images/icons/amazongpt/black-gold-teal/icon64.png?v=0fddfc7
@@ -1135,7 +1135,7 @@
 
     const update = {
 
-        appBottomPos() { appDiv.style.bottom = `${ config.minimized ? 61 - appDiv.offsetHeight : -7 }px` },
+        appBottomPos() { appDiv.style.bottom = `${ config.minimized ? 55 - appDiv.offsetHeight : -7 }px` },
 
         appStyle() {
             appStyle.innerText = (
