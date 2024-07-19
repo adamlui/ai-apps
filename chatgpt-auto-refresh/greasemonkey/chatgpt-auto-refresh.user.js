a5beea1fd7ad6c6c4ea6d85c21a6e484438755e2
adamlui@protonmail.com <adamlui@protonmail.com>
2024-07-19 16:50:36 -0700

    Replaced Review modal w/ direct launch of GF since Futurepedia listing defunct

--- chatgpt-auto-refresh/greasemonkey/chatgpt-auto-refresh.user.js
+++ chatgpt-auto-refresh/greasemonkey/chatgpt-auto-refresh.user.js
@@ -220,7 +220,7 @@
 // @description:zu      *NGOKUPHEPHA* susa ukusetha kabusha ingxoxo yemizuzu eyi-10 + amaphutha enethiwekhi ahlala njalo + Ukuhlolwa kwe-Cloudflare ku-ChatGPT.
 // @author              Adam Lui
 // @namespace           https://github.com/adamlui
-// @version             2024.7.19.1
+// @version             2024.7.19.2
 // @license             MIT
 // @match               *://chatgpt.com/*
 // @match               *://chat.openai.com/*
@@ -495,13 +495,7 @@
             [ // buttons
                 function checkForUpdates() { updateCheck() },
                 function getSupport() { safeWindowOpen(config.supportURL) },
-                function leaveAReview() { // show new modal
-                    const reviewModalID = chatgpt.alert(( msgs.alert_choosePlatform || 'Choose a platform' ) + ':', '',
-                        [ function greasyFork() { safeWindowOpen(config.greasyForkURL + '/feedback#post-discussion') },
-                          function futurepedia() { safeWindowOpen(
-                              'https://www.futurepedia.io/tool/chatgpt-auto-refresh#chatgpt-auto-refresh-review') }])
-                    document.getElementById(reviewModalID).querySelector('button')
-                        .style.display = 'none' }, // hide Dismiss button
+                function leaveAReview() { safeWindowOpen(config.greasyForkURL + '/feedback#post-discussion') },
                 function moreChatGPTapps() { safeWindowOpen('https://github.com/adamlui/chatgpt-apps') }
             ], '', 478 // set width
         )
