b30cbbda1f2150485e092f5bd613260afb125cea
adamlui@protonmail.com <adamlui@protonmail.com>
2024-07-19 23:12:00 -0700

    Added `event` to `menus.pin.clickHandler()` sig to avoid use of deprecated global `event`

--- duckduckgpt/greasemonkey/duckduckgpt.user.js
+++ duckduckgpt/greasemonkey/duckduckgpt.user.js
@@ -148,7 +148,7 @@
 // @description:zu         Yengeza izimpendulo ze-AI ku-DuckDuckGo (inikwa amandla yi-GPT-4o!)
 // @author                 KudoAI
 // @namespace              https://kudoai.com
-// @version                2024.7.19.5
+// @version                2024.7.19.6
 // @license                MIT
 // @icon                   https://media.ddgpt.com/images/icons/duckduckgpt/icon48.png?af89302
 // @icon64                 https://media.ddgpt.com/images/icons/duckduckgpt/icon64.png?af89302
@@ -1003,7 +1003,7 @@
         },
 
         pin: {
-            clickHandler() {
+            clickHandler(event) {
                 const pinMenu = event.target.closest('#pin-menu'),
                       itemLabel = event.target.textContent,
                       prevOffsetTop = appDiv.offsetTop
--- googlegpt/greasemonkey/googlegpt.user.js
+++ googlegpt/greasemonkey/googlegpt.user.js
@@ -149,7 +149,7 @@
 // @description:zu           Yengeza izimpendulo ze-AI ku-Google Search (inikwa amandla yi-Google Gemma + GPT-4o!)
 // @author                   KudoAI
 // @namespace                https://kudoai.com
-// @version                  2024.7.19.4
+// @version                  2024.7.19.5
 // @license                  MIT
 // @icon                     https://media.googlegpt.io/images/icons/googlegpt/black/icon48.png?8652a6e
 // @icon64                   https://media.googlegpt.io/images/icons/googlegpt/black/icon64.png?8652a6e
@@ -1183,7 +1183,7 @@
         },
 
         pin: {
-            clickHandler() {
+            clickHandler(event) {
                 const pinMenu = event.target.closest('#pin-menu'),
                       itemLabel = event.target.textContent,
                       prevOffsetTop = appDiv.offsetTop
