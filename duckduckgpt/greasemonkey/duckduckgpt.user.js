530f5d90bcc693a7618f855741e61c19b9d267b8
adamlui@protonmail.com <adamlui@protonmail.com>
2024-07-20 21:05:37 -0700

    Expanded Auto-Scroll to work in Anchored mode

--- duckduckgpt/greasemonkey/duckduckgpt.user.js
+++ duckduckgpt/greasemonkey/duckduckgpt.user.js
@@ -148,7 +148,7 @@
 // @description:zu         Yengeza izimpendulo ze-AI ku-DuckDuckGo (inikwa amandla yi-GPT-4o!)
 // @author                 KudoAI
 // @namespace              https://kudoai.com
-// @version                2024.7.20.1
+// @version                2024.7.20.2
 // @license                MIT
 // @icon                   https://media.ddgpt.com/images/icons/duckduckgpt/icon48.png?af89302
 // @icon64                 https://media.ddgpt.com/images/icons/duckduckgpt/icon64.png?af89302
@@ -2811,7 +2811,7 @@
 
                 // Auto-scroll if active
                 if (config.autoScroll && !isMobile && config.proxyAPIenabled && !config.streamingDisabled) {
-                    if (config.stickySidebar) answerPre.scrollTop = answerPre.scrollHeight
+                    if (config.stickySidebar || config.anchored) answerPre.scrollTop = answerPre.scrollHeight
                     else window.scrollBy({ top: appDiv.querySelector('#app-chatbar').getBoundingClientRect().bottom - window.innerHeight +12 })
                 }
             }
--- googlegpt/greasemonkey/googlegpt.user.js
+++ googlegpt/greasemonkey/googlegpt.user.js
@@ -149,7 +149,7 @@
 // @description:zu           Yengeza izimpendulo ze-AI ku-Google Search (inikwa amandla yi-Google Gemma + GPT-4o!)
 // @author                   KudoAI
 // @namespace                https://kudoai.com
-// @version                  2024.7.20
+// @version                  2024.7.20.1
 // @license                  MIT
 // @icon                     https://media.googlegpt.io/images/icons/googlegpt/black/icon48.png?8652a6e
 // @icon64                   https://media.googlegpt.io/images/icons/googlegpt/black/icon64.png?8652a6e
@@ -3135,7 +3135,7 @@
 
                 // Auto-scroll if active
                 if (config.autoScroll && !isMobile && config.proxyAPIenabled && !config.streamingDisabled) {
-                    if (config.stickySidebar) answerPre.scrollTop = answerPre.scrollHeight
+                    if (config.stickySidebar || config.anchored) answerPre.scrollTop = answerPre.scrollHeight
                     else window.scrollBy({ top: appDiv.querySelector('#app-chatbar').getBoundingClientRect().bottom - window.innerHeight +13 })
                 }
             }
