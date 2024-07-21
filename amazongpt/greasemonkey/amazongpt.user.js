1a401c85db2d34abe88fbddec5a32f61cc8c7c99
adamlui@protonmail.com <adamlui@protonmail.com>
2024-07-20 19:59:39 -0700

    Added `// @compatible vivaldi <description>`

--- amazongpt/greasemonkey/amazongpt.user.js
+++ amazongpt/greasemonkey/amazongpt.user.js
@@ -3,7 +3,7 @@
 // @description            Adds the magic of AI to Amazon shopping
 // @author                 KudoAI
 // @namespace              https://kudoai.com
-// @version                2024.7.20.4
+// @version                2024.7.20.5
 // @license                MIT
 // @icon                   https://amazongpt.kudoai.com/assets/images/icons/amazongpt/black-gold-teal/icon48.png?v=0fddfc7
 // @icon64                 https://amazongpt.kudoai.com/assets/images/icons/amazongpt/black-gold-teal/icon64.png?v=0fddfc7
@@ -12,6 +12,7 @@
 // @compatible             edge except for Streaming Mode w/ Tampermonkey (use ScriptCat instead)
 // @compatible             opera
 // @compatible             brave except for Streaming Mode w/ Tampermonkey (use ScriptCat instead)
+// @compatible             vivaldi except for Streaming Mode w/ Tampermonkey (use ScriptCat instead)
 // @match                  *://*.amazon.com/*
 // @match                  *://*.amazon.ae/*
 // @match                  *://*.amazon.be/*
