31e026aa54ee0fadb56997aac73db5b7d5be9726
adamlui@protonmail.com <adamlui@protonmail.com>
2024-07-24 23:24:25 -0700

    Added skinny FF scrollbar + hover-zoom to modal buttons

--- chatgpt-infinity/opera/extension/content.js
+++ chatgpt-infinity/opera/extension/content.js
@@ -223,7 +223,7 @@
         }}
 
     // Add/update TWEAKS style
-    const tweaksStyleUpdated = 20240702 // datestamp of last edit for this file's `tweaksStyle`
+    const tweaksStyleUpdated = 20240724 // datestamp of last edit for this file's `tweaksStyle`
     let tweaksStyle = document.getElementById('tweaks-style') // try to select existing style
     if (!tweaksStyle || parseInt(tweaksStyle.getAttribute('last-updated'), 10) < tweaksStyleUpdated) { // if missing or outdated
         if (!tweaksStyle) { // outright missing, create/id/attr/append it first
@@ -234,11 +234,13 @@
         tweaksStyle.innerText = (
             ( chatgpt.isDarkMode() ? '.chatgpt-modal > div { border: 1px solid white }' : '' )
           + '.chatgpt-modal button {'
-                + 'font-size: 0.77rem ; text-transform: uppercase ;'
-                + 'border-radius: 0 !important ; padding: 5px !important ; min-width: 102px }'
-            + '.modal-buttons { margin-left: -13px !important }'
-            + '.sticky div:active, .sticky div:focus {' // post-GPT-4o UI sidebar button container
-                + 'transform: none !important }' // disable distracting click zoom effect
+              + 'font-size: 0.77rem ; text-transform: uppercase ;'
+              + 'border-radius: 0 !important ; padding: 5px !important ; min-width: 102px }'
+          + '.chatgpt-modal button:hover { transform: scale(1.055) }'
+          + '.modal-buttons { margin-left: -13px !important }'
+          + '* { scrollbar-width: thin }' // make FF scrollbar skinny to not crop toggle
+          + '.sticky div:active, .sticky div:focus {' // post-GPT-4o UI sidebar button container
+              + 'transform: none !important }' // disable distracting click zoom effect
         )
     }
 
