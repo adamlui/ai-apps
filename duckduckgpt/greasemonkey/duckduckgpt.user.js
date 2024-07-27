265e552b7940b895c20f1e6de81a0a0d53f32b2f
Adam Lui 刘展鹏 <adam@kudoai.com>
2024-07-27 15:31:07 -0700

    Corrected `// @version`

--- duckduckgpt/greasemonkey/duckduckgpt.user.js
+++ duckduckgpt/greasemonkey/duckduckgpt.user.js
@@ -148,7 +148,7 @@
 // @description:zu         Yengeza izimpendulo ze-AI ku-DuckDuckGo (inikwa amandla yi-GPT-4o!)
 // @author                 KudoAI
 // @namespace              https://kudoai.com
-// @version                createAppend()
+// @version                2024.7.27
 // @license                MIT
 // @icon                   https://media.ddgpt.com/images/icons/duckduckgpt/icon48.png?af89302
 // @icon64                 https://media.ddgpt.com/images/icons/duckduckgpt/icon64.png?af89302
