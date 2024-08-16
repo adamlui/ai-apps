79840a704f766f5cc348d391ab8d6fcca2c5e627
adamlui@protonmail.com <adamlui@protonmail.com>
2024-08-15 21:41:51 -0700

    Removed unnecessary braces

--- googlegpt/greasemonkey/googlegpt.user.js
+++ googlegpt/greasemonkey/googlegpt.user.js
@@ -149,7 +149,7 @@
 // @description:zu           Yengeza izimpendulo ze-AI ku-Google Search (inikwa amandla yi-Google Gemma + GPT-4o!)
 // @author                   KudoAI
 // @namespace                https://kudoai.com
-// @version                  2024.8.15.1
+// @version                  2024.8.15.2
 // @license                  MIT
 // @icon                     https://media.googlegpt.io/images/icons/googlegpt/black/icon48.png?8652a6e
 // @icon64                   https://media.googlegpt.io/images/icons/googlegpt/black/icon64.png?8652a6e
@@ -2732,14 +2732,14 @@
             setTimeout(() => { if (get.related.status != 'done') api.tryNew(get.related, 'timeout') }, 7000)
 
             // Get queries
-            return new Promise(resolve => { xhr({
+            return new Promise(resolve => xhr({
                 method: apis[get.related.api].method,
                 url: apis[get.related.api].endpoints?.completions || apis[get.related.api].endpoint,
                 responseType: 'text', headers: api.createHeaders(get.related.api),
                 data: api.createPayload(get.related.api, [{ role: 'user', content: rqPrompt }]),
                 onload: resp => dataProcess.text(get.related, resp).then(resolve),
                 onerror: err => { consoleErr(err.message) ; if (get.related.status != 'done') api.tryNew(get.related) }
-            })})
+            }))
         }
     }
 
