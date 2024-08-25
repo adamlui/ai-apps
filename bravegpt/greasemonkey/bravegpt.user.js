74231d224bb79a3613eb159af1aaac318aa00b4a
adamlui@protonmail.com <adamlui@protonmail.com>
2024-08-25 08:22:15 -0700

    Added quote-stripping to related queries

--- bravegpt/greasemonkey/bravegpt.user.js
+++ bravegpt/greasemonkey/bravegpt.user.js
@@ -148,7 +148,7 @@
 // @description:zu        Yengeza izimpendulo ze-AI ku-Brave Search (inikwa amandla yi-GPT-4o!)
 // @author                KudoAI
 // @namespace             https://kudoai.com
-// @version               2024.8.25
+// @version               2024.8.25.1
 // @license               MIT
 // @icon                  https://media.bravegpt.com/images/icons/bravegpt/icon48.png?0a9e287
 // @icon64                https://media.bravegpt.com/images/icons/bravegpt/icon64.png?0a9e287
@@ -2751,7 +2751,7 @@ setTimeout(async () => {
                 function arrayify(strList) { // for get.related() calls
                     return (strList.match(/\d+\.\s*(.*?)(?=\n|\\n|$)/g) || [])
                         .slice(0, 5) // limit to 1st 5
-                        .map(match => match.replace(/^\d+\.\s*/, '')) // strip numbering
+                        .map(match => match.replace(/^(\d+\.)?\s*['"]?|['"]$/, '')) // strip numbering + quotes
                 }
         })}
     }
