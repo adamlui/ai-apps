53499d97325a320e77c441d69b48f7acb289ef03
adamlui@protonmail.com <adamlui@protonmail.com>
2024-09-10 22:41:11 -0700

    Corrected logo/icon method names

--- amazongpt/greasemonkey/amazongpt.user.js
+++ amazongpt/greasemonkey/amazongpt.user.js
@@ -3,7 +3,7 @@
 // @description            Adds the magic of AI to Amazon shopping
 // @author                 KudoAI
 // @namespace              https://kudoai.com
-// @version                2024.9.10.8
+// @version                2024.9.10.9
 // @license                MIT
 // @icon                   https://amazongpt.kudoai.com/assets/images/icons/amazongpt/black-gold-teal/icon48.png?v=0fddfc7
 // @icon64                 https://amazongpt.kudoai.com/assets/images/icons/amazongpt/black-gold-teal/icon64.png?v=0fddfc7
@@ -1629,7 +1629,7 @@
         scheme(newScheme) {
             log.caller = `update.scheme('${newScheme}')`
             log.debug(`Updating ${app.name} scheme to ${log.toTitleCase(newScheme)}...`)
-            scheme = newScheme ; logos.googleGPT.update() ; icons.googleGPT.update()
+            scheme = newScheme ; logos.amzgpt.update() ; icons.amzgpt.update()
             update.appStyle() ; update.stars() ; toggle.btnGlow() ; modals.settings.updateSchemeStatus()
             log.debug(`Success! ${app.name} updated to ${log.toTitleCase(newScheme)} scheme`)
         },
