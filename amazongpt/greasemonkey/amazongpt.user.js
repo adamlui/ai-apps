13786a1ee3216be5131f7f6a2c29f87d5f226505
adamlui@protonmail.com <adamlui@protonmail.com>
2024-09-16 01:47:25 -0700

    Changed non-product/category prompt from 'Wassup bot' to 'Hi there' to avoid out-of-context replies

--- amazongpt/greasemonkey/amazongpt.user.js
+++ amazongpt/greasemonkey/amazongpt.user.js
@@ -3,7 +3,7 @@
 // @description            Adds the magic of AI to Amazon shopping
 // @author                 KudoAI
 // @namespace              https://kudoai.com
-// @version                2024.9.16
+// @version                2024.9.16.1
 // @license                MIT
 // @icon                   https://amazongpt.kudoai.com/assets/images/icons/amazongpt/black-gold-teal/icon48.png?v=0fddfc7
 // @icon64                 https://amazongpt.kudoai.com/assets/images/icons/amazongpt/black-gold-teal/icon64.png?v=0fddfc7
@@ -2814,7 +2814,7 @@
         /\/dp\//.test(location.href) ? ('Tell me more about this product, including benefits and the brand if possible.'
                                      + ' Also talk about similar products in a markdown list. The product is: ' + document.title)
        : /\/b\//.test(location.href) ? ('Tell me more about what to look for when shopping for this category: ' + document.title)
-       : 'Wassup bot'
+       : 'Hi there'
     )
     let msgChain = [{ role: 'user', content: augmentQuery(firstQuery) }]
     appAlert('waitingResponse') ; get.reply(msgChain)
