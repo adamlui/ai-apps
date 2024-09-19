6fbec9782bd026e6c216d35594dbe5dc5c23e73f
adamlui@protonmail.com <adamlui@protonmail.com>
2024-09-19 23:21:39 +0000

    Moved modal functions into obj for improved structure ↞ [auto-sync from `adamlui/userscripts/chatgpt/chatgpt-auto-continue`]

--- chatgpt-auto-continue/greasemonkey/chatgpt-auto-continue.user.js
+++ chatgpt-auto-continue/greasemonkey/chatgpt-auto-continue.user.js
@@ -219,7 +219,7 @@
 // @description:zu      ⚡ Terus menghasilkan imibuzo eminingi ye-ChatGPT ngokwesizulu
 // @author              Adam Lui
 // @namespace           https://github.com/adamlui
-// @version             2024.9.18.1
+// @version             2024.9.19
 // @license             MIT
 // @match               *://chatgpt.com/*
 // @match               *://chat.openai.com/*
@@ -320,52 +320,12 @@
     
             // Add About entry
             const aboutLabel = `💡 ${ msgs.menuLabel_about || 'About' } ${ msgs.appName || app.name }`
-            menu.ids.push(GM_registerMenuCommand(aboutLabel, launchAboutModal))
+            menu.ids.push(GM_registerMenuCommand(aboutLabel, modals.about.show))
         },
 
         refresh() {
             if (env.scriptManager == 'OrangeMonkey') return
             for (const id of menu.ids) { GM_unregisterMenuCommand(id) } menu.register()
-        }
-    }
-
-    function launchAboutModal() {
-
-        // Show alert
-        const chatgptJSver = (/chatgpt-([\d.]+)\.min/.exec(GM_info.script.header) || [null, ''])[1],
-              headingStyle = 'font-size: 1.15rem',
-              pStyle = 'position: relative ; left: 3px',
-              pBrStyle = 'position: relative ; left: 4px ',
-              aStyle = 'color: ' + ( chatgpt.isDarkMode() ? '#c67afb' : '#8325c4' ) // purple
-        const aboutModalID = siteAlert(
-            msgs.appName || app.name, // title
-            `<span style="${headingStyle}"><b>🏷️ <i>${ msgs.about_version || 'Version' }</i></b>: </span>`
-                + `<span style="${pStyle}">${ GM_info.script.version }</span>\n`
-            + `<span style="${headingStyle}"><b>⚡ <i>${ msgs.about_poweredBy || 'Powered by' }</i></b>: </span>`
-                + `<span style="${pStyle}"><a style="${aStyle}" href="${app.urls.chatgptJS}" target="_blank" rel="noopener">`
-                + 'chatgpt.js</a>' + ( chatgptJSver ? ( ' v' + chatgptJSver ) : '' ) + '</span>\n'
-            + `<span style="${headingStyle}"><b>📜 <i>${ msgs.about_sourceCode || 'Source code' }</i></b>:</span>\n`
-                + `<span style="${pBrStyle}"><a href="${app.urls.gitHub}" target="_blank" rel="nopener">`
-                + app.urls.gitHub + '</a></span>',
-            [ // buttons
-                function checkForUpdates() { updateCheck() },
-                function getSupport() { safeWinOpen(app.urls.support) },
-                function leaveAReview() { safeWinOpen(app.urls.greasyFork + '/feedback#post-discussion') },
-                function moreChatGPTapps() { safeWinOpen(app.urls.relatedApps) }
-            ], '', 478 // set width
-        )
-
-        // Re-format buttons to include emoji + localized label + hide Dismiss button
-        for (const button of document.getElementById(aboutModalID).querySelectorAll('button')) {
-            if (/updates/i.test(button.textContent)) button.textContent = (
-                '🚀 ' + ( msgs.btnLabel_updateCheck || 'Check for Updates' ))
-            else if (/support/i.test(button.textContent)) button.textContent = (
-                '🧠 ' + ( msgs.btnLabel_getSupport || 'Get Support' ))
-            else if (/review/i.test(button.textContent)) button.textContent = (
-                '⭐ ' + ( msgs.btnLabel_leaveReview || 'Leave Review' ))
-            else if (/apps/i.test(button.textContent)) button.textContent = (
-                '🤖 ' + ( msgs.btnLabel_moreApps || 'More ChatGPT Apps' ))
-            else button.style.display = 'none' // hide Dismiss button
         }
     }
 
@@ -396,7 +356,7 @@
                                     + app.urls.update.replace(/.*\/(.*)meta\.js/, '$1user.js') + '"'
                                     + `> ${ msgs.link_viewChanges || 'View changes' }</a>`,
                             function update() { // button
-                                safeWinOpen(app.urls.update.replace('meta.js', 'user.js') + '?t=' + Date.now())
+                                modals.safeWinOpen(app.urls.update.replace('meta.js', 'user.js') + '?t=' + Date.now())
                             }, '', updateAlertWidth
                         )
 
@@ -417,10 +377,8 @@
                         + ( msgs.alert_isUpToDate || 'is up-to-date' ) + '!',
                     '', '', updateAlertWidth
                 )
-                launchAboutModal()
+                modals.about.show()
     }})}
-
-    function safeWinOpen(url) { window.open(url, '_blank', 'noopener') } // to prevent backdoor vulnerabilities
 
     // Define FEEDBACK functions
 
@@ -446,6 +404,54 @@
 
     function siteAlert(title = '', msg = '', btns = '', checkbox = '', width = '') {
         return chatgpt.alert(title, msg, btns, checkbox, width )}
+
+    // Define MODAL functions
+
+    const modals = {
+        about: {
+            show() {
+
+                // Show alert
+                const chatgptJSver = (/chatgpt-([\d.]+)\.min/.exec(GM_info.script.header) || [null, ''])[1],
+                      headingStyle = 'font-size: 1.15rem',
+                      pStyle = 'position: relative ; left: 3px',
+                      pBrStyle = 'position: relative ; left: 4px ',
+                      aStyle = 'color: ' + ( chatgpt.isDarkMode() ? '#c67afb' : '#8325c4' ) // purple
+                const aboutModalID = siteAlert(
+                    msgs.appName || app.name, // title
+                    `<span style="${headingStyle}"><b>🏷️ <i>${ msgs.about_version || 'Version' }</i></b>: </span>`
+                        + `<span style="${pStyle}">${ GM_info.script.version }</span>\n`
+                    + `<span style="${headingStyle}"><b>⚡ <i>${ msgs.about_poweredBy || 'Powered by' }</i></b>: </span>`
+                        + `<span style="${pStyle}"><a style="${aStyle}" href="${app.urls.chatgptJS}" target="_blank" rel="noopener">`
+                        + 'chatgpt.js</a>' + ( chatgptJSver ? ( ' v' + chatgptJSver ) : '' ) + '</span>\n'
+                    + `<span style="${headingStyle}"><b>📜 <i>${ msgs.about_sourceCode || 'Source code' }</i></b>:</span>\n`
+                        + `<span style="${pBrStyle}"><a href="${app.urls.gitHub}" target="_blank" rel="nopener">`
+                        + app.urls.gitHub + '</a></span>',
+                    [ // buttons
+                        function checkForUpdates() { updateCheck() },
+                        function getSupport() { modals.safeWinOpen(app.urls.support) },
+                        function leaveAReview() { modals.safeWinOpen(app.urls.greasyFork + '/feedback#post-discussion') },
+                        function moreChatGPTapps() { modals.safeWinOpen(app.urls.relatedApps) }
+                    ], '', 478 // set width
+                )
+        
+                // Re-format buttons to include emoji + localized label + hide Dismiss button
+                for (const button of document.getElementById(aboutModalID).querySelectorAll('button')) {
+                    if (/updates/i.test(button.textContent)) button.textContent = (
+                        '🚀 ' + ( msgs.btnLabel_updateCheck || 'Check for Updates' ))
+                    else if (/support/i.test(button.textContent)) button.textContent = (
+                        '🧠 ' + ( msgs.btnLabel_getSupport || 'Get Support' ))
+                    else if (/review/i.test(button.textContent)) button.textContent = (
+                        '⭐ ' + ( msgs.btnLabel_leaveReview || 'Leave Review' ))
+                    else if (/apps/i.test(button.textContent)) button.textContent = (
+                        '🤖 ' + ( msgs.btnLabel_moreApps || 'More ChatGPT Apps' ))
+                    else button.style.display = 'none' // hide Dismiss button
+                }
+            }
+        },
+
+        safeWinOpen(url) { window.open(url, '_blank', 'noopener') } // to prevent backdoor vulnerabilities
+    }
 
     // Run MAIN routine
 
