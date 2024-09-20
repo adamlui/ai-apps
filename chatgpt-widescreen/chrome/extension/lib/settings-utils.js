a990fa1d5dee147a6bc25efac49fa539ad8958b6
adamlui@protonmail.com <adamlui@protonmail.com>
2024-09-19 22:15:18 -0700

    Eliminated unneeded `app.configKeyPrefix` since `chrome.storage` is extension-specific

--- chatgpt-widescreen/chrome/extension/lib/settings-utils.js
+++ chatgpt-widescreen/chrome/extension/lib/settings-utils.js
@@ -1,16 +1,16 @@
-const app = {}, config = {}, settings = {
+const config = {}, settings = {
 
     load() {
         const keys = ( // original array if array, else new array from multiple args
             Array.isArray(arguments[0]) ? arguments[0] : Array.from(arguments))
         return Promise.all(keys.map(key => // resolve promise when all keys load
             new Promise(resolve => // resolve promise when single key value loads
-                chrome.storage.sync.get(`${ app.configKeyPrefix }_${ key }`, result => { // load from Chrome
-                    config[key] = result[`${ app.configKeyPrefix }_${ key }`] || false ; resolve()
+                chrome.storage.sync.get(key, result => { // load from Chrome
+                    config[key] = result[key] || false ; resolve()
     }))))},
 
     save(key, value) {
-        const obj = {} ; obj[`${ app.configKeyPrefix }_${ key }`] = value
+        const obj = {} ; obj[key] = value
         chrome.storage.sync.set(obj) // save to Chrome
         config[key] = value // save to memory
     }
