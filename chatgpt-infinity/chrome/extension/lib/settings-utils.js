45e8ffb766c78fe294291a7e78a6751bb238ee86
adamlui@protonmail.com <adamlui@protonmail.com>
2024-09-19 22:39:44 -0700

    Shortened `settings.load()`

--- chatgpt-infinity/chrome/extension/lib/settings-utils.js
+++ chatgpt-infinity/chrome/extension/lib/settings-utils.js
@@ -3,11 +3,11 @@ const config = {}, settings = {
     load() {
         const keys = ( // original array if array, else new array from multiple args
             Array.isArray(arguments[0]) ? arguments[0] : Array.from(arguments))
-        return Promise.all(keys.map(key => { // resolve promise when all keys load
-            new Promise(resolve => { // resolve promise when single key value loads
+        return Promise.all(keys.map(key => // resolve promise when all keys load
+            new Promise(resolve => // resolve promise when single key value loads
                 chrome.storage.sync.get(key, result => { // load from Chrome
                     config[key] = result[key] || false ; resolve()
-    })})}))},
+    }))))},
 
     save(key, value) {
         const obj = {} ; obj[key] = value
