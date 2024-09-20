98b13be06ff56037e72f3e885bd2c9dc69405e9d
adamlui@protonmail.com <adamlui@protonmail.com>
2024-09-20 01:12:26 +0000

    Renamed `amLabel` to `aboutLabel` for readability ↞ [auto-sync from `adamlui/userscripts/chatgpt/autoclear-chatgpt-history`]

--- autoclear-chatgpt-history/greasemonkey/autoclear-chatgpt-history.user.js
+++ autoclear-chatgpt-history/greasemonkey/autoclear-chatgpt-history.user.js
@@ -225,7 +225,7 @@
 // @description:zu      Ziba itshala lokucabanga okuzoshintshwa ngokuzenzakalelayo uma ukubuka chatgpt.com
 // @author              Adam Lui
 // @namespace           https://github.com/adamlui
-// @version             2024.9.19.1
+// @version             2024.9.19.2
 // @license             MIT
 // @icon                https://media.autoclearchatgpt.com/images/icons/openai/black/icon48.png?a8868ef
 // @icon64              https://media.autoclearchatgpt.com/images/icons/openai/black/icon64.png?a8868ef
@@ -358,9 +358,9 @@
             }))
 
             // Add About entry
-            const amLabel = '💡 ' + ( msgs.menuLabel_about || 'About' ) + ' '
+            const aboutLabel = '💡 ' + ( msgs.menuLabel_about || 'About' ) + ' '
                           + ( msgs.appName || app.name )
-            menu.ids.push(GM_registerMenuCommand.registerCommand(amLabel, modals.about.show))
+            menu.ids.push(GM_registerMenuCommand.registerCommand(aboutLabel, modals.about.show))
         },
 
         refresh() {
