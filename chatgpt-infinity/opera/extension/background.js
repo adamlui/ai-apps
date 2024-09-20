ae75f220e80a9dc222ea1a435c870aa5448e5894
adamlui@protonmail.com <adamlui@protonmail.com>
2024-09-20 08:55:37 -0700

    Restored launch chatgpt.com on-install

--- chatgpt-infinity/opera/extension/background.js
+++ chatgpt-infinity/opera/extension/background.js
@@ -1,3 +1,9 @@
+// Add on-install/update actions
+chrome.runtime.onInstalled.addListener(details => {
+    if (details.reason == 'install')
+        chrome.tabs.create({ url: 'https://chatgpt.com/' }) // open ChatGPT
+})
+
 // Relay popup.js msgs to all content.js
 chrome.runtime.onMessage.addListener(request =>
     chrome.tabs.query({}, tabs =>
