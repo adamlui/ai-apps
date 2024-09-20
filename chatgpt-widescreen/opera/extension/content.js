d2ed0e9d0d8947f01dabdc7aa2db0dd9fddbb72b
adamlui@protonmail.com <adamlui@protonmail.com>
2024-09-20 09:30:40 -0700

    Removed `sites.openai` init since removed from matches

--- chatgpt-widescreen/opera/extension/content.js
+++ chatgpt-widescreen/opera/extension/content.js
@@ -15,7 +15,6 @@
     const app = await (await fetch(chrome.runtime.getURL('app.json'))).json()
     app.urls.assetHost = app.urls.gitHub.replace('github.com', 'cdn.jsdelivr.net/gh') + `@${app.latestAssetCommitHash}`
     const sites = Object.assign(Object.create(null), await (await fetch(`${app.urls.assetHost}/data/sites.json`)).json())
-    sites.openai = { ...sites.chatgpt } // shallow copy to cover old domain
 
     // Init CONFIG
     await settings.load(...sites[site].availFeatures)
