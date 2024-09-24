9537ec13bbb8da10e6a859bb7bd579145d20dbc5
adamlui@protonmail.com <adamlui@protonmail.com>
2024-09-23 23:50:56 -0700

    Replaced `ui.app.scheme` in `schemeNotify()` icon logic w/ `scheme` arg for simplicity

--- duckduckgpt/greasemonkey/duckduckgpt.user.js
+++ duckduckgpt/greasemonkey/duckduckgpt.user.js
@@ -148,7 +148,7 @@
 // @description:zu         Yengeza izimpendulo ze-AI ku-DuckDuckGo (inikwa amandla yi-GPT-4o!)
 // @author                 KudoAI
 // @namespace              https://kudoai.com
-// @version                2024.9.23.1
+// @version                2024.9.23.2
 // @license                MIT
 // @icon                   https://media.ddgpt.com/images/icons/duckduckgpt/icon48.png?af89302
 // @icon64                 https://media.ddgpt.com/images/icons/duckduckgpt/icon64.png?af89302
@@ -1095,7 +1095,7 @@
                           notif = notifs[notifs.length -1]
 
                     // Append scheme icon
-                    const schemeIcon = icons[ui.app.scheme == 'light' ? 'sun' : scheme == 'dark' ? 'moon' : 'arrowsCycle'].create()
+                    const schemeIcon = icons[scheme == 'light' ? 'sun' : scheme == 'dark' ? 'moon' : 'arrowsCycle'].create()
                     schemeIcon.style.cssText = 'width: 23px ; height: 23px ; position: relative ; top: 3px ; margin-left: 6px'
                     notif.append(schemeIcon)
                 }
