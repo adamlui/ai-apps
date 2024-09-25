0ef0ed342337ba9b800212d796960d657fa08cd2
adamlui@protonmail.com <adamlui@protonmail.com>
2024-09-24 23:36:08 -0700

    Expanded commit hash in dom.js JSD URL to trigger GF CDN whitelisting

--- chatgpt-widescreen/greasemonkey/chatgpt-widescreen-mode.user.js
+++ chatgpt-widescreen/greasemonkey/chatgpt-widescreen-mode.user.js
@@ -222,7 +222,7 @@
 // @description:zu      Engeza izinhlobo zezimodi ze-Widescreen + Fullscreen ku-ChatGPT ukuze kube nokubonakala + ukuncitsha ukusukela
 // @author              Adam Lui
 // @namespace           https://github.com/adamlui
-// @version             2024.9.24.13
+// @version             2024.9.24.14
 // @license             MIT
 // @compatible          chrome
 // @compatible          firefox
@@ -240,7 +240,7 @@
 // @icon                https://media.chatgptwidescreen.com/images/icons/widescreen-robot-emoji/icon48.png?9a393be
 // @icon64              https://media.chatgptwidescreen.com/images/icons/widescreen-robot-emoji/icon64.png?9a393be
 // @require             https://cdn.jsdelivr.net/npm/@kudoai/chatgpt.js@3.3.1/dist/chatgpt.min.js#sha256-3yMGAFfqogI8VpkYXtVDQvk3wy8kMdJaQRE/rkAM/+8=
-// @require             https://cdn.jsdelivr.net/gh/adamlui/chatgpt-widescreen@d0efbd5/chrome/extension/lib/dom.js#sha256-uPLZZPoHc8IOhikZNFYVS9xPAxsv9R32D1esAJuvHJY=
+// @require             https://cdn.jsdelivr.net/gh/adamlui/chatgpt-widescreen@74f0649da8ae974608dd2aeca116e0c55ad367d5/chrome/extension/lib/dom.js#sha256-uPLZZPoHc8IOhikZNFYVS9xPAxsv9R32D1esAJuvHJY=
 // @connect             cdn.jsdelivr.net
 // @connect             greasyfork.org
 // @grant               GM_setValue
