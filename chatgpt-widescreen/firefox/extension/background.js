53e0805646f849441af4ad339800826ab97967b6
adamlui@protonmail.com <adamlui@protonmail.com>
2024-09-27 03:43:33 -0700

    Removed test fetcher

--- chatgpt-widescreen/firefox/extension/background.js
+++ chatgpt-widescreen/firefox/extension/background.js
@@ -7,19 +7,3 @@ chrome.runtime.onInstalled.addListener(details => {
 // Sync modes to activated tabs
 chrome.tabs.onActivated.addListener(activeInfo =>
     chrome.tabs.sendMessage(activeInfo.tabId, { action: 'sync.storageToUI' }))
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === 'fetchSites') {
-        fetch('https://cdn.jsdelivr.net/gh/adamlui/chatgpt-widescreen@ac4e3b1/sites.json')
-            .then(response => response.json())
-            .then(data => {
-                sendResponse({sites: data});
-            })
-            .catch(error => {
-                console.error('Error fetching sites.json:', error);
-                sendResponse({error: error.message});
-            });
-        return true;  // Indicates that the response is asynchronous
-    }
-});
-        
