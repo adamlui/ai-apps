9f67641fc4025820e88fd82af903d933e8868f1e
adamlui@protonmail.com <adamlui@protonmail.com>
2024-09-28 05:36:33 -0700

    Added `create.svgElem()`

--- chatgpt-infinity/opera/extension/lib/dom.js
+++ chatgpt-infinity/opera/extension/lib/dom.js
@@ -4,6 +4,12 @@ const dom = {
             const elem = document.createElement(elemType)
             Object.entries(attrs).forEach(([attr, val]) => elem.setAttribute(attr, val))
             return elem
+        },
+
+        svgElem(type, attrs) {
+            const elem = document.createElementNS('http://www.w3.org/2000/svg', type)
+            for (const attr in attrs) elem.setAttributeNS(null, attr, attrs[attr])
+            return elem
         }
     }
 }
