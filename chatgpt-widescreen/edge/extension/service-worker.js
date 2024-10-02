c4983d8e70b0dafac3fddbaee79fda74582948d8
adamlui@protonmail.com <adamlui@protonmail.com>
2024-10-02 00:25:32 -0700

    Deleted CWM opera/ + edge/ for simplicity

--- chatgpt-widescreen/edge/extension/service-worker.js
+++
@@ -1,9 +0,0 @@
-// Launch ChatGPT on install
-chrome.runtime.onInstalled.addListener(details => {
-    if (details.reason == 'install')
-        chrome.tabs.create({ url: 'https://chatgpt.com/' })
-})
-
-// Sync modes/settings to activated tabs
-chrome.tabs.onActivated.addListener(activeInfo =>
-    chrome.tabs.sendMessage(activeInfo.tabId, { action: 'sync.storageToUI' }))
