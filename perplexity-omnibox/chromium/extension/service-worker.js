f6d767553a3e64e1e10e4268a9dc207e3b0a76d5
adamlui@protonmail.com <adamlui@protonmail.com>
2024-10-08 07:20:24 -0700

    Added launch perplexity.ai on toolbar icon click

--- perplexity-omnibox/chromium/extension/service-worker.js
+++ perplexity-omnibox/chromium/extension/service-worker.js
@@ -1,3 +1,7 @@
-chrome.omnibox.onInputEntered.addListener(query => {
-    chrome.tabs.update({ url: `https://www.perplexity.ai/search/new?q=${query}` })
-})
+// Launch perplexity.ai on toolbar icon click
+chrome.action.onClicked.addListener(() =>
+    chrome.tabs.create({ url: 'https://www.perplexity.ai' }))
+
+// Query perplexity.ai on omnibox query submitted
+chrome.omnibox.onInputEntered.addListener(query =>
+    chrome.tabs.update({ url: `https://www.perplexity.ai/search/new?q=${query}` }))
