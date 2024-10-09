245be68a51f2099c5fbf72683623ba0938efef14
adamlui@protonmail.com <adamlui@protonmail.com>
2024-10-08 17:36:41 -0700

    Created Brave Omnibox Chromium extension

---
+++ brave-omnibox/chromium/extension/service-worker.js
@@ -0,0 +1,7 @@
+// Launch search.brave.com on toolbar icon click
+chrome.action.onClicked.addListener(() =>
+    chrome.tabs.create({ url: 'https://search.brave.com' }))
+
+// Query Brave AI on omnibox query submitted
+chrome.omnibox.onInputEntered.addListener(query =>
+    chrome.tabs.update({ url: `https://search.brave.com/search?q=${query}&summary=1` }))
